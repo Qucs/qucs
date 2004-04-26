6e6646ef9ad56d43e3b61c47993105638e23e675
ela <ela>
2004-04-26 13:49:51 +0000

    2004-04-26  Stefan Jahn  <stefan@lkcc.org>
            * dcsolver.cpp: Fixed memory leak when re-using a DC analysis.
    
            * net.cpp: Fixed same order parameter sweep definitions.
    
    2004-04-26  Stefan Jahn  <stefan@lkcc.org>
    
            * circulator.cpp: Implemented DC analysis for circulator.

--- qucs-core/src/components/circulator.h
+++ qucs-core/src/components/circulator.h
@@ -1,7 +1,7 @@
 /*
  * circulator.h - circulator class definitions
  *
- * Copyright (C) 2003 Stefan Jahn <stefan@lkcc.org>
+ * Copyright (C) 2003, 2004 Stefan Jahn <stefan@lkcc.org>
  *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,7 +18,7 @@
  * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.  
  *
- * $Id: circulator.h,v 1.3 2004/02/17 15:30:58 ela Exp $
+ * $Id: circulator.h,v 1.4 2004/04/26 13:50:03 ela Exp $
  *
  */
 
@@ -30,6 +30,7 @@ class circulator : public circuit
  public:
   circulator ();
   void calcSP (nr_double_t);
+  void calcDC (void);
 };
 
 #endif /* __CIRCULATOR_H__ */
--- qucs-core/src/net.h
+++ qucs-core/src/net.h
@@ -18,7 +18,7 @@
  * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.  
  *
- * $Id: net.h,v 1.5 2004/04/13 20:41:17 ela Exp $
+ * $Id: net.h,v 1.6 2004/04/26 13:50:03 ela Exp $
  *
  */
 
@@ -58,7 +58,9 @@ class net : public object
   void setReduced (int r) { reduced = r; }
   int getVoltageSources (void) { return nSources; }
   analysis * findAnalysis (char *);
-  analysis * firstOrderAnalysis (void);
+  analysis * findSecondOrder (void);
+  analysis * getChildAnalysis (analysis *);
+  char * getChild (analysis *);
   int orderAnalysis (void);
   analysis * findLastOrder (analysis *);
   environment * getEnv (void) { return env; }
