5e7d1d0e6e12146f66a0f85f5c3f670630521fb5
margraf <margraf>
2004-05-26 19:47:55 +0000

    *** empty log message ***

--- qucs/qucs_it.ts
+++ qucs/qucs_it.ts
@@ -2726,11 +2726,11 @@ Chiude il documento corrente</translation>
     </message>
     <message>
         <source>Settings</source>
-        <translation>Impostazioni</translation>
+        <translation type="obsolete">Impostazioni</translation>
     </message>
     <message>
         <source>Settings...</source>
-        <translation>Impostazioni...</translation>
+        <translation type="obsolete">Impostazioni...</translation>
     </message>
     <message>
         <source>Settings
@@ -3575,10 +3575,6 @@ Sets properties of the application</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Font:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Document Background Color:</source>
         <translation type="unfinished"></translation>
     </message>
@@ -3604,6 +3600,10 @@ Sets properties of the application</source>
     </message>
     <message>
         <source>Default Values</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Font (set after reload):</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
