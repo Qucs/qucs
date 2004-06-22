d9d5611cb0b072c89a0731134cf4868de72d060b
ela <ela@b5b04e8c-4942-46c9-ab4f-83783d557d1c>
2004-06-22 08:28:23 +0000

    * Added Isr and Nr to JFET properties.  Exchanged Drain and Source 	  port.  Using nfet/pfet for polarity.
    git-svn-id: https://qucs.svn.sourceforge.net/svnroot/qucs/trunk@209 b5b04e8c-4942-46c9-ab4f-83783d557d1c

--- qucs/qucs/qucs_it.ts
+++ qucs/qucs/qucs_it.ts
@@ -1522,6 +1522,18 @@ Wrong &apos;component&apos; line format!</source>
         <source>schematic symbol (european,US)</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>polarity (nfet,pfet)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>gate-junction recombination current parameter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Isr emission coefficient</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>QucsActions</name>
