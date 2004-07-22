609831d91a888eec4b798f1f19b0736807272549
ela <ela>
2004-07-22 21:04:42 +0000

    2004-07-22  Stefan Jahn  <stefan@lkcc.org>
            * check_touchstone.cpp: Support for noise parameters in
            touchstone files.

--- qucs-core/src/check_touchstone.cpp
+++ qucs-core/src/check_touchstone.cpp
@@ -18,7 +18,7 @@
  * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.  
  *
- * $Id: check_touchstone.cpp,v 1.3 2004/07/21 16:25:08 ela Exp $
+ * $Id: check_touchstone.cpp,v 1.4 2004/07/22 21:04:46 ela Exp $
  *
  */
 
@@ -49,14 +49,15 @@ dataset * touchstone_result = NULL;
 vector  * touchstone_vector = NULL;
 
 /* default touchstone options */
-struct touchstone_t touchstone_options = { "GHz", 'S', "MA", 50.0, 1e9, 0 };
+struct touchstone_t touchstone_options = { 
+  "GHz", 'S', "MA", 50.0, 1e9, 0, 0, 0 };
 
-/* avalaible touchstone options */
+/* available touchstone options */
 static char * touchstone_valid_options[] = {
   "hz", "khz", "mhz", "ghz", "s", "y", "z", "g", "h", "ma", "db", "ri", NULL };
 
 /* This subroutine is going to join vectors on multiple lines.  The
-   input and output list of vector of this function is the
+   input and output list of vectors of this function is the
    touchstone_vector variable. */
 static void touchstone_join (void) {
   vector * yroot, * xroot, * next = NULL;
@@ -66,8 +67,8 @@ static void touchstone_join (void) {
     next = (vector *) yroot->getNext ();
     for (xroot = next; xroot != NULL; xroot = next) {
       next = (vector *) xroot->getNext ();
-      /* append xroot vector to yroot vector ? */
-      if (xroot->getSize () % 2 == 0) {
+      /* append xroot vector to yroot vector (even no. of values) ? */
+      if (xroot->getSize () & 1 == 0) {
 	/* yes, delete the xroot vector and adjust list */
 	yroot->add (xroot);
 	yroot->setNext (next);
@@ -88,47 +89,65 @@ static void touchstone_join (void) {
    otherwise. */
 static int touchstone_vector_check (void) {
   vector * root = touchstone_vector, * next;
-  int even = 0, errors = 0, size = root->getSize ();
+  int even = 0, errors = 0, size = root->getSize (), noise = 0, lines = 1;
   nr_double_t f = real (root->get (0));
 
   /* check size of first line */
-  if ((size % 2) == 0) {
-    logprint (LOG_ERROR, "checker error, first line has %d (even) values\n", 
-	      size);
+  if ((size & 1) == 0) {
+    logprint (LOG_ERROR, "checker error, first data line has %d (even) "
+	      "values\n", size);
     errors++;
     even = 1;
   }
+  /* first line determines the number of expected ports */
   touchstone_options.ports = (int) sqrt ((size - 1) / 2);
 
   /* check first frequency value */
   if (f < 0.0) {
-    logprint (LOG_ERROR, "checker error, negative frequency value %e\n", f);
+    logprint (LOG_ERROR, "checker error, negative data frequency "
+	      "value %g\n", f);
     errors++;
   }
 
   /* go through each vector */
   for (root = (vector *) root->getNext (); root != NULL; root = next) {
     next = (vector *) root->getNext ();
+    nr_double_t freq = real (root->get (0));
+    
+    /* check increasing frequency value */
+    if (f >= freq) {
+      if (!noise) {
+	/* determined start of noise parameters */
+	noise++;
+	size = 5;
+	if (freq < 0.0) {
+	  logprint (LOG_ERROR, "checker error, negative noise frequency "
+		    "value %g\n", freq);
+	  errors++;
+	}
+      }
+      else {
+	logprint (LOG_ERROR, "checker error, %s line (f = %g) has "
+		  "decreasing frequency value\n", noise ? "noise" : "data",
+		  freq);
+	errors++;
+      }
+    }
+    f = freq;
+
     /* check size of vector */
     if (!even && root->getSize () != size) {
-      logprint (LOG_ERROR, "checker error, line (f = %e) has %d values, %d "
-		"required\n", real (root->get (0)), root->getSize (), size);
+      logprint (LOG_ERROR, "checker error, %s line (f = %g) has %d values, "
+		"%d required\n", noise ? "noise" : "data",
+		real (root->get (0)), root->getSize (), size);
       errors++;
     }
-    /* check frequency value */
-    if (real (root->get (0)) < 0.0) {
-      logprint (LOG_ERROR, "checker error, negative frequency value %e\n",
-		real (root->get (0)));
-      errors++;
-    }
-    /* check increasing frequency value */
-    if (f >= real (root->get (0))) {
-      logprint (LOG_ERROR, "checker error, line (f = %e) has decreasing "
-		"frequency value\n", real (root->get (0)));
-      errors++;
-    }
-    f = real (root->get (0));
+
+    /* count number of data lines without noise entries */
+    if (!noise) lines++;
   }
+  touchstone_options.noise = noise;
+  touchstone_options.lines = lines;
   return errors;
 }
 
@@ -186,22 +205,16 @@ static void touchstone_options_eval (void) {
 
 /* This little function returns a static string containing an
    appropriate variable name. */
-static char * touchstone_create_set (int i, int j) {
+static char * touchstone_create_set (int r, int c) {
   char * text;
-  text = matvec::createMatrixString (touchstone_options.parameter, i, j);
-  if (touchstone_options.ports == 2) {
-    /* handle special case for 2-port touchstone data, '21' data
-       precedes the '12' data */
-    if (i != j)
-      text = matvec::createMatrixString (touchstone_options.parameter, j, i);
-  }
+  text = matvec::createMatrixString (touchstone_options.parameter, r, c);
   return text;
 }
 
 /* The function actually creates the resulting dataset. */
 static void touchstone_create (void) {
-  vector * f, * v, * root, * next;
-  int ports = touchstone_options.ports;
+  vector * f, * v, * root, * next, * nf = NULL;
+  int ports = touchstone_options.ports, n;
   complex val;
   strlist * s;
 
@@ -212,62 +225,110 @@ static void touchstone_create (void) {
   s = new strlist ();
   s->add (f->getName ());
   /* create variable vectors for the resulting dataset */
-  for (int i = 0; i < ports; i++) {
-    for (int j = 0; j < ports; j++) {
+  for (int r = 1; r <= ports; r++) {
+    for (int c = 1; c <= ports; c++) {
       v = new vector ();
-      v->setName (touchstone_create_set (i + 1, j + 1));
-      v->setDependencies (s);
+      v->setName (touchstone_create_set (r, c));
+      v->setDependencies (new strlist (*s));
       touchstone_result->appendVariable (v);
     }
   }
+  delete s;
+
+  /* create noise vectors if necessary */
+  if (touchstone_options.noise) {
+    nf = new vector ("nfreq");
+    touchstone_result->appendDependency (nf);
+    s = new strlist ();
+    s->add (nf->getName ());
+    /* append noise parameters to dataset */
+    v = new vector ("Fmin");
+    v->setDependencies (new strlist (*s));
+    touchstone_result->appendVariable (v);
+    v = new vector ("Gopt");
+    v->setDependencies (new strlist (*s));
+    touchstone_result->appendVariable (v);
+    v = new vector ("Rn");
+    v->setDependencies (new strlist (*s));
+    touchstone_result->appendVariable (v);    
+    delete s;
+  }
 
   /* go through each vector */
-  for (root = touchstone_vector; root != NULL; root = next) {
+  for (n = 0, root = touchstone_vector; root != NULL; root = next, n++) {
     next = (vector *) root->getNext ();
-    /* fill frequency vector */
-    f->add (real (root->get (0)) * touchstone_options.factor);
-    /* go through each variable vector */
-    v = touchstone_result->getVariables ();
-    for (int i = 0; i < ports; i++) {
-      for (int j = 0; j < ports; j++) {
-	int pos = 1 + j * 2 + i * 2 * ports;
-	/* depending on the touchstone data format */
-	if (!strcmp (touchstone_options.format, "RI")) {
-	  val = rect (real (root->get (pos + 0)),
-		      real (root->get (pos + 1)));
+    // handle data lines
+    if (n < touchstone_options.lines) {
+      /* fill frequency vector */
+      f->add (real (root->get (0)) * touchstone_options.factor);
+      /* go through each variable vector */
+      v = touchstone_result->getVariables ();
+      for (int i = 0; i < ports; i++) {
+	for (int j = 0; j < ports; j++) {
+	  int pos = 1 + j * 2 + i * 2 * ports;
+	  /* handle special case for 2-port touchstone data, '21' data
+	     precedes the '12' data */
+	  if (ports == 2 && i != j) {
+	    pos = 1 + i * 2 + j * 2 * ports;
+	  }
+	  /* depending on the touchstone data format */
+	  if (!strcmp (touchstone_options.format, "RI")) {
+	    val = rect (real (root->get (pos + 0)),
+			real (root->get (pos + 1)));
+	  }
+	  else if (!strcmp (touchstone_options.format, "MA")) {
+	    val = polar (real (root->get (pos + 0)),
+			 rad (real (root->get (pos + 1))));
+	  }
+	  else if (!strcmp (touchstone_options.format, "dB")) {
+	    val = polar (pow (10.0, real (root->get (pos + 0)) / 20.0),
+			 rad (real (root->get (pos + 1))));
+	  }
+	  v->add (val);
+	  v = (vector *) v->getNext ();
 	}
-	else if (!strcmp (touchstone_options.format, "MA")) {
-	  val = polar (real (root->get (pos + 0)),
-		       rad (real (root->get (pos + 1))));
-	}
-	else if (!strcmp (touchstone_options.format, "dB")) {
-	  val = polar (pow (10.0, real (root->get (pos + 0)) / 20.0),
-		       rad (real (root->get (pos + 1))));
-	}
-	v->add (val);
-	v = (vector *) v->getNext ();
       }
+    }
+    // handle noise lines
+    else if (touchstone_options.noise) {
+      /* fill frequency vector */
+      nf->add (real (root->get (0)) * touchstone_options.factor);
+      /* fill noise parameter vectors */
+      v = touchstone_result->findVariable ("Fmin");
+      val = pow (10.0, real (root->get (1)) / 10.0);
+      v->add (val);
+      v = touchstone_result->findVariable ("Gopt");
+      val = polar (real (root->get (2)), rad (real (root->get (3))));
+      v->add (val);
+      v = touchstone_result->findVariable ("Rn");
+      val = real (root->get (4)) * circuit::z0 / touchstone_options.resistance;
+      v->add (val);
     }
   }
 }
 
-// The function re-normalizes S-parameters.
+/* The function re-normalizes S-parameters to the internal reference
+   impedance 50 Ohms. */
 static void touchstone_normalize_sp (void) {
   int ports = touchstone_options.ports;
   vector * v = touchstone_result->getVariables ();
   int len = v->getSize ();
   matrix s = matrix (ports);
   
+  // go through each matrix entry
   for (int n = 0; n < len; n++) {
     v = touchstone_result->getVariables ();
+    // save entries in a temporary matrix
     for (int i = 1; i <= ports; i++) {
       for (int j = 1; j <= ports; j++) {
 	s.set (i, j, v->get (n));
 	v = (vector *) v->getNext ();
       }
     }
+    // convert the temporary matrix
     s = stos (s, touchstone_options.resistance, circuit::z0);
     v = touchstone_result->getVariables ();
+    // restore the results in the entries
     for (int i = 1; i <= ports; i++) {
       for (int j = 1; j <= ports; j++) {
 	v->set (s.get (i, j), n);
@@ -283,43 +344,36 @@ static void touchstone_normalize (void) {
   vector * v = touchstone_result->getVariables ();
   int ports = touchstone_options.ports;
 
+  // nothing to do here
+  if (touchstone_options.resistance == circuit::z0)
+    return;
+
+  // transform S-parameters
   if (touchstone_options.parameter == 'S') {
     touchstone_normalize_sp ();
     return;
   }
-
+  // transform any other X-parameters
   for (int i = 1; i <= ports; i++) {
     for (int j = 1; j <= ports; j++) {
       switch (touchstone_options.parameter) {
-      case 'Y':
+      case 'Y': // Y-parameters
 	*v *= circuit::z0 / touchstone_options.resistance;
 	break;
-      case 'Z':
+      case 'Z': // Z-parameters
 	*v *= touchstone_options.resistance / circuit::z0;
 	break;
-      case 'G':
-	if (ports <= 2) {
-	  if (i == 1 && j == 1)
-	    *v *= circuit::z0 / touchstone_options.resistance;
-	  else if (i == 2 && j == 2)
-	    *v *= touchstone_options.resistance / circuit::z0;
-	}
-	else {
-	  logprint (LOG_ERROR, "checker error, impedance transformation for "
-		    "G-parameter %d-ports not defined\n", ports);
-	}
+      case 'G': // hybrid G-parameters
+	if (i == 1 && j == 1)
+	  *v *= circuit::z0 / touchstone_options.resistance;
+	else if (i == 2 && j == 2)
+	  *v *= touchstone_options.resistance / circuit::z0;
 	break;
-      case 'H':
-	if (ports <= 2) {
-	  if (i == 1 && j == 1)
-	    *v *= touchstone_options.resistance / circuit::z0;
-	  else if (i == 2 && j == 2)
-	    *v *= circuit::z0 / touchstone_options.resistance;
-	}
-	else {
-	  logprint (LOG_ERROR, "checker error, impedance transformation for "
-		    "H-parameter %d-ports not defined\n", ports);
-	}
+      case 'H': // hybrid H-parameters
+	if (i == 1 && j == 1)
+	  *v *= touchstone_options.resistance / circuit::z0;
+	else if (i == 2 && j == 2)
+	  *v *= circuit::z0 / touchstone_options.resistance;
 	break;
       }
       v = (vector *) v->getNext ();
@@ -346,6 +400,8 @@ static void touchstone_finalize (void) {
   touchstone_options.resistance = 50.0;
   touchstone_options.factor = 1e9;
   touchstone_options.ports = 0;
+  touchstone_options.noise = 0;
+  touchstone_options.lines = 0;
 }
 
 
@@ -390,6 +446,10 @@ int touchstone_check (void) {
     }
   }
 
+  /* evaluate the option line and put values into touchstone_options
+     structure */
+  touchstone_options_eval ();
+
   if (touchstone_vector == NULL) {
     logprint (LOG_ERROR, "checker error, no data in touchstone file\n");
     errors++;
@@ -400,11 +460,24 @@ int touchstone_check (void) {
 
     /* check each vector */
     errors += touchstone_vector_check ();
-  }
 
-  /* evaluate the option line and put values into touchstone_options
-     structure */
-  touchstone_options_eval ();
+    /* check validity of ports and parameters */
+    if ((touchstone_options.parameter == 'G' || 
+	 touchstone_options.parameter == 'H') &&
+	touchstone_options.ports != 2) {
+      logprint (LOG_ERROR, "checker error, %c-parameters for %d-ports not "
+		"defined\n", touchstone_options.parameter,
+		touchstone_options.ports);
+      errors++;
+    }
+
+    /* check noise parameter compatibility */
+    if (touchstone_options.noise && touchstone_options.ports != 2) {
+      logprint (LOG_ERROR, "checker error, noise parameters for %d-ports not "
+		"defined\n", touchstone_options.ports);
+      errors++;
+    }
+  }
 
   /* finally create a dataset */
   if (!errors) {
