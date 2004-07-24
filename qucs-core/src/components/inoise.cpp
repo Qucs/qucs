2199f082e5dafa2f608b6bbbe5d013bafca4c2c2
ela <ela@b5b04e8c-4942-46c9-ab4f-83783d557d1c>
2004-07-24 00:15:58 +0000

    * Added missing noise source files.
    git-svn-id: https://qucs.svn.sourceforge.net/svnroot/qucs/trunk@267 b5b04e8c-4942-46c9-ab4f-83783d557d1c

---
+++ qucs-core/src/components/inoise.cpp
@@ -0,0 +1,57 @@
+/*
+ * inoise.cpp - noise current source class implementation
+ *
+ * Copyright (C) 2004 Stefan Jahn <stefan@lkcc.org>
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this package; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.  
+ *
+ * $Id: inoise.cpp,v 1.1 2004-07-24 00:15:58 ela Exp $
+ *
+ */
+
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "complex.h"
+#include "object.h"
+#include "node.h"
+#include "circuit.h"
+#include "constants.h"
+#include "component_id.h"
+#include "inoise.h"
+
+inoise::inoise () : circuit (2) {
+  setS (1, 1, 1.0);
+  setS (1, 2, 0.0);
+  setS (2, 1, 0.0);
+  setS (2, 2, 1.0);
+  type = CIR_INOISE;
+}
+
+void inoise::calcSP (nr_double_t frequency) {
+  nr_double_t i = getPropertyDouble ("i");
+  nr_double_t e = getPropertyDouble ("e");
+  nr_double_t c = getPropertyDouble ("c");
+  nr_double_t a = getPropertyDouble ("a");
+  nr_double_t ipsd = i / (a + c * pow (frequency, e)) / kB / T0 * z0;
+  setN (1, 1, +ipsd); setN (2, 2, +ipsd);
+  setN (1, 2, -ipsd); setN (2, 1, -ipsd);
+}
---
+++ qucs-core/src/components/inoise.h
@@ -0,0 +1,36 @@
+/*
+ * inoise.h - noise current source class definitions
+ *
+ * Copyright (C) 2004 Stefan Jahn <stefan@lkcc.org>
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this package; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.  
+ *
+ * $Id: inoise.h,v 1.1 2004-07-24 00:15:58 ela Exp $
+ *
+ */
+
+#ifndef __INOISE_H__
+#define __INOISE_H__
+
+class inoise : public circuit
+{
+ public:
+  inoise ();
+  void calcDC (void) { }
+  void calcSP (nr_double_t);
+};
+
+#endif /* __INOISE_H__ */
