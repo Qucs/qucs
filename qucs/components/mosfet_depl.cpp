58ce8d0b5c00b62eb6f48d8f0a1632bf71f7f6b2
ela <ela>
2004-08-03 15:50:46 +0000

    2004-08-03  Stefan Jahn  <stefan@lkcc.org>
            * mosfet_depl.cpp (MOSFET_depl), mosfet.cpp (MOSFET): Added
            model properties.

--- qucs/components/mosfet_depl.cpp
+++ qucs/components/mosfet_depl.cpp
@@ -36,8 +36,8 @@ MOSFET_depl::MOSFET_depl()
   Lines.append(new Line( -9,  0, -4,  5,QPen(QPen::darkBlue,2)));
 
   Ports.append(new Port(-30,  0));
-  Ports.append(new Port(  0, 30));
   Ports.append(new Port(  0,-30));
+  Ports.append(new Port(  0, 30));
 
   x1 = -30; y1 = -30;
   x2 =   4; y2 =  30;
@@ -47,8 +47,76 @@ MOSFET_depl::MOSFET_depl()
   Model = "DMOSFET";
   Name  = "T";
 
-//  Props.append(new Property("Type", "n", true,
-//			QObject::tr("polarity (n,p)")));
+  // this must be the first property in the list !!!
+  Props.append(new Property("Type", "nfet", false,
+	QObject::tr("polarity (nfet,pfet)")));
+  Props.append(new Property("Vt0", "-1.0 V", true,
+	QObject::tr("zero-bias threshold voltage")));
+  Props.append(new Property("Kp", "2e-5", true,
+	QObject::tr("transconductance coefficient in A/m^2")));
+  Props.append(new Property("Gamma", "0.0", false,
+	QObject::tr("bulk threshold")));
+  Props.append(new Property("Phi", "0.6 V", false,
+	QObject::tr("surface potential")));
+  Props.append(new Property("Lambda", "0.0", true,
+	QObject::tr("channel-length modulation parameter")));
+  Props.append(new Property("Rd", "0.0 Ohm", false,
+	QObject::tr("drain ohmic resistance")));
+  Props.append(new Property("Rs", "0.0 Ohm", false,
+	QObject::tr("source ohmic resistance")));
+  Props.append(new Property("Rg", "0.0 Ohm", false,
+	QObject::tr("gate ohmic resistance")));
+  Props.append(new Property("Is", "1e-14 A", false,
+	QObject::tr("bulk junction saturation current")));
+  Props.append(new Property("N", "1.0", false,
+	QObject::tr("bulk junction emission coefficient")));
+  Props.append(new Property("W", "1 um", false,
+	QObject::tr("channel width")));
+  Props.append(new Property("L", "1 um", false,
+	QObject::tr("channel length")));
+  Props.append(new Property("Ld", "0.0", false,
+	QObject::tr("lateral diffusion length")));
+  Props.append(new Property("Tox", "0.1 um", false,
+	QObject::tr("oxide thickness")));
+  Props.append(new Property("Cgso", "0.0", false,
+	QObject::tr("gate-source overlap capacitance per meter of "
+		    "channel width in F/m")));
+  Props.append(new Property("Cgdo", "0.0", false,
+	QObject::tr("gate-drain overlap capacitance per meter of "
+		    "channel width in F/m")));
+  Props.append(new Property("Cgbo", "0.0", false,
+	QObject::tr("gate-bulk overlap capacitance per meter of "
+		    "channel length in F/m")));
+  Props.append(new Property("Cbd", "0.0 F", false,
+	QObject::tr("zero-bias bulk-drain junction capacitance")));
+  Props.append(new Property("Cbs", "0.0 F", false,
+	QObject::tr("zero-bias bulk-source junction capacitance")));
+  Props.append(new Property("Pb", "0.8 V", false,
+	QObject::tr("bulk junction potential")));
+  Props.append(new Property("Mj", "0.5", false,
+	QObject::tr("bulk junction bottom grading coefficient")));
+  Props.append(new Property("Fc", "0.5", false,
+	QObject::tr("bulk junction forward-bias depletion capacitance "
+		    "coefficient")));
+  Props.append(new Property("Tt", "0.0 ps", false,
+	QObject::tr("bulk transit time")));
+  Props.append(new Property("Nsub", "0.0", false,
+	QObject::tr("substrate bulk doping density in 1/cm^3")));
+  Props.append(new Property("Nss", "0.0", false,
+	QObject::tr("surface state density in 1/cm^2")));
+  Props.append(new Property("Tpg", "1", false,
+	QObject::tr("gate material type: 0 = alumina; -1 = same as bulk; "
+		    "1 = opposite to bulk")));
+  Props.append(new Property("Uo", "600.0", false,
+	QObject::tr("surface mobility in cm^2/Vs")));
+  Props.append(new Property("Kf", "0.0", false,
+	QObject::tr("flicker noise coefficient")));
+  Props.append(new Property("Af", "1.0", false,
+	QObject::tr("flicker noise exponent")));
+  Props.append(new Property("Ffe", "1.0", false,
+	QObject::tr("flicker noise frequency exponent")));
+  Props.append(new Property("Temp", "26.85", false,
+	QObject::tr("simulation temperature in degree Celsius")));
 }
 
 MOSFET_depl::~MOSFET_depl()
