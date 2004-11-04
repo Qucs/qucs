bd4f5ef561566c7d2160b514a8e4ebd60d946917
ela <ela>
2004-11-04 14:41:46 +0000

    * fixed translations files...

--- qucs/qucs_es.ts
+++ qucs/qucs_es.ts
@@ -2131,15 +2131,11 @@ Use:  qucsedit [-r] archivo
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>positive voltage limit at the output</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>negative voltage limit at the output</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>OpAmp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>absolute value of maximum and minimum output voltage</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
