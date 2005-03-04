06dda13184ef41e408458e26a00b453d23a58d0b
raimi <raimi>
2005-03-04 15:29:54 +0000

    qtgeneric.ts by Markus Gothe <nietzschian@gmail.com>
    2005-03-03  Stefan Jahn  <stefan@lkcc.org>
    
            * qucstrans.cpp: Initial attempt to create a 'transcalc' like
            GUI for Qt.  Thus the program can be ported for use with Qucs.

---
+++ qucs/qucs-transcalc/main.cpp
@@ -0,0 +1,150 @@
+/***************************************************************************
+                          main.cpp  -  description
+                             -------------------
+    begin                : Sun Feb 27 2005
+    copyright            : (C) 2004 by Stefan Jahn
+    email                : stefan@lkcc.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <stdlib.h>
+
+#include <qapplication.h>
+#include <qstring.h>
+#include <qtextcodec.h>
+#include <qtranslator.h>
+#include <qfile.h>
+#include <qtextstream.h>
+#include <qmessagebox.h>
+#include <qdir.h>
+#include <qfont.h>
+
+#include "qucstrans.h"
+
+tQucsSettings QucsSettings;
+
+// Loads the settings file and stores the settings.
+bool loadSettings() {
+  QFile file(QDir::homeDirPath()+QDir::convertSeparators ("/.qucs/transrc"));
+  if(!file.open(IO_ReadOnly)) return false; // settings file doesn't exist
+
+  QTextStream stream(&file);
+  QString Line, Setting;
+
+  while(!stream.atEnd()) {
+    Line = stream.readLine();
+    Setting = Line.section('=',0,0);
+    Line    = Line.section('=',1,1);
+    if(Setting == "TransWindow") {
+	QucsSettings.x  = Line.section(",",0,0).toInt();
+	QucsSettings.y  = Line.section(",",1,1).toInt();
+	QucsSettings.dx = Line.section(",",2,2).toInt();
+	QucsSettings.dy = Line.section(",",3,3).toInt();
+	break; }
+  }
+  file.close();
+
+  file.setName(QDir::homeDirPath()+QDir::convertSeparators ("/.qucs/transrc"));
+  if(!file.open(IO_ReadOnly)) return true; // qucs settings not necessary
+
+  while(!stream.atEnd()) {
+    Line = stream.readLine();
+    Setting = Line.section('=',0,0);
+    Line    = Line.section('=',1,1);
+    if(Setting == "Font") {     // get font from "transrc"
+	QucsSettings.font.fromString(Line);
+	break;
+    }
+  }
+  file.close();
+
+  return true;
+}
+
+// Saves the settings in the settings file.
+bool saveApplSettings(QucsTranscalc *qucs)
+{
+  if(qucs->x() == QucsSettings.x)
+    if(qucs->y() == QucsSettings.y)
+      if(qucs->width() == QucsSettings.dx)
+	if(qucs->height() == QucsSettings.dy)
+	  return true;   // nothing has changed
+
+
+  QFile file(QDir::homeDirPath()+QDir::convertSeparators ("/.qucs/transrc"));
+  if(!file.open(IO_WriteOnly)) {
+    QMessageBox::warning(0, QObject::tr("Warning"),
+			QObject::tr("Cannot save settings !"));
+    return false;
+  }
+
+  QString Line;
+  QTextStream stream(&file);
+
+  stream << "Settings file, Qucs TransCalc " PACKAGE_VERSION "\n"
+	 << "TransWindow=" << qucs->x() << ',' << qucs->y() << ','
+	 << qucs->width() << ',' << qucs->height() << '\n';
+
+  file.close();
+  return true;
+}
+
+// #########################################################################
+// ##########                                                     ##########
+// ##########                  Program Start                      ##########
+// ##########                                                     ##########
+// #########################################################################
+
+int main(int argc, char *argv[])
+{
+  // apply default settings
+  QucsSettings.x = 100;
+  QucsSettings.y = 50;
+  QucsSettings.dx = 520;
+  QucsSettings.dy = 550;
+  QucsSettings.font = QFont("Helvetica", 12);
+
+  // is application relocated?
+  char * var = getenv ("QUCSDIR");
+  if (var != NULL) {
+    QDir QucsDir = QDir (var);
+    QString QucsDirStr = QucsDir.canonicalPath ();
+    QucsSettings.BitmapDir =
+      QDir::convertSeparators (QucsDirStr + "/share/qucs/bitmaps/");
+    QucsSettings.LangDir =
+      QDir::convertSeparators (QucsDirStr + "/share/qucs/lang/");
+  } else {
+    QucsSettings.BitmapDir = BITMAPDIR;
+    QucsSettings.LangDir = LANGUAGEDIR;
+  }
+
+  loadSettings();
+
+  QApplication a(argc, argv);
+  a.setFont(QucsSettings.font);
+
+  QTranslator tor( 0 );
+  tor.load( QString("qucs_") + QTextCodec::locale(), QucsSettings.LangDir);
+  a.installTranslator( &tor );
+
+  QucsTranscalc *qucs = new QucsTranscalc();
+  a.setMainWidget(qucs);
+  qucs->resize(QucsSettings.dx, QucsSettings.dy); // size and position ...
+  qucs->move(QucsSettings.x, QucsSettings.y);     // ... before "show" !!!
+  qucs->show();
+  int result = a.exec();
+  saveApplSettings(qucs);
+  return result;
+}
