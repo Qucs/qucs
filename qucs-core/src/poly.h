aaa4870a29c5986f8949c35d0d29cb36809ec0aa
raimi <raimi@b5b04e8c-4942-46c9-ab4f-83783d557d1c>
2005-05-23 19:50:51 +0000

    now also add the files
    git-svn-id: https://qucs.svn.sourceforge.net/svnroot/qucs/trunk@623 b5b04e8c-4942-46c9-ab4f-83783d557d1c

---
+++ qucs-core/src/poly.h
@@ -0,0 +1,49 @@
+/*
+ * poly.h - poly class definitions and implementation
+ *
+ * Copyright (C) 2005 Stefan Jahn <stefan@lkcc.org>
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this package; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.  
+ *
+ * $Id: poly.h,v 1.1 2005-05-23 19:50:51 raimi Exp $
+ *
+ */
+
+#ifndef __POLY_H__
+#define __POLY_H__
+
+class poly
+{
+ public:
+  poly (nr_double_t _x, nr_double_t _y)
+    : x(_x), f0(_y) { f1 = f2 = 0; }
+  poly (nr_double_t _x, nr_double_t _f0, nr_double_t _f1)
+    : x(_x), f0(_f0), f1(_f1) { f2 = 0; }
+  poly (nr_double_t _x, nr_double_t _f0, nr_double_t _f1, nr_double_t _f2)
+    : x(_x), f0(_f0), f1(_f1), f2(_f2) { }
+  ~poly ()
+    { }
+  nr_double_t eval (nr_double_t _x) {
+    nr_double_t dx = _x - x; return f0 + dx * (f1 + dx * f2);
+  }
+
+  nr_double_t x;  // x     - argument
+  nr_double_t f0; // f(x)  - value
+  nr_double_t f1; // f'(x) - 1st derivative
+  nr_double_t f2; // f"(x) - 2nd derivative
+};
+
+#endif /* __POLY_H__ */
---
+++ qucs-core/src/spline.h
@@ -0,0 +1,65 @@
+/*
+ * spline.h - spline class definitions
+ *
+ * Copyright (C) 2005 Stefan Jahn <stefan@lkcc.org>
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this package; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.  
+ *
+ * $Id: spline.h,v 1.1 2005-05-23 19:50:51 raimi Exp $
+ *
+ */
+
+#ifndef __SPLINE_H__
+#define __SPLINE_H__
+
+// Types of boundary conditions.
+enum spline_boundary_type {
+  SPLINE_BC_UNKNOWN = -1,
+  SPLINE_BC_NATURAL,
+  SPLINE_BC_CLAMPED,
+  SPLINE_BC_PERIODIC
+};
+
+class vector;
+class poly;
+
+class spline
+{
+ public:
+  spline ();
+  spline (vector *, vector *);
+  ~spline ();
+
+  void vectors (vector *, vector *);
+  void construct (void);
+  poly evaluate (nr_double_t);
+  void setBoundary (int b) { boundary = b; }
+
+ private:
+  nr_double_t * upper_bound (nr_double_t *, nr_double_t *, nr_double_t);
+
+ private:
+  nr_double_t * x;
+  nr_double_t * f0;
+  nr_double_t * f1;
+  nr_double_t * f2;
+  nr_double_t * f3;
+  nr_double_t d0, dn;
+  int n;
+  int boundary;
+};
+
+#endif /* __SPLINE_H__ */
