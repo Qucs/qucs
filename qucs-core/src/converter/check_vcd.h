97997bc248fc602bd6abc9e1543bce66f34c4a65
raimi <raimi@b5b04e8c-4942-46c9-ab4f-83783d557d1c>
2005-12-26 21:13:31 +0000

    2005-12-26  Raimund 'Raimi' Jacob  <raimi@lkcc.org>
    	* qucsconv.cpp: New infrastructure for converter steps.
    
    	* begin of vcd recognition
    
    
    git-svn-id: https://qucs.svn.sourceforge.net/svnroot/qucs/trunk@740 b5b04e8c-4942-46c9-ab4f-83783d557d1c

---
+++ qucs-core/src/converter/check_vcd.h
@@ -0,0 +1,77 @@
+/*
+ * check_vcd.h - checker definitions for a vcd
+ *
+ * Copyright (C) 2005 Raimund Jacob <raimi@lkcc.org>
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this package; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+ * Boston, MA 02110-1301, USA.  
+ *
+ * $Id: check_vcd.h,v 1.1 2005-12-26 21:13:31 raimi Exp $
+ *
+ */
+
+#ifndef __CHECK_VCD_H__
+#define __CHECK_VCD_H__
+
+/* Externalize variables used by the scanner and parser. */
+extern int vcd_lineno;
+extern FILE * vcd_in;
+
+__BEGIN_DECLS
+
+/* Available functions of the checker. */
+int  vcd_checker (void);
+int  vcd_parse (void);
+int  vcd_error (char *);
+int  vcd_lex (void);
+int  vcd_lex_destroy (void);
+void vcd_destroy (void);
+
+__END_DECLS
+
+/* VCD data structures. */
+struct vcd_vardef {
+  int type;
+  int size;
+  char * idcode;
+  char * name;
+  struct vcd_vardef * next;
+};
+
+struct vcd_scope {
+  int type;
+  char * name;
+  struct vcd_vardef * root;
+};
+
+struct vcd_change {
+  struct vcd_vardef * var;
+  char * value;
+  struct vcd_change * next;
+};
+
+struct vcd_changeset {
+  /* Note: .vcd may contain multiple mergable sets for a single time */
+  int time;
+  struct vcd_change * root;
+};
+
+struct vcd_file {
+  int time;
+  char * scale;
+  struct vcd_scope * root;
+};
+
+#endif /* __CHECK_VCD_H__ */
--- qucs-core/src/converter/qucsconv.cpp
+++ qucs-core/src/converter/qucsconv.cpp
@@ -18,7 +18,7 @@
  * the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
  * Boston, MA 02110-1301, USA.
  *
- * $Id: qucsconv.cpp,v 1.9 2005-11-14 19:19:14 raimi Exp $
+ * $Id: qucsconv.cpp,v 1.10 2005-12-26 21:13:31 raimi Exp $
  *
  */
 
@@ -33,6 +33,25 @@
 
 #include "check_spice.h"
 #include "qucs_producer.h"
+
+struct actionset_t {
+  char * informat; /* -if parameter */
+  char * outformat; /* -of parameter */
+
+  /* Callback for the logic, return errorcode of program */
+  int (* execute) (struct actionset_t *, char * infile, char * outfile);
+};
+
+int spice2qucs (struct actionset_t *, char *, char *);
+int vcd2qucs (struct actionset_t *, char *, char *);
+
+struct actionset_t actionset[] = {
+  {"spice", "qucs",    spice2qucs},
+  {"spice", "qucslib", spice2qucs},
+  {"vcd", "qucsdata",  vcd2qucs},
+  {NULL, NULL, NULL}
+};
+
 
 FILE * open_file (char * file, char * flag) {
   FILE * fd = NULL;
@@ -99,38 +118,70 @@ int main (int argc, char ** argv) {
     }
   }
 
-  if (input && !strcmp (input, "spice")) {
-    int ret = 0;
-    if ((spice_in = open_file (infile, "r")) == NULL) {
-      ret = -1;
-    } else if (spice_parse () != 0) {
-      ret = -1;
-    } else if (spice_checker () != 0) {
-      ret = -1;
+  int inknown = 0;
+  int outknown = 0;
+  for (int j = 0; actionset[j].informat != NULL; j++) {
+    int in = 0, out = 0;
+
+    if (input && !strcmp (input, actionset[j].informat)) {
+      in = 1;
+      inknown = 1;
     }
-    spice_lex_destroy ();
-    fclose (spice_in);
-    if (ret) {
-      spice_destroy ();
-      return -1;
+
+    if (output && !strcmp (output, actionset[j].outformat)) {
+      out = 1;
+      outknown = 1;
+    }
+
+    if (in && out) {
+      return actionset[j].execute (&actionset[j], infile, outfile);
     }
   }
-  else {
+
+  if (!inknown) {
     fprintf (stderr, "invalid input data specification `%s'\n",
 	     input ? input : "not given");
+  }
+
+  if (!outknown) {
+    fprintf (stderr, "invalid output data specification `%s'\n",
+	     output ? output : "not given");
+  }
+
+  return -1;
+}
+
+
+int spice2qucs (struct actionset_t * action, char * infile, char * outfile) {
+  int ret = 0;
+  if ((spice_in = open_file (infile, "r")) == NULL) {
+    ret = -1;
+  } else if (spice_parse () != 0) {
+    ret = -1;
+  } else if (spice_checker () != 0) {
+    ret = -1;
+  }
+  spice_lex_destroy ();
+  fclose (spice_in);
+  if (ret) {
+    spice_destroy ();
     return -1;
   }
 
-  if (output && (!strcmp (output, "qucs") || !strcmp (output, "qucslib"))) {
-    if ((qucs_out = open_file (outfile, "w")) == NULL)
-      return -1;
-    if (!strcmp (output, "qucs"))
-      qucs_producer ();
-    else
-      qucslib_producer ();
-    fclose (qucs_out);
-  }
-
+  if ((qucs_out = open_file (outfile, "w")) == NULL)
+    return -1;
+  if (!strcmp (action->outformat, "qucs"))
+    qucs_producer ();
+  else /* "qucslib" */
+    qucslib_producer ();
+  fclose (qucs_out);
   spice_destroy ();
   return 0;
 }
+
+
+int vcd2qucs (struct actionset_t * action, char * infile, char * outfile) {
+  /* TBI */
+  return 0;
+}
+
