7f7976fe3335ad6237bf8d57d57e768f2ecb8054
raimi <raimi>
2006-01-13 09:07:34 +0000

    from stefan: no utf-8 in sources

--- qucs/qucs-filter/qf_filter.cpp
+++ qucs/qucs-filter/qf_filter.cpp
@@ -17,6 +17,7 @@
 
 #include <math.h>
 #include <iostream>
+#include <strstream>
 #include <string>
 #include <stdlib.h>
 #include <string.h>
@@ -30,7 +31,7 @@
 qf_filter::qf_filter (int n) :
   type (LOWPASS), kind (UNDEF), o (n), f (0), bw (0), imp (1),
   ncomp (0), Comp (NULL) {
-//  std::cerr << "New filter of " << n << "th order.\n" ;
+//  std::cerr << "New filter of " << n << "th order.\n";
 }
 
 // Default constructor
@@ -59,7 +60,6 @@ qf_filter::~qf_filter (void) {
     free (Comp);
 }
 
-
 // Extraction routines
 
 // Extract finite attenuation pole
@@ -72,24 +72,23 @@ void qf_filter::extract_pole_pCsLC (double p, qfc * pComp, double Ws) {
   double bdpl = BD.evalX2 (pl);
 
   // Partial removal of infinite pole (first // cap)
-  // c = [B(s)/s] (s² = - Ω²)
+  // c = [B(s)/s] (s^2 = - O^2)
   double c = ((BN << 1).evalX2 (pl)) / bdpl;
   pComp->val = c * Ws;
-
   qf_poly cS (c, 0, 0, 1);
-  BN = BN - (BD * cS);		// B = B - c s 
+  BN = BN - (BD * cS);		// B = B - cs
   BN.disp ("BN");
   BN.div (0, p);
 
   // Full removal of finite pole
-  // c1 = (s B(s) / (s² + Ω²)) @ s² = - Ω²
+  // c1 = (s B(s) / (s^2 + O^2)) @ s^2 = - O^2
   BN.disp ("BN");
   double c1 = (BN >> 1).evalX2 (pl) / bdpl;
   (pComp + 1)->val = c1;
   (pComp + 2)->val = -Ws / (c1 * pl);
   (pComp + 1)->val *= Ws;
 
-  // 1/B = 1/B - (s/c1) / (s² + Ω²)
+  // 1/B = 1/B - (s/c1) / (s^2 + O^2)
   BD = BD - (BN >> 1) * (1 / c1);
   BD.div (0, p);
 
@@ -113,7 +112,7 @@ void qf_filter::dump_cout (void) {
       break;
     case RES:
       std::cout << "Res. from node ";
-      unit = "Ω";
+      unit = "Ohm";
       break;
     }
 
@@ -129,7 +128,7 @@ void qf_filter::dump_cout (void) {
     else if (Comp[i].val >= 1e-3)
       std::cout << Comp[i].val * 1e3 << " m" << unit << '\n';
     else if (Comp[i].val >= 1e-6)
-      std::cout << Comp[i].val * 1e6 << " µ" << unit << '\n';
+      std::cout << Comp[i].val * 1e6 << " u" << unit << '\n';
     else if (Comp[i].val >= 1e-9)
       std::cout << Comp[i].val * 1e9 << " n" << unit << '\n';
     else if (Comp[i].val >= 1e-12)
@@ -141,54 +140,67 @@ void qf_filter::dump_cout (void) {
   }
 }
 
-void qf_filter::dump_spice (void) {
+std::string qf_filter::to_spice (void) {
+  std::ostrstream res;
   unsigned i = 0;
 
+  res << "* SPICE filter\n";
   for (unsigned ic = 1, il = 1, ir = 1; i < ncomp; i++) {
     switch (Comp[i].comp) {
     case CAP:
-      std::cout << "C" << ic << "\t";
-      std::cout << Comp[i].node1 << "\t" << Comp[i].node2;
-      std::cout << "\t" << Comp[i].val << '\n';
+      res << "C" << ic << "\t";
+      res << Comp[i].node1 << "\t" << Comp[i].node2;
+      res << "\t" << Comp[i].val << '\n';
       // Insert parallel resistor
-      std::cout << "R" << ir << "\t";
-      std::cout << Comp[i].node1 << "\t" << Comp[i].node2;
-      std::cout << "\t10G\n";
+      res << "R" << ir << "\t";
+      res << Comp[i].node1 << "\t" << Comp[i].node2;
+      res << "\t10G\n";
       ic++;
       ir++;
       break;
     case IND:
-      std::cout << "L" << il << "\t";
-      std::cout << Comp[i].node1 << "\t"
+      res << "L" << il << "\t";
+      res << Comp[i].node1 << "\t"
 	<< Comp[i].node1 * 100 + Comp[i].node2 << "\t";
-      std::cout << Comp[i].val << "\n";
+      res << Comp[i].val << "\n";
       il++;
       // Insert serial resistor
-      std::cout << "R" << ir << "\t";
-      std::cout << Comp[i].node1 * 100 + Comp[i].node2
+      res << "R" << ir << "\t";
+      res << Comp[i].node1 * 100 + Comp[i].node2
 	<< "\t" << Comp[i].node2 << "\t";
-      std::cout << "1u\n";
+      res << "1u\n";
       ir++;
       break;
     case RES:
-      std::cout << "R" << ir << "\t";
+      res << "R" << ir << "\t";
       ir++;
       break;
     }
   }
 
-  std::cout << "RI\t10000\t1\t" << imp << "\n";
-  std::cout << "RL\t";
+  res << "RI\t10000\t1\t" << imp << "\n";
+  res << "RL\t";
 
   if (Comp[i - 1].node2 == 0)
-    std::cout << Comp[i - 1].node1;
+    res << Comp[i - 1].node1;
   else
-    std::cout << Comp[i - 1].node2;
+    res << Comp[i - 1].node2;
 
-  std::cout << "\t0\t" << imp << "\n";
-  std::cout << "VCC\t10000\t0\tDC\t1\tAC\t1\n";
-  std::cout << ".end\n";
+  res << "\t0\t" << imp << "\n";
+  res << "VCC\t10000\t0\tDC\t1\tAC\t1\n";
+  res << ".END\n";
+  return res.str();
 }
 
-void qf_filter::to_Qucs (void) {
+void qf_filter::dump_spice (void) {
+  std::cout << to_spice();
+}
+
+std::string qf_filter::to_qucs (void) {
+  std::ostrstream res;
+  return res.str();
+}
+
+void qf_filter::dump_qucs (void) {
+  std::cout << to_qucs();
 }
