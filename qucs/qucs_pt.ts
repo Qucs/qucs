cdca4242f0c70cf6ed48f54efc8611b531ee6af8
margraf <margraf>
2006-01-16 07:19:57 +0000

    *** empty log message ***

--- qucs/qucs_pt.ts
+++ qucs/qucs_pt.ts
@@ -3137,6 +3137,10 @@ Usage:  qucsedit [-r] file
         <source>RS-FlipFlop</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>Error: Wrong time format in &quot;%1&quot;. Use positive number with units</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>QucsActions</name>
