db41e49c04eed9b913dcc0259fc2444cd287b890
margraf <margraf@b5b04e8c-4942-46c9-ab4f-83783d557d1c>
2006-07-24 06:49:10 +0000

    *** empty log message ***
    git-svn-id: https://qucs.svn.sourceforge.net/svnroot/qucs/trunk@932 b5b04e8c-4942-46c9-ab4f-83783d557d1c

---
+++ qucs/qucs/components/opt_sim.cpp
@@ -0,0 +1,56 @@
+/***************************************************************************
+                               opt_sim.cpp
+                              -------------
+    begin                : Sat Jul 22 2006
+    copyright            : (C) 2006 by Michael Margraf
+    email                : michael.margraf@alumni.tu-berlin.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "opt_sim.h"
+#include "main.h"
+
+
+Optimize_Sim::Optimize_Sim()
+{
+  Description = QObject::tr("Optimization");
+
+  Texts.append(new Text(0, 0, Description, QPen::darkBlue, QucsSettings.largeFontSize));
+
+  x1 = -10; y1 = -9;
+  x2 = x1+128; y2 = y1+41;
+
+  tx = 0;
+  ty = y2+1;
+  Model = ".Opt";
+  Name  = "Opt";
+
+  Props.append(new Property("Type", "lin", true,
+			QObject::tr("sweep type")+" [lin, log, list, const]"));
+}
+
+Optimize_Sim::~Optimize_Sim()
+{
+}
+
+Component* Optimize_Sim::newOne()
+{
+  return new Optimize_Sim();
+}
+
+Element* Optimize_Sim::info(QString& Name, char* &BitmapFile, bool getNewOne)
+{
+  Name = QObject::tr("optimization");
+  BitmapFile = "optimize";
+
+  if(getNewOne)  return new Optimize_Sim();
+  return 0;
+}
---
+++ qucs/qucs/components/opt_sim.h
@@ -0,0 +1,32 @@
+/***************************************************************************
+                               opt_sim.h
+                              -----------
+    begin                : Sat Jul 22 2006
+    copyright            : (C) 2006 by Michael Margraf
+    email                : michael.margraf@alumni.tu-berlin.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef OPT_SIM_H
+#define OPT_SIM_H
+
+#include "component.h"
+
+
+class Optimize_Sim : public Component  {
+public:
+  Optimize_Sim();
+ ~Optimize_Sim();
+  Component* newOne();
+  static Element* info(QString&, char* &, bool getNewOne=false);
+};
+
+#endif
