37378bdac9ce36bc43747a296e0f241ef983b6ae
margraf <margraf@b5b04e8c-4942-46c9-ab4f-83783d557d1c>
2006-12-27 07:08:32 +0000

    *** empty log message ***
    git-svn-id: https://qucs.svn.sourceforge.net/svnroot/qucs/trunk@1043 b5b04e8c-4942-46c9-ab4f-83783d557d1c

---
+++ qucs/components/bondwire.cpp
@@ -0,0 +1,73 @@
+/***************************************************************************
+                               bondwire.cpp
+                              --------------
+    begin                : Dec 22 2006
+    copyright            : (C) 2006 by Michael Margraf
+    email                : michael.margraf@alumni.tu-berlin.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "bondwire.h"
+
+
+BondWire::BondWire()
+{
+  Description = QObject::tr("bond wire");
+
+  Lines.append(new Line(-30, 0,-8, 0,QPen(QPen::darkBlue,3)));
+  Lines.append(new Line( 30, 0, 8, 0,QPen(QPen::darkBlue,3)));
+
+  Arcs.append(new Arc(-11,-10, 22, 26, 16*30,16*120,QPen(QPen::darkBlue,1)));
+  Arcs.append(new Arc(-19,-13, 10, 13,16*205,16*130,QPen(QPen::darkBlue,1)));
+  Arcs.append(new Arc(  9,-13, 10, 13,16*205,16*130,QPen(QPen::darkBlue,1)));
+
+  Ports.append(new Port(-30, 0));
+  Ports.append(new Port( 30, 0));
+
+  x1 = -30; y1 =-13;
+  x2 =  30; y2 =  5;
+
+  tx = x1+4;
+  ty = y2+4;
+  Model = "Bond";
+  Name  = "Line";
+
+  Props.append(new Property("L", "3 mm", true,
+		QObject::tr("length of the wire")));
+  Props.append(new Property("D", "50 um", true,
+		QObject::tr("diameter of the wire")));
+  Props.append(new Property("H", "2 mm", true,
+		QObject::tr("height above ground plane")));
+  Props.append(new Property("rho", "0.022e-6", false,
+		QObject::tr("specific resistance of the metal")));
+  Props.append(new Property("mur", "1", false,
+		QObject::tr("relative permeability of the metal")));
+  Props.append(new Property("Model", "FREESPACE", false,
+	QObject::tr("bond wire model")+" [Freespace, Mirror, Descharles]"));
+  Props.append(new Property("Subst", "Subst1", true,
+		QObject::tr("substrate")));
+  Props.append(new Property("Temp", "26.85", false,
+		QObject::tr("simulation temperature in degree Celsius")));
+}
+
+Component* BondWire::newOne()
+{
+  return new BondWire();
+}
+
+Element* BondWire::info(QString& Name, char* &BitmapFile, bool getNewOne)
+{
+  Name = QObject::tr("Bond Wire");
+  BitmapFile = "bondwire";
+
+  if(getNewOne)  return new BondWire();
+  return 0;
+}
---
+++ qucs/components/twistedpair.cpp
@@ -0,0 +1,84 @@
+/***************************************************************************
+                              twistedpair.cpp
+                             -----------------
+    begin                : Dec 19 2006
+    copyright            : (C) 2006 by Michael Margraf
+    email                : michael.margraf@alumni.tu-berlin.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "twistedpair.h"
+
+
+TwistedPair::TwistedPair()
+{
+  Description = QObject::tr("twisted pair transmission line");
+
+  Arcs.append(new Arc(-25,-36, 18, 38,16*230, 16*68,QPen(QPen::darkBlue,1)));
+  Arcs.append(new Arc(-25, -2, 18, 38, 16*62, 16*68,QPen(QPen::darkBlue,1)));
+
+  Arcs.append(new Arc(-17,-36, 18, 38,16*242, 16*56,QPen(QPen::darkBlue,1)));
+  Arcs.append(new Arc(-17, -2, 18, 38, 16*62, 16*56,QPen(QPen::darkBlue,1)));
+
+  Arcs.append(new Arc( -9,-36, 18, 38,16*242, 16*56,QPen(QPen::darkBlue,1)));
+  Arcs.append(new Arc( -9, -2, 18, 38, 16*62, 16*56,QPen(QPen::darkBlue,1)));
+
+  Arcs.append(new Arc( -1,-36, 18, 38,16*242, 16*56,QPen(QPen::darkBlue,1)));
+  Arcs.append(new Arc( -1, -2, 18, 38, 16*62, 16*56,QPen(QPen::darkBlue,1)));
+
+  Arcs.append(new Arc(  7,-36, 18, 38,16*242, 16*68,QPen(QPen::darkBlue,1)));
+  Arcs.append(new Arc(  7, -2, 18, 38, 16*50, 16*68,QPen(QPen::darkBlue,1)));
+
+  Arcs.append(new Arc(-40,-10, 20, 33, 16*32, 16*58,QPen(QPen::darkBlue,2)));
+  Arcs.append(new Arc(-40,-23, 20, 33,16*270, 16*58,QPen(QPen::darkBlue,2)));
+  Arcs.append(new Arc( 20,-10, 20, 33, 16*90, 16*58,QPen(QPen::darkBlue,2)));
+  Arcs.append(new Arc( 20,-23, 20, 33,16*212, 16*58,QPen(QPen::darkBlue,2)));
+
+  Ports.append(new Port(-30,-10));
+  Ports.append(new Port( 30,-10));
+  Ports.append(new Port( 30, 10));
+  Ports.append(new Port(-30, 10));
+
+  x1 = -30; y1 =-12;
+  x2 =  30; y2 = 12;
+
+  tx = x1+4;
+  ty = y2+4;
+  Model = "TWIST";
+  Name  = "Line";
+
+  Props.append(new Property("d", "0.5 mm", true,
+		QObject::tr("diameter of conductor")));
+  Props.append(new Property("D", "0.8 mm", true,
+		QObject::tr("diameter of wire (conductor and insulator)")));
+  Props.append(new Property("er", "4", false,
+		QObject::tr("dielectric constant of insulator")));
+  Props.append(new Property("T", "100", false,
+		QObject::tr("twists per length in 1/m")));
+}
+
+TwistedPair::~TwistedPair()
+{
+}
+
+Component* TwistedPair::newOne()
+{
+  return new TwistedPair();
+}
+
+Element* TwistedPair::info(QString& Name, char* &BitmapFile, bool getNewOne)
+{
+  Name = QObject::tr("Twisted-Pair");
+  BitmapFile = "twistedpair";
+
+  if(getNewOne)  return new TwistedPair();
+  return 0;
+}
