44fd797baf5e9869877061346d250a06952d8c76
ela <ela>
2007-02-05 19:46:46 +0000

    2007-02-05  Stefan Jahn  <stefan@lkcc.org>
            * microstrip/mscross.cpp: Applied asymmetric corrections for
            the microstrip cross junction model.  There is still an open
            issue about the formulas...

--- qucs-core/src/components/microstrip/mscross.cpp
+++ qucs-core/src/components/microstrip/mscross.cpp
@@ -18,7 +18,7 @@
  * the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
  * Boston, MA 02110-1301, USA.  
  *
- * $Id: mscross.cpp,v 1.5 2007/02/04 18:08:52 ela Exp $
+ * $Id: mscross.cpp,v 1.6 2007/02/05 19:46:47 ela Exp $
  *
  */
 
@@ -75,8 +75,42 @@ void mscross::initAC (void) {
   allocMatrixMNA ();
 }
 
-void mscross::calcAC (nr_double_t f) {
-  setMatrixY (calcMatrixY (f));
+void mscross::calcAC (nr_double_t frequency) {
+  setMatrixY (calcMatrixY (frequency));
+}
+
+nr_double_t mscross::capCorrection (nr_double_t W, nr_double_t f) {
+  substrate * subst = getSubstrate ();
+  nr_double_t er = subst->getPropertyDouble ("er");
+  nr_double_t h  = subst->getPropertyDouble ("h");
+  nr_double_t t  = subst->getPropertyDouble ("t");
+  char * SModel = getPropertyString ("MSModel");
+  char * DModel = getPropertyString ("MSDispModel");
+  nr_double_t Zl1, Er1, Zl2, Er2;
+  nr_double_t ZlEff, ErEff, WEff;
+  msline::analyseQuasiStatic (W, h, t, 9.9, SModel, ZlEff, ErEff, WEff);
+  msline::analyseDispersion  (W, h, 9.9, ZlEff, ErEff, f, DModel,
+                              Zl1, Er1);
+  msline::analyseQuasiStatic (W, h, t, er, SModel, ZlEff, ErEff, WEff);
+  msline::analyseDispersion  (W, h, er, ZlEff, ErEff, f, DModel,
+                              Zl2, Er2);
+  return Zl1 / Zl2 * sqrt (Er2 / Er1);
+}
+
+nr_double_t mscross::calcCap (nr_double_t W1, nr_double_t h, nr_double_t W2) {
+  nr_double_t W1h = W1 / h;
+  nr_double_t W2h = W2 / h;
+  nr_double_t X = log10 (W1h) * (86.6 * W2h - 30.9 * sqrt (W2h) + 367) + 
+    cubic (W2h) + 74 * W2h + 130;
+  return 1e-9 * W1 * 0.25 * (4 * X * pow (W1h, -1.0 / 3.0) - 60 +
+			     1 / W2h / 2 - 0.375 * W1h * (1 - W2h));
+}
+
+nr_double_t mscross::calcInd (nr_double_t W1, nr_double_t h, nr_double_t W2) {
+  nr_double_t W1h = W1 / h;
+  nr_double_t W2h = W2 / h;
+  nr_double_t Y = 165.6 * W2h + 31.2 * sqrt (W2h) - 11.8 * sqr (W2h);
+  return 1e-9 * h * (Y * W1h - 32 * W2h + 3) * pow (W1h, -1.5);
 }
 
 matrix mscross::calcMatrixY (nr_double_t f) {
@@ -85,62 +119,35 @@ matrix mscross::calcMatrixY (nr_double_t f) {
   nr_double_t W3 = getPropertyDouble ("W3");
   nr_double_t W4 = getPropertyDouble ("W4");
   substrate * subst = getSubstrate ();
-  nr_double_t er = subst->getPropertyDouble ("er");
   nr_double_t h  = subst->getPropertyDouble ("h");
-  nr_double_t t  = subst->getPropertyDouble ("t");
-  nr_double_t W1h = W1 / h;
-  nr_double_t W2h = W2 / h;
-  nr_double_t X, Y;
-  nr_double_t C1, C2, C3, C4, L1, L2, L3;
+  nr_double_t W1h = (W1 + W3) / 2 / h;
+  nr_double_t W2h = (W2 + W4) / 2 / h;
+  nr_double_t C1, C2, C3, C4, L1, L2, L3, L4, L5;
 
-  // original model
-  X = log10 (W1h) * (86.6 * W2h - 30.9 * sqrt (W2h) + 367) + 
-    cubic (W2h) + 74 * W2h + 130;
-  C1 = C2 = C3 = C4 = 1e-9 * W1 * (0.25 * X * pow (1 / W1h, 1.0 / 3.0) - 60 +
-				   1 / W2h / 2 - 0.375 * W1h * (1 - W2h));
-  Y = 165.6 * W2h + 31.2 * sqrt (W2h) - 11.8 * sqr (W2h);
-  L1 = 1e-9 * h * (Y * W1h - 32 * W2h + 3) * pow (1 / W1h, 1.5);
-  L2 = 1e-9 * h * (Y * W2h - 32 * W1h + 3) * pow (1 / W2h, 1.5);
-  L3 = 1e-9 * h * (5 * W2h * cos (M_PI / 2 * (1.5 - W1h)) -
+  // apply asymmetric modifications of original model
+  C1 = calcCap (W1, h, (W2 + W4) / 2);
+  C2 = calcCap (W2, h, (W1 + W3) / 2);
+  C3 = calcCap (W3, h, (W4 + W2) / 2);
+  C4 = calcCap (W4, h, (W3 + W1) / 2);
+
+  L1 = calcInd (W1, h, (W2 + W4) / 2);
+  L2 = calcInd (W2, h, (W1 + W3) / 2);
+  L3 = calcInd (W3, h, (W4 + W2) / 2);
+  L4 = calcInd (W4, h, (W3 + W1) / 2);
+
+  L5 = 1e-9 * h * (5 * W2h * cos (M_PI / 2 * (1.5 - W1h)) -
 		   (1 + 7 / W1h ) / W2h - 337.5);
 
-  // L3 correction
-  L3 = L3 * 0.8;
+  // center inductance correction
+  L5 = L5 * 0.8;
 
   // capacitance corrections
-  char * SModel = getPropertyString ("MSModel");
-  char * DModel = getPropertyString ("MSDispModel");
-  nr_double_t Zl1, Er1, Zl2, Er2;
-  nr_double_t ZlEff, ErEff, WEff;
-  msline::analyseQuasiStatic (W1, h, t, 9.9, SModel, ZlEff, ErEff, WEff);
-  msline::analyseDispersion  (W1, h, 9.9, ZlEff, ErEff, f, DModel,
-                              Zl1, Er1);
-  msline::analyseQuasiStatic (W1, h, t, er, SModel, ZlEff, ErEff, WEff);
-  msline::analyseDispersion  (W1, h, er, ZlEff, ErEff, f, DModel,
-                              Zl2, Er2);
-  C1 = C1 * Zl1 / Zl2 * sqrt (Er2 / Er1);
-  msline::analyseQuasiStatic (W2, h, t, 9.9, SModel, ZlEff, ErEff, WEff);
-  msline::analyseDispersion  (W2, h, 9.9, ZlEff, ErEff, f, DModel,
-                              Zl1, Er1);
-  msline::analyseQuasiStatic (W2, h, t, er, SModel, ZlEff, ErEff, WEff);
-  msline::analyseDispersion  (W2, h, er, ZlEff, ErEff, f, DModel,
-                              Zl2, Er2);
-  C2 = C2 * Zl1 / Zl2 * sqrt (Er2 / Er1);
-  msline::analyseQuasiStatic (W3, h, t, 9.9, SModel, ZlEff, ErEff, WEff);
-  msline::analyseDispersion  (W3, h, 9.9, ZlEff, ErEff, f, DModel,
-                              Zl1, Er1);
-  msline::analyseQuasiStatic (W3, h, t, er, SModel, ZlEff, ErEff, WEff);
-  msline::analyseDispersion  (W3, h, er, ZlEff, ErEff, f, DModel,
-                              Zl2, Er2);
-  C3 = C3 * Zl1 / Zl2 * sqrt (Er2 / Er1);
-  msline::analyseQuasiStatic (W4, h, t, 9.9, SModel, ZlEff, ErEff, WEff);
-  msline::analyseDispersion  (W4, h, 9.9, ZlEff, ErEff, f, DModel,
-                              Zl1, Er1);
-  msline::analyseQuasiStatic (W4, h, t, er, SModel, ZlEff, ErEff, WEff);
-  msline::analyseDispersion  (W4, h, er, ZlEff, ErEff, f, DModel,
-                              Zl2, Er2);
-  C4 = C4 * Zl1 / Zl2 * sqrt (Er2 / Er1);
+  C1 = C1 * capCorrection (W1, f);
+  C2 = C2 * capCorrection (W2, f);
+  C3 = C3 * capCorrection (W3, f);
+  C4 = C4 * capCorrection (W4, f);
 
+  // compute admittance matrix
   complex yc1 = rect (0, 2 * M_PI * f * C1);
   complex yc2 = rect (0, 2 * M_PI * f * C2);
   complex yc3 = rect (0, 2 * M_PI * f * C3);
@@ -148,19 +155,21 @@ matrix mscross::calcMatrixY (nr_double_t f) {
   complex yl1 = 1 / rect (0, 2 * M_PI * f * L1);
   complex yl2 = 1 / rect (0, 2 * M_PI * f * L2);
   complex yl3 = 1 / rect (0, 2 * M_PI * f * L3);
-  matrix M (6);
-  M.set (NODE_1, NODE_1, yl1 + yc1);
-  M.set (NODE_2, NODE_2, yl2 + yc2);
-  M.set (NODE_3, NODE_3, yl1 + yc3);
-  M.set (NODE_4, NODE_4, yl2 + yc4);
-  M.set (NODE_1, NODE_5, -yl1); M.set (NODE_5, NODE_1, -yl1);
-  M.set (NODE_3, NODE_5, -yl1); M.set (NODE_5, NODE_3, -yl1);
-  M.set (NODE_2, NODE_6, -yl2); M.set (NODE_6, NODE_2, -yl2);
-  M.set (NODE_4, NODE_6, -yl2); M.set (NODE_6, NODE_4, -yl2);
-  M.set (NODE_5, NODE_6, -yl3); M.set (NODE_6, NODE_5, -yl2);
-  M.set (NODE_5, NODE_5, yl1 + yl1 + yl3);
-  M.set (NODE_6, NODE_6, yl2 + yl2 + yl3);
-  return M;
+  complex yl4 = 1 / rect (0, 2 * M_PI * f * L4);
+  complex yl5 = 1 / rect (0, 2 * M_PI * f * L5);
+  matrix Y (6);
+  Y.set (NODE_1, NODE_1, yl1 + yc1);
+  Y.set (NODE_2, NODE_2, yl2 + yc2);
+  Y.set (NODE_3, NODE_3, yl3 + yc3);
+  Y.set (NODE_4, NODE_4, yl4 + yc4);
+  Y.set (NODE_1, NODE_5, -yl1); Y.set (NODE_5, NODE_1, -yl1);
+  Y.set (NODE_3, NODE_5, -yl3); Y.set (NODE_5, NODE_3, -yl3);
+  Y.set (NODE_2, NODE_6, -yl2); Y.set (NODE_6, NODE_2, -yl2);
+  Y.set (NODE_4, NODE_6, -yl4); Y.set (NODE_6, NODE_4, -yl4);
+  Y.set (NODE_5, NODE_6, -yl5); Y.set (NODE_6, NODE_5, -yl5);
+  Y.set (NODE_5, NODE_5, yl1 + yl3 + yl5);
+  Y.set (NODE_6, NODE_6, yl2 + yl4 + yl5);
+  return Y;
 }
 
 void mscross::initTR (void) {
