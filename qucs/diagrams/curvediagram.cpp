de4ae46cfe293415cd2527d848a5b0d20c6b262b
ela <ela@b5b04e8c-4942-46c9-ab4f-83783d557d1c>
2007-08-16 13:46:05 +0000

    *** empty log message ***
    git-svn-id: https://qucs.svn.sourceforge.net/svnroot/qucs/trunk@1312 b5b04e8c-4942-46c9-ab4f-83783d557d1c

--- qucs/diagrams/curvediagram.cpp
+++ qucs/diagrams/curvediagram.cpp
@@ -15,6 +15,10 @@
  *                                                                         *
  ***************************************************************************/
 
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
 #include <math.h>
 #include <float.h>
 #ifdef __MINGW32__
