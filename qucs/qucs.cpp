f44780c15591a11a682d1ebddf0d563657ec4bf4
ela <ela>
2008-01-02 20:59:11 +0000

    2008-01-02  Stefan Jahn  <stefan@lkcc.org>
            * triac.cpp (Triac), thyristor.cpp (Thyristor), diac.cpp (Diac):
            Added new components.

--- qucs/qucs.cpp
+++ qucs/qucs.cpp
@@ -341,7 +341,8 @@ pInfoFunc nonlinearComps[] =
    &MOSFET::info, &MOSFET::info_p, &MOSFET::info_depl,
    &MOSFET_sub::info, &MOSFET_sub::info_p, &MOSFET_sub::info_depl,
    &OpAmp::info, &hicumL2p1::info, &HBT_X::info, &EqnDefined::info,
-   &mod_amp::info, &hic2_full::info, 0};
+   &mod_amp::info, &hic2_full::info, &Diac::info, &Triac::info,
+   &Thyristor::info, 0};
 
 pInfoFunc digitalComps[] =
   {&Digi_Source::info, &Logical_Inv::info, &Logical_OR::info,
