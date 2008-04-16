19aeb8121461f9d05a89f8990011c4808815ed9d
ela <ela@b5b04e8c-4942-46c9-ab4f-83783d557d1c>
2008-04-16 18:31:36 +0000

    2008-04-16  Stefan Jahn  <stefan@lkcc.org>
            * dialogs/digisettingsdialog.cpp (DigiSettingsDialog): Started to
            add settings for pre-compiled digital simulation modules.
    
    
    git-svn-id: https://qucs.svn.sourceforge.net/svnroot/qucs/trunk@1520 b5b04e8c-4942-46c9-ab4f-83783d557d1c

--- qucs/qucs_he.ts
+++ qucs/qucs_he.ts
@@ -614,6 +614,22 @@
         <source>Error</source>
         <translation type="unfinished">שגיאה</translation>
     </message>
+    <message>
+        <source>Digital Simulation Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Simulation</source>
+        <translation type="unfinished">סימולציה</translation>
+    </message>
+    <message>
+        <source>Precompile Module</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Libraries:</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>DisplayDialog</name>
--- qucs/qucs_jp.ts
+++ qucs/qucs_jp.ts
@@ -638,6 +638,22 @@
         <source>Error</source>
         <translation>エラー</translation>
     </message>
+    <message>
+        <source>Digital Simulation Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Simulation</source>
+        <translation type="unfinished">シミュレーション</translation>
+    </message>
+    <message>
+        <source>Precompile Module</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Libraries:</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>DisplayDialog</name>
