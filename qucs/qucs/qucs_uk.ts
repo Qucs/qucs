28de6afb63f9a0ca1d3e0119da8d81704dc341e0
ela <ela@b5b04e8c-4942-46c9-ab4f-83783d557d1c>
2008-10-28 19:32:05 +0000

    * added first draft of unified NMOS/PMOS EKV model
    git-svn-id: https://qucs.svn.sourceforge.net/svnroot/qucs/trunk@1605 b5b04e8c-4942-46c9-ab4f-83783d557d1c

--- qucs/qucs/qucs_uk.ts
+++ qucs/qucs/qucs_uk.ts
@@ -6402,6 +6402,10 @@ Usage:  qucsedit [-r] file
         <source>material parameter for temperature model</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>EPFL-EKV MOS 2.6 verilog device</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>QucsApp</name>
