949410d59f2d4f8448e248fbed6c2f53a4413771
ela <ela@b5b04e8c-4942-46c9-ab4f-83783d557d1c>
2010-10-23 16:24:53 +0000

    2010-10-23  Stefan Jahn  <stefan@lkcc.org>
            * symbolwidget.cpp (createSymbol): Adjusted symbol for new HICUM
            models.
    
    
    git-svn-id: https://qucs.svn.sourceforge.net/svnroot/qucs/trunk@1783 b5b04e8c-4942-46c9-ab4f-83783d557d1c

--- qucs/qucs-lib/symbolwidget.cpp
+++ qucs/qucs-lib/symbolwidget.cpp
@@ -262,7 +262,8 @@ int SymbolWidget::createSymbol(const QString& Lib_, const QString& Comp_)
   }
   else if(Comp == "hicumL2V2p1" || Comp == "hic2_full" ||
 	  Comp == "hic0_full" || Comp == "hicumL0V1p2" ||
-	  Comp == "hicumL2V2p23") {
+	  Comp == "hicumL2V2p23" || Comp == "hicumL2V2p24" ||
+	  Comp == "hicumL0V1p3") {
     // normal bipolar
     Lines.append(new Line(-10,-15,-10, 15,QPen(QPen::darkBlue,3)));
     Lines.append(new Line(-30,  0,-10,  0,QPen(QPen::darkBlue,2)));
