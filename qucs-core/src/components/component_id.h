beadeb1ccf301d367bb5d1b063524b52c169feed
ela <ela>
2010-11-06 18:47:59 +0000

    2010-10-23  Stefan Jahn  <stefan@lkcc.org>
            * hicumL0V1p2g.va: Added new HICUM L0 v1.2g compact model.

--- qucs-core/src/components/component_id.h
+++ qucs-core/src/components/component_id.h
@@ -18,7 +18,7 @@
  * the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
  * Boston, MA 02110-1301, USA.  
  *
- * $Id: component_id.h,v 1.68 2010/10/23 15:29:12 ela Exp $
+ * $Id: component_id.h,v 1.69 2010/11/06 18:47:59 ela Exp $
  *
  */
 
@@ -149,7 +149,7 @@ enum circuit_type {
   CIR_MESFET,
   CIR_EKV26MOS,
   CIR_hicumL0V1p2,
-  CIR_hicumL0V1p3,
+  CIR_hicumL0V1p2g,
   CIR_hicumL2V2p23,
   CIR_hicumL2V2p24,
   CIR_photodiode,
