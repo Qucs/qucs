1f9835c75a5051dd3bec4ef50334f50f5594339e
ela <ela>
2010-11-06 18:58:14 +0000

    2010-10-23  Stefan Jahn  <stefan@lkcc.org>
            * hicumL0V1p2g.cpp (hicumL0V1p2g): Added new HICUM/L0 v1.2g model.

--- qucs/qucs/qucs_ca.ts
+++ qucs/qucs/qucs_ca.ts
@@ -7161,10 +7161,6 @@ Use:  qucsedit [-r] fitxer
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>HICUM Level 0 v1.3 verilog device</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>high-injection roll-off current</source>
         <translation type="unfinished"></translation>
     </message>
@@ -7181,23 +7177,35 @@ Use:  qucsedit [-r] fitxer
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Bandgap parameter of ver</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Bandgap parameter of vef</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Specific recombination current at the BC barrier for high forward injection</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>npn HICUM L0 v1.3</source>
+        <source>HICUM Level 0 v1.2g verilog device</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>pnp HICUM L0 v1.3</source>
+        <source>TC of avalanche prefactor, identical to alfav of Hicum/L2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>TC of avalanche exponential factor, identical to alqav of Hicum/L2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bandgap TC parameter of ver</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bandgap TC parameter of vef</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>npn HICUM L0 v1.2g</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>pnp HICUM L0 v1.2g</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
