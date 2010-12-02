224306a654f0d0fc69902eccce00b8b4b9126e3f
Frans Schreuder <fransschreuder@gmail.com>
2010-12-02 09:11:19 +0100

    Added a readme about this repository

--- qucs/qucs/qucs_tr.ts
+++ qucs/qucs/qucs_tr.ts
@@ -7083,6 +7083,14 @@ Kullanım:  qucsedit [-r] kütük
         <source>pnp HICUM L0 v1.2g</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>microchip lange coupler</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Microstrip Lange Coupler</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>QucsActions</name>
