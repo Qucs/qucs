6663635cf9dabe59348c0ccdcb7fe2f01c8354ae
Frans Schreuder <fransschreuder@gmail.com>
2011-05-12 09:55:37 +0200

    added microstrip lange coupler to translation files

--- qucs/qucs/qucs_pt.ts
+++ qucs/qucs/qucs_pt.ts
@@ -7221,6 +7221,14 @@ Usage:  qucsedit [-r] file
         <source>pnp HICUM L0 v1.3</source>
         <translation type="unfinished"></translation>
     </message>
+    <message>
+        <source>microstrip lange coupler</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Microstrip Lange Coupler</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 <context>
     <name>QucsApp</name>
