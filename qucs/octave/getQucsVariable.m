37379a86cd9c3ac26cb0e8f77acdfc17c8c94602
Frans Schreuder <info@usbpicprog.org>
2013-01-02 11:57:11 +0100

    Merged changes from svn trunk into qucs-qt4-qt3support (r1845 by crobarcro)

--- qucs/octave/getQucsVariable.m
+++ qucs/octave/getQucsVariable.m
@@ -1,3 +1,30 @@
+function data = getQucsVariable(dataSet,name)
+% Get the specified variable from Qucs data set
+%
+% Syntax
+%
+% data = getQucsVariable(dataSet,name)
+%
+% Input
+%
+% dataSet - a qucs dataset in the format produced by loadQucsDataSet.m
+%
+% name - a string containing the name of the variable to be 
+%   extracted from the data set
+%
+% Output
+%
+% data - vector of values containing the variable data
+%
+%
+% Example
+%
+% [x] = getQucsVariable (data, "out.v")
+% x = 
+%   1 + 0.5i
+%   2 + 1.5i
+%
+
 % -*- texinfo -*-
 % @deftypefn {Function File} {[@var{x}] =} getQucsVariable (@var{dataset}, @var{varname})
 % Load the vector @var{x} from a Qucs dataset @var{dataset} with the variable name @var{varname}.
@@ -14,17 +41,18 @@
 % Copyright 2010 (C) 2010 by Eberhard Gamm <eberhard.gamm@gmx.de>
 % Published under GNU General Public License (GPL V2). No warranty at all.
 
-function data = getQucsVariable(dataSet,name)
-% Get variable from data set
-
-data = [];
-if isempty(dataSet)
+  data = [];
+  if isempty(dataSet)
     error('Data set is empty');
-end
-for i=1:numel(dataSet)
+  end
+
+  for i=1:numel(dataSet)
     if strcmp(dataSet(i).name,name)
-        data = dataSet(i).data;
-        return;
+      data = dataSet(i).data;
+      return;
     end
+  end
+
+  error(['Variable ' name ' not found in data set']);
+
 end
-error(['Variable ' name ' not found in data set']);
--- qucs/octave/loadQucsVariable.m
+++ qucs/octave/loadQucsVariable.m
@@ -1,3 +1,31 @@
+function [x] = loadQucsVariable(filename, varName)
+% Load the specified variable from Qucs data set saved to disk
+%
+% Syntax
+%
+% x = loadQucsVariable(filename, varName)
+%
+% Input
+%
+% filename - a string containing the file name containing the 
+%  Qucs output
+%
+% varName - a string containing the name of the variable to be
+%   extracted from the data set
+%
+% Output
+%
+% x - vector of values containing the requested variable data
+%
+%
+% Example
+%
+% [x] = loadQucsVariable ("actest.dat", "out.v")
+% x = 
+%   1 + 0.5i
+%   2 + 1.5i
+%
+
 % -*- texinfo -*-
 % @deftypefn {Function File} {[@var{x}] =} loadQucsVariable (@var{filename}, @var{varname})
 % Read the vector @var{x} from a Qucs dataset file @var{filename}.
@@ -17,7 +45,8 @@
 % Copyright 2010 by Michael Margraf (michael.margraf@alumni.tu-berlin.de)
 % Published under GNU General Public License (GPL V2). No warranty at all.
 
-function [x] = loadQucsVariable(filename, varName)
     x = [];
     data = loadQucsDataSet(filename);
     x = getQucsVariable(data, varName);
+    
+end
