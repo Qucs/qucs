aba451d1e554ca2e3574b88a28a22a731b56726f
Frans Schreuder <info@usbpicprog.org>
2013-01-02 11:57:11 +0100

    Merged changes from svn trunk into qucs-qt4-qt3support (r1845 by crobarcro)

--- qucs/qucs/octave/loadQucsDataSet.m
+++ qucs/qucs/octave/loadQucsDataSet.m
@@ -1,82 +1,118 @@
+function dataSet = loadQucsDataSet(dataSetFile)
+% Load a dataset produced by the Qucs circuit simulator.
+%
+% Syntax
+%
+% dataSet = loadQucsDataSet(dataSetFile)
+%
+% Input
+%
+%  dataSetFile - name of the data file produced by Qucs.
+%
+% Output
+%
+%  dataSet - an array of structures containing the information from the
+%    Qucs data file. Each structure contains the following fields:
+%
+%    name       | The name of the variable associated with the data in the 
+%               | 'data' field of the structure, e.g. 
+%               |
+%    nameDep    | The name of the dependent variable associated with the
+%               | data, e.g. for a time series simulation this will be
+%               | 'time' and another structure will hold the time data as a
+%               | variable.
+%               | 
+%    dep        | Value of 0 or 1 specifying if the data in the data field 
+%               | is dependent on another variable. 
+%               |
+%    data       | Vector of values containing the data for the specified
+%               | variable
+%
+
 % -*- texinfo -*-
 % @deftypefn {Function File} {[@var{data}] =} loadQucsDataSet (@var{filename})
 % Loads the Qucs dataset from the Qucs dataset file @var{filename}.
 % @end deftypefn
 
 % Copyright 2010 (C) 2010 by Eberhard Gamm <eberhard.gamm@gmx.de>
+% Modified  2012 Richard Crozier
 % Published under GNU General Public License (GPL V2). No warranty at all.
 
-function dataSet = loadQucsDataSet(dataSetFile)
-% Load Qucs data set
+    dataSet = [];
+    fid = fopen(dataSetFile,'r');
+    if fid < 0
+        fprintf(1,'Data set %s not found\n',dataSetFile);
+	    return;
+    end
 
-dataSet = [];
-fid = fopen(dataSetFile,'r');
-if fid < 0
-    fprintf(1,'Data set %s not found\n',dataSetFile);
-	return;
-end
+    error = 0;
+    idata = 0;
+    idx = 0;
+    while ~feof(fid) && ~error
+        line = fgetl(fid);
+        if ~ischar(line)
+            break;
+        end
+        if isempty(line)
+            break;
+        end
+        ll = length(line);
+        if line(1) == '<'
+            if strcmp(line(1:min(7,ll)),'<indep ')
+%             [name,len,cnt] = sscanf(line(8:end-1),'%s %d','C');
+                out = textscan(line(8:end-1),'%s %d');
+                if numel(out) ~= 2
+                    fprintf(1,'Invalid independent variable in data set %s\n',dataSetFile);
+                    error = 1;
+                    break;
+                end
+                name = out{1}{1};
+                numdatapoints = out{2};
+                idx = idx + 1;
+                dataSet(idx).name = name;
+                dataSet(idx).nameDep = '-';
+                dataSet(idx).dep = 0;
+                dataSet(idx).data = zeros(1,numdatapoints);
+                idata = 1;
+            end
+            if strcmp(line(1:min(5,ll)),'<dep ')
+%             [name,nameDep,cnt] = sscanf(line(6:end-1),'%s %s','C');
+                out = textscan(line(5:end-1),'%s %s');
+                if numel(out) ~= 2
+                    fprintf(1,'Invalid dependent variable in data set %s\n',dataSetFile);
+                    error = 1;
+                    break;
+                end
+                name = out{1}{1};
+                nameDep = out{2}{1};
+                idx = idx + 1;
+                dataSet(idx).name = name;
+                dataSet(idx).nameDep = nameDep;
+                dataSet(idx).dep = 1;
+                dataSet(idx).data = zeros(1,numdatapoints);
+                idata = 1;
+            end
+            if strcmp(line(1:min(8,ll)),'</indep>') || strcmp(line(1:min(6,ll)),'</dep>')
+                dataSet(idx).data = dataSet(idx).data(1:idata-1);
+                dataSet(idx).len = idata - 1;
+                idata = 0;
+            end
+        else
+            if idata > 0
+                [val,cnt] = sscanf(line,'%f');
+                if cnt ~= 1
+                    fprintf(1,'Invalid data in data set %s\n',dataSetFile);
+                    error = 1;
+                    break;
+                end
+                if idata > length(dataSet(idx).data)
+                    dataSet(idx).data = [dataSet(idx).data zeros(1,10000)];
+                end
+                dataSet(idx).data(idata) = val;
+                idata = idata + 1;
+            end
+        end
+    end
+    fclose(fid);
 
-error = 0;
-idata = 0;
-idx = 0;
-while ~feof(fid) && ~error
-    line = fgetl(fid);
-    if ~ischar(line)
-        break;
-    end
-    if isempty(line)
-        break;
-    end
-    ll = length(line);
-    if line(1) == '<'
-        if strcmp(line(1:min(7,ll)),'<indep ')
-            [name,len,cnt] = sscanf(line(8:end-1),'%s %d','C');
-            if cnt ~= 2
-                fprintf(1,'Invalid independent variable in data set %s\n',dataSetFile);
-                error = 1;
-                break;
-                error
-            end
-            idx = idx + 1;
-            dataSet(idx).name = name;
-            dataSet(idx).nameDep = '-';
-            dataSet(idx).dep = 0;
-            dataSet(idx).data = zeros(1,10000);
-            idata = 1;
-        end
-        if strcmp(line(1:min(5,ll)),'<dep ')
-            [name,nameDep,cnt] = sscanf(line(6:end-1),'%s %s','C');
-            if cnt ~= 2
-                fprintf(1,'Invalid dependent variable in data set %s\n',dataSetFile);
-                error = 1;
-                break;
-            end
-            idx = idx + 1;
-            dataSet(idx).name = name;
-            dataSet(idx).nameDep = nameDep;
-            dataSet(idx).dep = 1;
-            dataSet(idx).data = zeros(1,10000);
-            idata = 1;
-        end
-        if strcmp(line(1:min(8,ll)),'</indep>') || strcmp(line(1:min(6,ll)),'</dep>')
-            dataSet(idx).data = dataSet(idx).data(1:idata-1);
-            dataSet(idx).len = idata - 1;
-            idata = 0;
-        end
-    else
-        if idata > 0
-            [val,cnt] = sscanf(line,'%f');
-            if cnt ~= 1
-                fprintf(1,'Invalid data in data set %s\n',dataSetFile);
-                error = 1;
-                break;
-            end
-            if idata > length(dataSet(idx).data)
-                dataSet(idx).data = [dataSet(idx).data zeros(1,10000)];
-            end
-            dataSet(idx).data(idata) = val;
-            idata = idata + 1;
-        end
-    end
 end
-fclose(fid);
--- qucs/qucs/octave/showQucsDataSet.m
+++ qucs/qucs/octave/showQucsDataSet.m
@@ -1,3 +1,6 @@
+function showQucsDataSet(dataSet)
+% Dssplay the contents of Qucs data set to the command line
+
 % -*- texinfo -*-
 % @deftypefn {Function File} showQucsDataSet (@var{dataset})
 % Shows the content of the Qucs dataset @var{dataset}.
@@ -5,20 +8,19 @@
 
 % Copyright 2010 (C) 2010 by Eberhard Gamm <eberhard.gamm@gmx.de>
 % Published under GNU General Public License (GPL V2). No warranty at all.
-
-function showQucsDataSet(dataSet)
-% Show contents of data set
-
-if isempty(dataSet)
-    fprintf(1,'\nData set is empty.\n\n');
-else
-    fprintf(1,'\nContents of data set:\n');
-    fprintf(1,'------------------------------------------------------------\n');
-    fprintf(1,'Variable                 Dependency                   Length\n');
-    fprintf(1,'------------------------------------------------------------\n');
-    for i = 1:length(dataSet)
-        fprintf(1,'%-24s %-24s %10d\n',dataSet(i).name,...
-            dataSet(i).nameDep,dataSet(i).len);
+    
+	if isempty(dataSet)
+        fprintf(1,'\nData set is empty.\n\n');
+    else
+        fprintf(1,'\nContents of data set:\n');
+        fprintf(1,'------------------------------------------------------------\n');
+        fprintf(1,'Variable                 Dependency                   Length\n');
+        fprintf(1,'------------------------------------------------------------\n');
+        for i = 1:length(dataSet)
+            fprintf(1,'%-24s %-24s %10d\n',dataSet(i).name,...
+                dataSet(i).nameDep,dataSet(i).len);
+        end
+        fprintf(1,'------------------------------------------------------------\n\n');
     end
-    fprintf(1,'------------------------------------------------------------\n\n');
+
 end
