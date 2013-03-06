65c9aa5021e9000099a886a9e2ee5ebf4c94a584
Frans Schreuder <fransschreuder@gmail.com>
2013-03-06 14:54:36 +0100

    : object.h: added some doxygen comments, no changes to logic http://sourceforge.net/p/qucs/code/1871/

--- qucs-core/src/object.h
+++ qucs-core/src/object.h
@@ -7,20 +7,26 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this package; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
- * Boston, MA 02110-1301, USA.  
+ * Boston, MA 02110-1301, USA.
  *
- * $Id$
+ * $Id: object.h 1871 2013-03-06 12:53:05Z crobarcro $
  *
  */
+
+ /*! \file object.h
+  *
+  * Contains the definition of the generic object class.
+  *
+  */
 
 #ifndef __OBJECT_H__
 #define __OBJECT_H__
@@ -34,6 +40,13 @@ class property;
 class variable;
 class vector;
 
+/*! \class object
+ * \brief generic object class.
+ *
+ * Generic object class from which many Qucs classes are
+ * inheirited.
+ *
+ */
 class object
 {
  public:
