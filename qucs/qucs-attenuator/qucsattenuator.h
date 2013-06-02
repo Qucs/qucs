5ed5d044660002acf44083cbcfe5c973c7545ab2
cnovak <clemens@familie-novak.net>
2013-06-02 11:03:29 +0200

    QDialog->QMainWindow

--- qucs/qucs-attenuator/qucsattenuator.h
+++ qucs/qucs-attenuator/qucsattenuator.h
@@ -14,7 +14,7 @@
 #ifndef QUCSATTENUATOR_H
 #define QUCSATTENUATOR_H
 
-#include <QDialog>
+#include <QMainWindow>
 #include <QLabel>
 
 #include "attenuatorfunc.h"
@@ -37,7 +37,7 @@ struct tQucsSettings
 
 extern struct tQucsSettings QucsSettings;
 
-class QucsAttenuator : public QDialog
+class QucsAttenuator : public QMainWindow
 {
  Q_OBJECT
  public:
