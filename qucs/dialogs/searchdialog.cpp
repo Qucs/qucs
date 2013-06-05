fa373229d06e4833f220dfb65f438a1a9b01026c
Guilherme Brondani Torri <guitorri@gmail.com>
2013-06-05 12:05:46 +0200

    [qucs-qt4] ported dialogs/searchdialog
    Removed Qt3Support classes.
    Fixed the search funcionality
    TODO find already start from cursor, remove checkbox? start from top?

--- qucs/dialogs/searchdialog.cpp
+++ qucs/dialogs/searchdialog.cpp
@@ -19,17 +19,15 @@
 #include "textdoc.h"
 #include "qucs.h"
 
-#include <q3hbox.h>
-#include <qlabel.h>
-#include <qlayout.h>
-#include <qcheckbox.h>
-#include <qlineedit.h>
-#include <qtabwidget.h>
-#include <q3vgroupbox.h>
-#include <qpushbutton.h>
-#include <qmessagebox.h>
-//Added by qt3to4:
-#include <Q3VBoxLayout>
+#include <QLabel>
+#include <QCheckBox>
+#include <QLineEdit>
+#include <QGroupBox>
+#include <QPushButton>
+#include <QMessageBox>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QDebug>
 
 
 SearchDialog::SearchDialog(QucsApp *App_)
@@ -37,35 +35,45 @@ SearchDialog::SearchDialog(QucsApp *App_)
 {
   App = App_;
 
-  Q3VBoxLayout *all = new Q3VBoxLayout(this);
+  QVBoxLayout *all = new QVBoxLayout(this);
   all->setMargin(5);
 
-  Q3VGroupBox *g1 = new Q3VGroupBox(tr("Text to search for"), this);
+  QGroupBox *g1 = new QGroupBox(tr("Text to search for"));
+  QVBoxLayout *vbox1 = new QVBoxLayout;
+  SearchEdit = new QLineEdit();
+  vbox1->addWidget(SearchEdit);   
+  g1->setLayout(vbox1);
+  
   all->addWidget(g1);
-  SearchEdit = new QLineEdit(g1);
 
-  ReplaceGroup = new Q3VGroupBox(tr("Text to replace with"), this);
+  ReplaceGroup = new QGroupBox(tr("Text to replace with"));
+  QVBoxLayout *vbox2 = new QVBoxLayout;
+  ReplaceEdit = new QLineEdit();
+  vbox2->addWidget(ReplaceEdit);
+  ReplaceGroup->setLayout(vbox2);
+  
   all->addWidget(ReplaceGroup);
-  ReplaceEdit = new QLineEdit(ReplaceGroup);
 
-  AskBox = new QCheckBox(tr("Ask before replacing"), this);
+  AskBox = new QCheckBox(tr("Ask before replacing"));
   all->addWidget(AskBox);
 
-  PositionBox = new QCheckBox(tr("From cursor position"), this);
+  PositionBox = new QCheckBox(tr("From cursor position"));
   all->addWidget(PositionBox);
-  CaseBox = new QCheckBox(tr("Case sensitive"), this);
+  CaseBox = new QCheckBox(tr("Case sensitive"));
   all->addWidget(CaseBox);
-  WordBox = new QCheckBox(tr("Whole words only"), this);
+  WordBox = new QCheckBox(tr("Whole words only"));
   all->addWidget(WordBox);
-  BackwardBox = new QCheckBox(tr("Search backwards"), this);
+  BackwardBox = new QCheckBox(tr("Search backwards"));
   all->addWidget(BackwardBox);
 
-  Q3HBox *Buttons = new Q3HBox(this);
-  all->addWidget(Buttons);
-  QPushButton *ButtonSearch = new QPushButton(tr("Search"), Buttons);
+  QHBoxLayout *Buttons = new QHBoxLayout();
+  all->addLayout(Buttons);
+  QPushButton *ButtonSearch = new QPushButton(tr("Search"));
+  Buttons->addWidget(ButtonSearch);
+  QPushButton *ButtonCancel = new QPushButton(tr("Cancel"));
+  Buttons->addWidget(ButtonCancel);
   connect(ButtonSearch, SIGNAL(clicked()), SLOT(slotSearch()));
-  connect(new QPushButton(tr("Cancel"), Buttons),
-          SIGNAL(clicked()), SLOT(reject()));
+  connect(ButtonCancel, SIGNAL(clicked()), SLOT(reject()));
 
   ButtonSearch->setDefault(true);
 }
@@ -78,12 +86,12 @@ SearchDialog::~SearchDialog()
 void SearchDialog::initSearch(bool replace)
 {
   if(replace) {
-    setCaption(tr("Replace Text"));
+    setWindowTitle(tr("Replace Text"));
     AskBox->setHidden(false);
     ReplaceGroup->setHidden(false);
   }
   else {
-    setCaption(tr("Search Text"));
+    setWindowTitle(tr("Search Text"));
     AskBox->hide();
     ReplaceGroup->hide();
   }
@@ -102,46 +110,54 @@ void SearchDialog::searchText(bool fromCursor, int Offset)
 {
   TextDoc *Doc = (TextDoc*)App->DocumentTab->currentPage();
 
+  //FIXME : the find already searches from cursor
   int Line=0, Column=0, count=0, i;
   if(fromCursor) {
-    //Doc->getCursorPosition(&Line, &Column);
     Line = Doc->textCursor().blockNumber();
     Column = Doc->textCursor().columnNumber();
   }
   else if(BackwardBox->isChecked())
-    Line = Doc->document()->blockCount();//Doc->paragraphs();
+    Line = Doc->document()->blockCount();
 
   if(!BackwardBox->isChecked())
     Column += Offset;
 
   if(SearchEdit->text().isEmpty())
     return;
-  /* TODO
-  while(Doc->find(SearchEdit->text(), CaseBox->isChecked(),
-         WordBox->isChecked(), !BackwardBox->isChecked(), &Line, &Column)) {
 
-    count++;
-    if(AskBox->isHidden())  // search only ?
-      return;
-
-    i = QMessageBox::Yes;
-    if(AskBox->isChecked()) {
-      i = QMessageBox::information(this,
-             tr("Replace..."), tr("Replace occurrence ?"),
-             QMessageBox::Yes | QMessageBox::Default, QMessageBox::No,
-             QMessageBox::Cancel | QMessageBox::Escape);
-    }
-    switch(i) {
-      case QMessageBox::Yes:
-               Doc->insert(ReplaceEdit->text());
-               Column += ReplaceEdit->text().length();
-               break;
-      case QMessageBox::No:
-               Column += SearchEdit->text().length();
-               break;
-      default: return;
-    }
-  }*/
+  QFlag findFlags = 0;
+  if (CaseBox->isChecked())
+    findFlags = QTextDocument::FindCaseSensitively;
+  if (WordBox->isChecked())
+    findFlags = findFlags | QTextDocument::FindWholeWords;
+  if (BackwardBox->isChecked())
+    findFlags = findFlags | QTextDocument::FindBackward;  
+  
+  while ( Doc->find(SearchEdit->text(), findFlags)) {
+      
+      count++;
+      if(AskBox->isHidden())  // search only ?
+        return;
+      
+      i = QMessageBox::Yes;
+      if(AskBox->isChecked()) {
+        i = QMessageBox::information(this,
+               tr("Replace..."), tr("Replace occurrence ?"),
+               QMessageBox::Yes | QMessageBox::Default, QMessageBox::No,
+               QMessageBox::Cancel | QMessageBox::Escape);
+      }
+      
+      switch(i) {
+        case QMessageBox::Yes:
+                 Doc->insert(ReplaceEdit->text());
+                 Column += ReplaceEdit->text().length();
+                 break;
+        case QMessageBox::No:
+                 Column += SearchEdit->text().length();
+                 break;
+        default: return;
+      }
+  }
 
   if(count == 0)
     QMessageBox::information(this, tr("Search..."),
