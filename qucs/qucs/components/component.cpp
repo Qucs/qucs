b98ca1c3a676e0bfce6af0bc016dfac19d11619d
Guilherme Brondani Torri <guitorri@gmail.com>
2013-07-08 23:36:24 +0200

    fix bug 118 - file components cannot mirror/rotate
    The problem is that the mirrorX and rotated count happens during load
    and at that time the number of Ports ins not available.
    The number of Ports is only available after recreate, (createSymbol) which
    opens subcircuits, verilog and vhdl to count the ports.
    Therefore, we skip the Port count for these components.

--- qucs/qucs/components/component.cpp
+++ qucs/qucs/components/component.cpp
@@ -378,7 +378,8 @@ void Component::print(ViewPainter *p, float FontScale)
 // Rotates the component 90 counter-clockwise around its center
 void Component::rotate()
 {
-  if (Model != "Sub") // skip port count for Subcircuit
+  // Port count only available after recreate, createSymbol
+  if ((Model != "Sub") && (Model !="VHDL") && (Model != "Verilog")) // skip port count
     if(Ports.count() < 1) return;  // do not rotate components without ports
   int tmp, dx, dy;
 
@@ -477,8 +478,9 @@ void Component::rotate()
 // Mirrors the component about the x-axis.
 void Component::mirrorX()
 {
- if (Model != "Sub") // skip port count for Subcircuit
-   if(Ports.count() < 1) return;  // do not rotate components without ports
+  // Port count only available after recreate, createSymbol
+  if ((Model != "Sub") && (Model !="VHDL") && (Model != "Verilog")) // skip port count
+    if(Ports.count() < 1) return;  // do not rotate components without ports
 
   // mirror all lines
   for(Line *p1 = Lines.first(); p1 != 0; p1 = Lines.next()) {
@@ -538,7 +540,8 @@ void Component::mirrorX()
 // Mirrors the component about the y-axis.
 void Component::mirrorY()
 {
-  if (Model != "Sub") // skip port count for Subcircuit
+  // Port count only available after recreate, createSymbol
+  if ((Model != "Sub") && (Model !="VHDL") && (Model != "Verilog")) // skip port count
     if(Ports.count() < 1) return;  // do not rotate components without ports
 
   // mirror all lines
