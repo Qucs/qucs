d6c3ad0bff69af9b3c46bea6fffa3ab8b9c1a719
Vadim Kusnetzov <ra3xdh@gmail.com>
2013-11-15 13:02:04 +0400

    New diagram export dialog is realised.

--- qucs/qucs/dialogs/exportdiagramdialog.h
+++ qucs/qucs/dialogs/exportdiagramdialog.h
@@ -12,8 +12,37 @@ class ExportDiagramDialog : public QDialog
 public:
     explicit ExportDiagramDialog(QWidget *parent = 0);
     
+private:
+    QPushButton* ExportButt;
+    QPushButton* CancelButt;
+    QPushButton* SaveButt;
+
+    QLabel* lblFilename;
+    QLabel *lblResolutionX;
+    QLabel *lblResolutionY;
+
+    QCheckBox* cbResolution;
+
+    QLineEdit* editFilename;
+    QLineEdit* editResolutionX;
+    QLineEdit* editResolutionY;
+
+    QHBoxLayout* lower1;
+    QHBoxLayout* lower2;
+    QHBoxLayout* lower3;
+    QVBoxLayout* top;
+
+public:
+    QString FileToSave();
+    bool OriginalSize();
+    int Xpixels();
+    int Ypixels();
+
 signals:
     
+private slots:
+    void setFileName();
+
 public slots:
     
 };
