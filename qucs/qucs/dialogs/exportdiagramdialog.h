5c47a945cfbfd023aeffc711b21ca1fefd3d8b2b
Frans Schreuder <fransschreuder@gmail.com>
2013-11-28 18:44:03 +0100

    added gpl header to exportdiagramdialog, and removed static home path

--- qucs/qucs/dialogs/exportdiagramdialog.h
+++ qucs/qucs/dialogs/exportdiagramdialog.h
@@ -1,3 +1,19 @@
+/***************************************************************************
+                               exportdiagramdialog.h
+                              ------------------
+    begin                : Thu Nov 28 2013
+    copyright            : (C) 2013 by Vadim Kuznetzov
+    email                : <ra3xdh@gmail.com>
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #ifndef EXPORTDIAGRAMDIALOG_H
 #define EXPORTDIAGRAMDIALOG_H
 
