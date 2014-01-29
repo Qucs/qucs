e951367d7ca35b4543a7e743ed62b82d7656242d
Richard C Crozier <user@example.com>
2014-01-29 16:21:26 +0000

    tidied up boostconverter m-code example

--- examples/external_interface/m-interface/asynchronous_boost_converter_example.m
+++ examples/external_interface/m-interface/asynchronous_boost_converter_example.m
@@ -1,4 +1,6 @@
 % asynchronous_boost_converter_example.m
+%
+% 
 
 cd(fileparts(which('asynchronous_boost_converter_example.m')));
 
@@ -10,9 +12,10 @@ tend = 5e-4;
 
 clear qtr_async1
 
-% qtr_async1 = synctrcircuit('m-test-2.net');
+% create a new asynchronous solver object from a netlist
 qtr_async1 = asynctrcircuit('boostconverter.net');
 
+% initialist the simulation
 qtr_async1.init(tstart, (tend - tstart) / (10 * n));
 
 % get the number of nodes
@@ -23,58 +26,25 @@ M = qtr_async1.getm;
 
 % choose some time points
 T1 = linspace(tstart, tend, n)';
-
+% initialise storage for the solution
 Y1 = zeros(numel(T1), M+N);
+% get the initial solution
 Y1(1, 1:(N+M)) = qtr_async1.getsolution();
-
+% step through time solving the circuit
 for ind = 2:numel(T1)
     
-    fprintf(1, 't: %f\n', T1(ind));
-    
+    % accept the step into the solution history
 	qtr_async1.acceptstep(T1(ind));
     
+    % get the node voltages and currents at the current time
     Y1(ind, 1:(N+M)) = qtr_async1.getsolution();
-    
-%     qtr_async.printx()
 
 end
 
-figure; plotyy(T1, Y1(:,1:N), T1, Y1(:,((N+1):(N+M))));
-
-%% ode test
-
-clear qtr_async2
-
-% dbstop in synctrcircuit.m at 203
-% dbstop in synctrcircuit.m at 218
-% dbstop in synctrcircuit.m at 220
-
-% qtr_async2 = synctrcircuit('m-test-2.net');
-qtr_async2 = synctrcircuit('boostconverter.net');
-
-qtr_async2.init(tstart);
-
-y0 = qtr_async2.getsolution();
-
-odeopts = odeset( 'OutputFcn', @synctrcircuit.odeoutputfcn, ...
-                  'InitialStep', (tend-tstart)/n/2, ...
-                  ... 'MaxStep', (tend-tstart)/n, ...
-                  'Refine', 1, ...
-                  'RelTol', 1e-2);
-
-[T2, Y2] = ode15s( @synctrcircuit.odefcn, [ tstart+(0.001)*tstart, tend ], y0, odeopts, qtr_async2);
-
-figure; plotyy(T2, Y2(:,1:N), T2, Y2(:,((N+1):(N+M))));
-
-
-%% comparison
-
-
-figure; plot(T1, Y1(:,3), 'r', T2, Y2(:,3), 'b');
-
-
-
-
-
-
-
+% plot the node voltages and branch currents
+figure; 
+AX = plotyy(T1 * 1000, Y1(:,1:N), T1 * 1000, Y1(:,((N+1):(N+M))));
+title ('Boost Converter', 'FontSize', 14)
+xlabel('Time (ms)', 'FontSize', 14)
+set(get(AX(1),'Ylabel'),'String','Node Voltages', 'FontSize', 14) 
+set(get(AX(2),'Ylabel'),'String','Branch Currents', 'FontSize', 14) 
