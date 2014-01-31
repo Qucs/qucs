a18b2ec26f7673a8eb35174590ca2892c314e9b2
Richard Crozier <richard.crozier@example.com>
2014-01-31 16:59:40 +0000

    Added Libraries browser tab

--- qucs/qucs-lib/symbolwidget.h
+++ qucs/qucs-lib/symbolwidget.h
@@ -28,44 +28,45 @@
 #include <QMouseEvent>
 #include <QPaintEvent>
 
+#include "element.h"
 
 class QPaintEvent;
 class QSizePolicy;
 
 
-struct Line {
-  Line(int _x1, int _y1, int _x2, int _y2, QPen _style)
-       : x1(_x1), y1(_y1), x2(_x2), y2(_y2), style(_style) {};
-  int   x1, y1, x2, y2;
-  QPen  style;
-};
-
-struct Arc {
-  Arc(int _x, int _y, int _w, int _h, int _angle, int _arclen, QPen _style)
-      : x(_x), y(_y), w(_w), h(_h), angle(_angle),
-	arclen(_arclen), style(_style) {};
-  int   x, y, w, h, angle, arclen;
-  QPen  style;
-};
-
-struct Area {
-  Area(int _x, int _y, int _w, int _h, QPen _Pen,
-	QBrush _Brush = QBrush(Qt::NoBrush))
-	: x(_x), y(_y), w(_w), h(_h), Pen(_Pen), Brush(_Brush) {};
-  int    x, y, w, h;
-  QPen   Pen;
-  QBrush Brush;    // filling style/color
-};
-
-struct Text {
-  Text(int _x, int _y, const QString& _s, QColor _Color = QColor(0,0,0),
-	float _Size = 10.0, float _mCos=1.0, float _mSin=0.0)
-	: x(_x), y(_y), s(_s), Color(_Color), Size(_Size) {};
-  int     x, y;
-  QString s;
-  QColor  Color;
-  float   Size;
-};
+//struct Line {
+//  Line(int _x1, int _y1, int _x2, int _y2, QPen _style)
+//       : x1(_x1), y1(_y1), x2(_x2), y2(_y2), style(_style) {};
+//  int   x1, y1, x2, y2;
+//  QPen  style;
+//};
+//
+//struct Arc {
+//  Arc(int _x, int _y, int _w, int _h, int _angle, int _arclen, QPen _style)
+//      : x(_x), y(_y), w(_w), h(_h), angle(_angle),
+//	arclen(_arclen), style(_style) {};
+//  int   x, y, w, h, angle, arclen;
+//  QPen  style;
+//};
+//
+//struct Area {
+//  Area(int _x, int _y, int _w, int _h, QPen _Pen,
+//	QBrush _Brush = QBrush(Qt::NoBrush))
+//	: x(_x), y(_y), w(_w), h(_h), Pen(_Pen), Brush(_Brush) {};
+//  int    x, y, w, h;
+//  QPen   Pen;
+//  QBrush Brush;    // filling style/color
+//};
+//
+//struct Text {
+//  Text(int _x, int _y, const QString& _s, QColor _Color = QColor(0,0,0),
+//	float _Size = 10.0, float _mCos=1.0, float _mSin=0.0)
+//	: x(_x), y(_y), s(_s), Color(_Color), Size(_Size) {};
+//  int     x, y;
+//  QString s;
+//  QColor  Color;
+//  float   Size;
+//};
 
 
 
@@ -91,8 +92,8 @@ private:
   void  paintEvent(QPaintEvent*);
 
   int  analyseLine(const QString&);
-  bool getIntegers(const QString&,
-       int *i1=0, int *i2=0, int *i3=0, int *i4=0, int *i5=0, int *i6=0);
+
+
   bool getPen  (const QString&, QPen&, int);
   bool getBrush(const QString&, QBrush&, int);
 
@@ -112,4 +113,4 @@ private:
   //Q3PtrList<Text>       Texts;
 };
 
-#endif
+#endif // SYMBOLWIDGET_H
