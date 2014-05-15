bd25fedbf917c6c08d865f667331e0ccaf6339a5
Vadim Kuznetzov <ra3xdh@gmail.com>
2014-05-15 16:51:10 +0400

    Added stub for transfer function definition dialog

---
+++ transferfuncdialog.h
@@ -0,0 +1,19 @@
+#ifndef TRANSFERFUNCDIALOG_H
+#define TRANSFERFUNCDIALOG_H
+
+#include <QWidget>
+#include <QtGui>
+
+class TransferFuncDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit TransferFuncDialog(QWidget *parent = 0);
+    
+signals:
+    
+public slots:
+    
+};
+
+#endif // TRANSFERFUNCDIALOG_H
