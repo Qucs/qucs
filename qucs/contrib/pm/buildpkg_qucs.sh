859586a295d353839d6ef605f97365d386f098b7
Guilherme Brondani Torri <guitorri@gmail.com>
2014-06-29 20:57:05 +0200

    fixes osx package build script

--- qucs/contrib/pm/buildpkg_qucs.sh
+++ qucs/contrib/pm/buildpkg_qucs.sh
@@ -12,29 +12,34 @@
 
 # TODO extract / cd into tarball
 
+# SDK is now handled by the configure.ac
 # set -stdlib=libstdcc++ for OS X 10.6
 # set -stdlib=libc++     for OS X 10.7 and higher
-
 # SDK might me necessary.
-
-STDLIB=libc++
-OSXVER=10.7
-
-SDK=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${OSXVER}.sdk
-
+#STDLIB=libc++
+#OSXVER=10.7
+#SDK=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${OSXVER}.sdk
 # configure source
-CFLAGS="-mmacosx-version-min=$OSXVER -arch x86_64" \
-CXXFLAGS="-stdlib=$STDLIB -mmacosx-version-min=$VER -arch x86_64" \
-LDDFLAGS="-stdlib=$STDLIB" \
-./configure --prefix=/usr/local --disable-asco  --disable-adms
+#CFLAGS="-mmacosx-version-min=$OSXVER -arch x86_64" \
+#CXXFLAGS="-stdlib=$STDLIB -mmacosx-version-min=$VER -arch x86_64" \
+#LDDFLAGS="-stdlib=$STDLIB" \
+
+
+./configure --prefix=/usr/local --disable-adms \
+--with-macosx-sdk=10.6 \
+--with-macos-version-min-required=10.6 \
+--with-macos-version-max-allowed=10.6
+
 
 # build
 make -j 8
 
+# might need to install with sudo, to set correct permissions
 # Install to a separate directory for capture.
 DEST=/tmp/installdir
 mkdir $DEST
-make install DESTDIR=$DEST
+
+sudo make install DESTDIR=$DEST
 
 
 # Simple flat package stamp
@@ -46,7 +51,7 @@ command="pkgbuild \
          --root $DEST \
          --identifier org.qucs.pkg \
          --version 1.0 \
-         --install-location /usr/local \
+         --install-location / \
          ./qucs-${version}-${date}.pkg"
 
 echo "${command}"
