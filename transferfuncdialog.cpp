90dfcb5cea6cefea2e4c1a504f5ce9d86317ef58
Vadim Kusnetzov <ra3xdh@gmail.com>
2014-07-11 07:33:10 +0400

    Added GPL header to all *.cpp and *.h

--- transferfuncdialog.cpp
+++ transferfuncdialog.cpp
@@ -1,3 +1,21 @@
+/***************************************************************************
+                           transferfuncdialog.cpp
+                              ----------------
+    begin                : Wed Apr 10 2014
+    copyright            : (C) 2014 by Vadim Kuznetsov
+    email                : ra3xdh@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
 #include "transferfuncdialog.h"
 
 TransferFuncDialog::TransferFuncDialog(QVector<long double> &a, QVector<long double> &b, QWidget *parent) :
