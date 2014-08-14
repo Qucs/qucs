60637aad1e509d425253cabff9c79cf5b6dc5e47
Vadim Kuznetzov <ra3xdh@gmail.com>
2014-08-14 19:12:36 +0400

    fixed issue #18

--- qucs/qucs.cpp
+++ qucs/qucs.cpp
@@ -1466,10 +1466,12 @@ void QucsApp::slotMenuDelProject()
 
   if(s.isEmpty()) return;
 
-  s = s.left(s.length()-1);  // cut off trailing '/'
-  int i = s.findRev('/');
+  if (s.endsWith(QDir::separator())) {
+      s = s.left(s.length()-1);  // cut off trailing '/'
+  }
+  int i = s.findRev(QDir::separator());
   if(i > 0) s = s.mid(i+1);  // cut out the last subdirectory
-  s = s.left(s.length()-4);  // remove "_prj" from name
+  s.chop(4); // remove "_prj" from name
   deleteProject(d, s);
   readProjects();   // re-reads all projects and inserts them into the ListBox
 }
