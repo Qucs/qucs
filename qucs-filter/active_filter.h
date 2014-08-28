07dca8e16c76b7034cbbf0c23e98725c250c9b7a
Verkhovin Vyacheslav <arhiv6@gmail.com>
2014-08-28 11:41:14 +0700

    Add GPL informations in modifed files

--- qucs-filter/active_filter.h
+++ qucs-filter/active_filter.h
@@ -2,6 +2,15 @@
   copyright: (C) 2010 by Michael Margraf
  ***************************************************************************/
 
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef ACTIVE_FILTER_H
 #define ACTIVE_FILTER_H
 
--- qucs-filter/tl_filter.cpp
+++ qucs-filter/tl_filter.cpp
@@ -2,6 +2,15 @@
   copyright: (C) 2010 by Michael Margraf
  ***************************************************************************/
 
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include "tl_filter.h"
 
 #define  MAX_ERROR  1e-7
