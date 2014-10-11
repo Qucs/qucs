0fe6504528576e41711a548a80ce356ae85469b3
Vadim Kuznetzov <ra3xdh@gmail.com>
2014-10-11 17:12:20 +0400

    Added isSimulation property analysis while generating spice netlist

--- qucs/qucs/schematic_file.cpp
+++ qucs/qucs/schematic_file.cpp
@@ -1975,10 +1975,17 @@ QString Schematic::createSpiceNetlist(QTextStream& stream, int NumPorts)
 
     QString s, Time;
     for(Component *pc = DocComps.first(); pc != 0; pc = DocComps.next()) {
-      if(isAnalog) {
+      if(isAnalog && !(pc->isSimulation)) {
         s = pc->getSpiceNetlist();
+        stream<<s;
       }
-      stream<<s;
+    }
+
+    for(Component *pc = DocComps.first(); pc != 0; pc = DocComps.next()) {
+       if(pc->isSimulation) {
+           s = pc->getSpiceNetlist();
+           stream<<s;
+       }
     }
 
     delete err;
