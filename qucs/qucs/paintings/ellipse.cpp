c52ba0e4843a6006ef6d1c594d37639d1604acc1
yodalee <lc85301@gmail.com>
2014-11-03 02:33:24 +0800

    remove qtgui include in cpp

--- qucs/qucs/paintings/ellipse.cpp
+++ qucs/qucs/paintings/ellipse.cpp
@@ -14,7 +14,6 @@
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
-#include <QtGui>
 #include "ellipse.h"
 #include "filldialog.h"
 #include "schematic.h"
