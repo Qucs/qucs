c3bb40f04b355deac0b32b7c10f620377ba5ac83
yodalee <lc85301@gmail.com>
2014-11-03 23:50:58 +0800

    move Q3layout to Qt4
    Q3HBox->QHBoxLayout
    Q3VBox->QVBoxLayout
    Q3GridLayout->QGridLayout

--- qucs/qucs/components/spicedialog.h
+++ qucs/qucs/components/spicedialog.h
@@ -20,10 +20,6 @@
 
 #include <QDialog>
 #include <QRegExp>
-#include <QDir>
-//Added by qt3to4:
-#include <QTextStream>
-#include <Q3VBoxLayout>
 
 class Schematic;
 class SpiceFile;
@@ -32,13 +28,12 @@ class QPushButton;
 class Q3ListBox;
 class Q3ListBoxItem;
 class QCheckBox;
-class Q3VBoxLayout;
+class QVBoxLayout;
 class QProcess;
 class QRegExpValidator;
 class QComboBox;
 class QucsApp;
-
-
+class QTextStream;
 
 class SpiceDialog : public QDialog {
    Q_OBJECT
@@ -73,13 +68,14 @@ protected slots:
 private:
   bool loadSpiceNetList(const QString&);
 
-  Q3VBoxLayout *all;   // the mother of all widgets
+  QVBoxLayout *all;   // the mother of all widgets
   QRegExpValidator  *Validator, *ValRestrict;
   QRegExp     Expr;
   Q3ListBox    *NodesList, *PortsList;
   QCheckBox   *FileCheck, *SimCheck;
   QLineEdit   *FileEdit, *CompNameEdit;
-  QPushButton *ButtBrowse, *ButtEdit, *ButtAdd, *ButtRemove;
+  QPushButton *ButtBrowse, *ButtEdit, *ButtAdd, *ButtRemove,
+              *ButtOK, *ButtApply, *ButtCancel;
   QComboBox   *PrepCombo;
   SpiceFile   *Comp;
   Schematic   *Doc;
