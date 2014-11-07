bedcea4e4753ccad286dd2c85b793a92b8ade99d
yodalee <lc85301@gmail.com>
2014-11-08 02:15:36 +0800

    remove schematic include

--- qucs/qucs/qucsdoc.cpp
+++ qucs/qucs/qucsdoc.cpp
@@ -15,10 +15,6 @@
  *                                                                         *
  ***************************************************************************/
 
-#ifdef HAVE_CONFIG_H
-# include <config.h>
-#endif
-
 #include <QFileInfo>
 
 #include "qucsdoc.h"
