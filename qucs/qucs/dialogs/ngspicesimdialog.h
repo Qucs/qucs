2770c1bd7b35cc093535cae0f7d5ac3779950bcf
Vadim Kuznetzov <ra3xdh@gmail.com>
2014-11-09 14:59:34 +0400

    Added GPL header and config.h

--- qucs/qucs/dialogs/ngspicesimdialog.h
+++ qucs/qucs/dialogs/ngspicesimdialog.h
@@ -1,3 +1,20 @@
+/***************************************************************************
+                           ngspicesimdialog.cpp
+                             ----------------
+    begin                : Sun Nov 9 2014
+    copyright            : (C) 2014 by Vadim Kuznetsov
+    email                : ra3xdh@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #ifndef NGSPICESIMDIALOG_H
 #define NGSPICESIMDIALOG_H
 
