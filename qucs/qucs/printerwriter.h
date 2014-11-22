5a8397c671adef77416aa9cd02cc295af57512f4
yodalee <lc85301@gmail.com>
2014-11-23 03:06:24 +0800

    initial commit of printer class structure

---
+++ qucs/qucs/printerwriter.h
@@ -0,0 +1,36 @@
+/*
+ * printerwriter.h - declaraction of writer to printer
+ *
+ * Copyright (C) 2014, Yodalee, lc85301@gmail.com
+ *
+ * This file is part of Qucs
+ *
+ * Qucs is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Qucs.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef PRINTERWRITER_H_
+#define PRINTERWRITER_H_ value
+
+class QucsDoc;
+
+class PrinterWriter
+{
+public:
+  PrinterWriter ();
+  virtual ~PrinterWriter ();
+  virtual void print(QucsDoc *);
+};
+
+#endif
