07eaadb7aba81eeb579866d142c560809db930b8
yodalee <lc85301@gmail.com>
2014-11-23 03:06:24 +0800

    initial commit of printer class structure

---
+++ qucs/imagewriter.cpp
@@ -0,0 +1,36 @@
+/*
+ * imagewriter.cpp - implementation of writer to image
+ *
+ * Copyright (C) 2014, Yodalee, lc85301@gmail.com
+ *
+ * This file is part of Qucs
+ *
+ * Qucs is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Qucs.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "imagewriter.h"
+
+ImageWriter::ImageWriter()
+{
+}
+
+ImageWriter::~ImageWriter()
+{
+}
+
+void
+ImageWriter::print(QWidget *doc)
+{
+}
---
+++ qucs/printerwriter.cpp
@@ -0,0 +1,41 @@
+/*
+ * printerwriter.cpp - implementation of writer to printer
+ *
+ * Copyright (C) 2014, Yodalee, lc85301@gmail.com
+ *
+ * This file is part of Qucs
+ *
+ * Qucs is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Qucs.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "printerwriter.h"
+#include "qucsdoc.h"
+#include "schematic.h"
+#include "textdoc.h"
+
+#include <QWidget>
+
+PrinterWriter::PrinterWriter()
+{
+}
+
+PrinterWriter::~PrinterWriter()
+{
+}
+
+void
+PrinterWriter::print(QucsDoc *doc)
+{
+}
