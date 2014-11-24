47e3817856638ce4b872ec57853bc67fd1bcc58f
yodalee <lc85301@gmail.com>
2014-11-24 21:12:45 +0800

    basic implementation in printerwriter

--- qucs/qucs/printerwriter.cpp
+++ qucs/qucs/printerwriter.cpp
@@ -21,21 +21,66 @@
  */
 
 #include "printerwriter.h"
-#include "qucsdoc.h"
 #include "schematic.h"
 #include "textdoc.h"
 
-#include <QWidget>
+#include <QPrinter>
+#include <QPainter>
+#include <QPrintDialog>
 
 PrinterWriter::PrinterWriter()
 {
+  //default setting
+  Printer = new QPrinter(QPrinter::HighResolution);
+  Printer->setOptionEnabled(QPrinter::PrintSelection, true);
+  Printer->setOptionEnabled(QPrinter::PrintPageRange, false);
+  Printer->setOptionEnabled(QPrinter::PrintToFile, true);
+  Printer->setColorMode(QPrinter::Color);
+  Printer->setFullPage(true);
+
+  fitToPage = false;
 }
 
 PrinterWriter::~PrinterWriter()
 {
+  delete Printer;
 }
 
 void
-PrinterWriter::print(QucsDoc *doc)
+PrinterWriter::print(QWidget *doc)
 {
+  QPrintDialog *dialog = new QPrintDialog(Printer, 0);
+  dialog->setWindowTitle(QObject::tr("Print Document"));
+  dialog->addEnabledOption(QAbstractPrintDialog::PrintSelection);
+
+
+  if (doc->inherits("QPlainTextEdit"))
+  {
+    if (dialog->exec() == QDialog::Accepted) {
+       static_cast<QPlainTextEdit *>(doc)->print(Printer);
+    }
+  }
+  else {
+    Printer->setOrientation(QPrinter::Landscape);
+
+    if (dialog->exec() == QDialog::Accepted)
+    {
+      QPainter Painter(Printer);
+      if(!Painter.device()) {     // valid device available ?
+        return;
+      }
+      for (int z = Printer->numCopies(); z > 0; --z) {
+        if (Printer->aborted()) {
+          break;
+        }
+
+        static_cast<Schematic *>(doc)->print(Printer, &Painter,
+                Printer->printRange() == QPrinter::AllPages, fitToPage);
+        if (z > 1 && !Printer->newPage()) {
+          return;
+        }
+      }
+    }
+  }
+  delete dialog;
 }
