686eaa2d058bef83ef9090efa6a5072ca67722ae
Guilherme Brondani Torri <guitorri@gmail.com>
2015-01-19 14:10:39 +0100

    namespace: set constants as static in constants.h

--- qucs-core/src/constants.h
+++ qucs-core/src/constants.h
@@ -45,62 +45,62 @@ Qucs physical constants
 */
 
 /*!\brief speed of light in vacuum (\f$c_0\f$) */
-const double C0   = 299792458.0;
+static const double C0   = 299792458.0;
 /*!\brief magnetic constant of vacuum (\f$\mu_0=4\pi\times10^{-7}\f$) */
-const double  MU0 = 12.566370614e-7;
+static const double  MU0 = 12.566370614e-7;
 /*!\brief Electric constant of vacuum \f$\varepsilon_0\f$*/
-const double E0   = 8.854187817e-12;
+static const double E0   = 8.854187817e-12;
 /*!\brief Wave impedance in vacuum (\f$Z_0=\sqrt{\frac{\mu_0}{\varepsilon_0}}\f$)*/
-const double Z0   = 376.73031346958504364963;
+static const double Z0   = 376.73031346958504364963;
 /*!\brief Planck constant (\f$h\f$)
    \todo Add h bar (\f$h/(2\pi)\f$)
 */
-const double Hp   = 6.626069311e-34;
+static const double Hp   = 6.626069311e-34;
 /*!\brief Absolute 0 in centigrade  */
-const double K    = -273.15;
+static const double K    = -273.15;
 /*!\brief standard temperature      */
-const double T0   = 290;
+static const double T0   = 290;
 
 /*!\brief Boltzmann constant (\f$k_B\f$) */
-const double kB      = 1.380650524e-23;
+static const double kB      = 1.380650524e-23;
 /*!\brief Elementary charge (\f$q_e\f$)  */
-const double Q_e     = 1.6021765314e-19;
+static const double Q_e     = 1.6021765314e-19;
 /*!\brief Boltzmann constant over Elementary charge (\f$k_B/q_e\f$)*/
-const double kBoverQ = 0.86173433260414314916e-4;
+static const double kBoverQ = 0.86173433260414314916e-4;
 /*!\brief Elementary charge over Boltzmann constant (\f$q_e/k_B\f$)*/
-const double QoverkB = 1.16045045690360379713e+4;
+static const double QoverkB = 1.16045045690360379713e+4;
 
 /*!\brief Relative permittivity of Silicon dioxide (Silica) */
-const double ESiO2     = 3.9;
+static const double ESiO2     = 3.9;
 /*!\brief Relative permittivity of Silicon */
-const double ESi       = 11.7;
+static const double ESi       = 11.7;
 /*!\brief relative permittivity of Germanium */
-const double EGe       = 15.8;
+static const double EGe       = 15.8;
 /*!\brief Relative permittivity of Gallium(III) arsenide */
-const double EGaAs     = 13.1;
+static const double EGaAs     = 13.1;
 /*!\brief Intrinsic carrier concentration in 1/m^3 of Silicon */
-const double NiSi      = 1.45e16;
+static const double NiSi      = 1.45e16;
 /*!\brief Intrinsic carrier concentration in 1/m^3 of Germanium */
-const double NiGe      = 2.40e19;
+static const double NiGe      = 2.40e19;
 /*!\brief Intrinsic carrier concentration in 1/m^3 of Gallium(III) arsenide */
-const double NiGaAs    = 9.00e12;
+static const double NiGaAs    = 9.00e12;
 /*!\brief Energy gap at 300K in eV of Silicon */
-const double EgSi      = 1.11;
+static const double EgSi      = 1.11;
 /*!\brief Energy gap at 300K in eV of Schottky
   \todo What kind of metal (ideal, real, gold)?
 */
-const double EgSchottky = 0.69;
+static const double EgSchottky = 0.69;
 /*!\brief Energy gap at 300K in eV of Germanium */
-const double EgGe       = 0.67;
+static const double EgGe       = 0.67;
 /*!\brief Energy gap at 300K in eV of Gallium(III) arsenide */
-const double EgGaAs     = 1.43;
+static const double EgGaAs     = 1.43;
 /*!\brief Energy gap at 0K in eV of Silicon */
-const double Eg0Si      = 1.16;
+static const double Eg0Si      = 1.16;
 
 /*!\brief Gmin
    \todo Define and document
 */
-const double GMin = NR_TINY;
+static const double GMin = NR_TINY;
 
 /**
 @}
