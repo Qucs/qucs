953fe0e60aaf814c1a64d46e9e2ad6743c99066d
Vadim Kuznetzov <ra3xdh@gmail.com>
2015-01-25 10:18:06 +0300

    Added GPL headers

--- qucs/qucs/extsimkernels/xyce.cpp
+++ qucs/qucs/extsimkernels/xyce.cpp
@@ -1,3 +1,21 @@
+/***************************************************************************
+                               xyce.cpp
+                             ----------------
+    begin                : Fri Jan 16 2015
+    copyright            : (C) 2015 by Vadim Kuznetsov
+    email                : ra3xdh@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+
 #include "xyce.h"
 
 Xyce::Xyce(Schematic *sch_, QObject *parent) :
