45ad931dcefdd9dca2591c76d68b9de74cc1d7b1
Guilherme Brondani Torri <guitorri@gmail.com>
2015-01-27 23:09:28 +0100

    remove excessive ;
    it's a drive-by fix, but this kind of stuff is always drive-by (felix)

--- qucs/qucs/paintings/painting.h
+++ qucs/qucs/paintings/painting.h
@@ -25,10 +25,10 @@
 class Painting : public Element  {
 public:
   Painting();
- ~Painting() {};
+ ~Painting() {}
 
-  virtual void getCenter(int&, int &) {};
-  virtual bool getSelected(float, float, float) { return false; };
+  virtual void getCenter(int&, int &) {}
+  virtual bool getSelected(float, float, float) { return false; }
 
 public: // legacy anonymous stuff.
   virtual void setArgs2Int(int, int) {
@@ -46,18 +46,18 @@ public:
   virtual QString save();
   virtual QString saveCpp();
   virtual QString saveJSON();
-  virtual void paint(ViewPainter*) {};
+  virtual void paint(ViewPainter*) {}
   virtual void MouseMoving(Schematic*, int, int, int, int,
-                           Schematic*, int, int, bool) {};
-  virtual bool MousePressing() { return false; };
+                           Schematic*, int, int, bool) {}
+  virtual bool MousePressing() { return false; }
   virtual void Bounding(int&, int&, int&, int&);
-  virtual bool resizeTouched(float, float, float) { return false; };
-  virtual void MouseResizeMoving(int, int, Schematic*) {};
+  virtual bool resizeTouched(float, float, float) { return false; }
+  virtual void MouseResizeMoving(int, int, Schematic*) {}
 
-  virtual void rotate() {};
-  virtual void mirrorX() {};
-  virtual void mirrorY() {};
-  virtual bool Dialog() { return false; };
+  virtual void rotate() {}
+  virtual void mirrorX() {}
+  virtual void mirrorY() {}
+  virtual bool Dialog() { return false; }
 
   QString toPenString (int);
   QString toBrushString (int);
