dc7a069c1f1e8a4ca24f3da3ca513b5fcc0145d9
Vadim Kuznetzov <ra3xdh@gmail.com>
2015-02-15 18:52:59 +0300

    Added iProbe support

--- qucs/qucs/components/component.cpp
+++ qucs/qucs/components/component.cpp
@@ -717,6 +717,16 @@ QString Component::getSpiceNetlist(bool isXyce)
     return s;
 }
 
+QString Component::getExpression(bool isXyce)
+{
+    return QString("");
+}
+
+QString Component::getProbeVariable(bool isXyce)
+{
+    return QString("");
+}
+
 // -------------------------------------------------------
 QString Component::verilogCode(int)
 {
