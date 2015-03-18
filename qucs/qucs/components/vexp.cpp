b4e0555458afab77817e161a9589c7f8a849bead
MikeBrinson <mbrin72043@yahoo.co.uk>
2015-03-18 17:11:31 +0000

    VExp and IExp sources added to spice4qucs

--- qucs/qucs/components/vexp.cpp
+++ qucs/qucs/components/vexp.cpp
@@ -2,20 +2,27 @@
                                 vexp.cpp
                                ----------
     begin                : Fri Mar 9 2007
-    copyright            : (C) 2007 by Gunther Kraut
+    copyright          : (C) 2007 by Gunther Kraut
     email                : gn.kraut@t-online.de
+    spice4qucs code added  Wed. 18 March 2015
+    copyright          : (C) 2015 by Mike Brinson
+    email                : mbrin72043@yahoo.co.uk
+
  ***************************************************************************/
 
 /***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
+ *                                                                                                                     *
+ *   This program is free software; you can redistribute it and/or modify      *
+ *   it under the terms of the GNU General Public License as published by   *
+ *   the Free Software Foundation; either version 2 of the License, or           *
+ *   (at your option) any later version.                                                                          *
+ *                                                                                                                   *
  ***************************************************************************/
 
 #include "vexp.h"
+#include "node.h"
+#include "misc.h"
+#include "extsimkernels/spicecompat.h"
 
 
 vExp::vExp()
@@ -26,7 +33,7 @@ vExp::vExp()
   Arcs.append(new Arc(-12,-12, 24, 24,     0, 16*360,QPen(Qt::darkBlue,2)));
   Lines.append(new Line(-30,  0,-12,  0,QPen(Qt::darkBlue,2)));
   Lines.append(new Line( 30,  0, 12,  0,QPen(Qt::darkBlue,2)));
-  Lines.append(new Line( 18,  5, 18, 11,QPen(Qt::red,1)));
+  
   Lines.append(new Line( 21,  8, 15,  8,QPen(Qt::red,1)));
   Lines.append(new Line(-18,  5,-18, 11,QPen(Qt::black,1)));
 
@@ -51,6 +58,7 @@ vExp::vExp()
   tx = x1+4;
   ty = y2+4;
   Model = "Vexp";
+  SpiceModel = "V";
   Name  = "V";
 
   Props.append(new Property("U1", "0 V", true,
@@ -85,4 +93,24 @@ Element* vExp::info(QString& Name, char* &BitmapFile, bool getNewOne)
 
   if(getNewOne)  return new vExp();
   return 0;
+}
+
+QString vExp::spice_netlist(bool isXyce)
+{
+    QString s = spicecompat::check_refdes(Name,SpiceModel);
+    foreach(Port *p1, Ports) {
+        QString nam = p1->Connection->Name;
+        if (nam=="gnd") nam = "0";
+        s += " "+ nam;   // node names
+    }
+
+   QString U1= spicecompat::normalize_value(Props.at(0)->Value);
+   QString U2 = spicecompat::normalize_value(Props.at(1)->Value);
+   QString T1 = spicecompat::normalize_value(Props.at(2)->Value);
+   QString T2 = spicecompat::normalize_value(Props.at(3)->Value);
+   QString Tr = spicecompat::normalize_value(Props.at(4)->Value);
+   QString Tf = spicecompat::normalize_value(Props.at(5)->Value);
+
+    s += QString(" DC 0 EXP(%1 %2 %3 %4 %5 %6) AC 0\n").arg(U1).arg(U2).arg(T1).arg(Tr).arg(T2).arg(Tf);
+    return s;
 }
