0227e2f5ab9077ea428ea59105d80db05c8a4aa7
MikeBrinson <mbrin72043@yahoo.co.uk>
2015-03-19 15:50:12 +0000

    Vpulse bug corrected and added Ipulse.

--- qucs/qucs/components/ipulse.cpp
+++ qucs/qucs/components/ipulse.cpp
@@ -2,9 +2,12 @@
                                 ipulse.cpp
                                ------------
     begin                : Sat Sep 18 2004
-    copyright            : (C) 2004 by Michael Margraf
+    copyright          : (C) 2004 by Michael Margraf
     email                : michael.margraf@alumni.tu-berlin.de
- ***************************************************************************/
+    spice4qucs code added  Thurs. 19 March 2015
+    copyright          : (C) 2015 by Vadim Kusnetsov (Vadim Kuznetsov (ra3xdh@gmail.com) 
+                                           and Mike Brinson (mbrin72043@yahoo.co.uk
+ **************************************************************************/
 
 /***************************************************************************
  *                                                                         *
@@ -16,7 +19,9 @@
  ***************************************************************************/
 
 #include "ipulse.h"
-
+#include "node.h"
+#include "misc.h"
+#include "extsimkernels/spicecompat.h"
 
 iPulse::iPulse()
 {
@@ -79,4 +84,37 @@ Element* iPulse::info(QString& Name, char* &BitmapFile, bool getNewOne)
 
   if(getNewOne)  return new iPulse();
   return 0;
+}
+
+QString iPulse::spice_netlist(bool isXyce)
+{
+    QString s = spicecompat::check_refdes(Name,SpiceModel);
+
+    foreach(Port *p1, Ports) {
+        QString nam = p1->Connection->Name;
+        if (nam=="gnd") nam = "0";
+        s += " "+ nam;   // node names
+    }
+
+    double T1,T2, TrVal, TfVal, Pw,fac,Per;
+    QString unit;
+
+    QString VL = spicecompat::normalize_value(Props.at(0)->Value); // VL
+    QString VH = spicecompat::normalize_value(Props.at(1)->Value); // VH
+   QString Tr = spicecompat::normalize_value(Props.at(4)->Value); // Tr 
+    QString Tf = spicecompat::normalize_value(Props.at(5)->Value); // Tf
+
+    misc::str2num(Props.at(2)->Value,T1,unit,fac); // Td
+    T1 *= fac;
+    misc::str2num(Props.at(3)->Value,T2,unit,fac); //T2
+    Pw=T2*fac-T1;
+    misc::str2num(Props.at(4)->Value,TrVal,unit,fac); //Value of Tr
+    Pw = Pw - TrVal*fac;
+    misc::str2num(Props.at(5)->Value,TfVal,unit,fac); //Value of Tf   
+    Pw = Pw - TfVal*fac;
+    Per = 1.0e9*Pw;
+
+    s += QString(" DC 0 PULSE(-%1 -%2 %3 %4 %5 %6 %7) AC 0\n").arg(VL).arg(VH).arg(T1).arg(Tr).arg(Tf).arg(Pw).arg(Per);
+
+    return s;
 }
