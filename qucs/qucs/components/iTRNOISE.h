ef83017f81f6930c17150d1bf1b601e3cbc9f4cf
MikeBrinson <mbrin72043@yahoo.co.uk>
2015-04-01 17:08:29 +0100

    Added more V and I generators.

---
+++ qucs/qucs/components/iTRNOISE.h
@@ -0,0 +1,37 @@
+/***************************************************************************
+                         iTRNOISE.h  -  description
+                   --------------------------------------
+    begin                    : Fri Mar 9 2007
+    copyright              : (C) 2007 by Gunther Kraut
+    email                     : gn.kraut@t-online.de
+    spice4qucs code added  Tue. 31 March 2015
+    copyright              : (C) 2015 by Mike Brinson
+    email                    : mbrin72043@yahoo.co.uk
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ #ifndef ITRNOISE_H
+#define ITRNOISE_H
+
+#include "component.h"
+
+class iTRNOISE : public Component {
+public:
+  iTRNOISE();
+  ~iTRNOISE();
+  Component* newOne();
+  static Element* info(QString&, char* &, bool getNewOne=false);
+protected:
+  QString netlist();
+  QString spice_netlist(bool isXyce = false);
+};
+
+#endif // ITRNOISE_H
