59bcb19114ebcbc3ed4c1efa53b02a4161d16f66
yodalee <lc85301@gmail.com>
2015-04-17 01:08:10 +0800

    initial commit of schematicview

---
+++ qucs/qucs/schematicview.cpp
@@ -0,0 +1,111 @@
+/*
+ * schematicview.cpp - implementation view of schematic
+ *
+ * Copyright (C) 2014, Yodalee, lc85301@gmail.com
+ *
+ * This file is part of Qucs
+ *
+ * Qucs is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Qucs.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <QAction>
+#include <QFileInfo>
+
+#include "schematicview.h"
+#include "qucs.h"
+
+SchematicView::SchematicView(QucsApp *App_, const QString &Name_)
+  : QGraphicsView(App_), QucsDoc(App_, Name_)
+{
+}
+
+SchematicView::~SchematicView()
+{
+}
+
+// ---------------------------------------------------
+void SchematicView::becomeCurrent(bool update)
+{
+  QString *ps;
+  emit signalCursorPosChanged(0, 0);
+
+  // update appropriate menu entry
+  bool symbolMode = 0;
+  if (symbolMode) {
+    if (DocName.right(4) == ".sym") {
+      App->symEdit->setText(tr("Edit Text"));
+      App->symEdit->setStatusTip(tr("Edits the Text"));
+      App->symEdit->setWhatsThis(tr("Edit Text\n\nEdits the text file"));
+    }
+    else {
+      App->symEdit->setText(tr("Edit Schematic"));
+      App->symEdit->setStatusTip(tr("Edits the schematic"));
+      App->symEdit->setWhatsThis(tr("Edit Schematic\n\nEdits the schematic"));
+    }
+  }
+  else {
+    App->symEdit->setText(tr("Edit Circuit Symbol"));
+    App->symEdit->setStatusTip(tr("Edits the symbol for this schematic"));
+    App->symEdit->setWhatsThis(
+	tr("Edit Circuit Symbol\n\nEdits the symbol for this schematic"));
+  }
+
+  if(symbolMode) {
+  }
+  else {
+  }
+}
+
+// ---------------------------------------------------
+void SchematicView::setName (const QString& Name_)
+{
+  DocName = Name_;
+  QFileInfo Info (DocName);
+  if (App) App->DocumentTab->setTabText(App->DocumentTab->indexOf(this), Info.fileName());
+  QString base = Info.completeBaseName ();
+  QString ext = Info.suffix();
+  DataSet = base + ".dat";
+  Script = base + ".m";
+  if (ext != "dpl")
+    DataDisplay = base + ".dpl";
+  else
+    DataDisplay = base + ".sch";
+}
+
+bool SchematicView::load()
+{
+  return false;
+}
+
+int SchematicView::save()
+{
+  return 0;
+}
+
+void SchematicView::print(QPrinter *, QPainter *, bool, bool)
+{
+}
+
+float SchematicView::zoomBy(float)
+{
+}
+
+void SchematicView::showAll()
+{
+}
+
+void SchematicView::showNoZoom()
+{
+}
---
+++ qucs/qucs/schematicview.h
@@ -0,0 +1,61 @@
+/*
+ * schematicview.h - definition view of schematic
+ *
+ * Copyright (C) 2014, Yodalee, lc85301@gmail.com
+ *
+ * This file is part of Qucs
+ *
+ * Qucs is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Qucs.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef SCHEMATICVIEW_H_
+#define SCHEMATICVIEW_H_ value
+
+#include <QGraphicsView>
+#include <QString>
+
+#include "qucsdoc.h"
+
+class QucsApp;
+class QString;
+class QPainter;
+
+class SchematicView : public QGraphicsView, public QucsDoc
+{
+  Q_OBJECT
+public:
+  SchematicView (QucsApp *, const QString &);
+  virtual ~SchematicView ();
+
+  // function that inherited by QucsDoc
+  void setName(const QString&);
+  bool load();
+  int  save();
+  void print(QPrinter*, QPainter*, bool, bool);
+  void becomeCurrent(bool);
+  float zoomBy(float);
+  void showAll();
+  void showNoZoom();
+
+  // schematic-special function
+
+signals:
+  void signalCursorPosChanged(int, int);
+
+protected slots:
+private:
+};
+
+#endif /* SCHEMATICVIEW_H_ */
