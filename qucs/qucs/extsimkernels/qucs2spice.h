bd9b279ea869869da5d4a71c2b101045a0fe0c01
Vadim Kuznetzov <ra3xdh@gmail.com>
2015-04-17 16:19:07 +0300

    Added EDD inside LibComps support

--- qucs/qucs/extsimkernels/qucs2spice.h
+++ qucs/qucs/extsimkernels/qucs2spice.h
@@ -5,19 +5,6 @@
 
 namespace qucs2spice
 {
-    QString convert_rcl(QString line);
-    QString convert_header(QString line);
-    QString convert_diode(QString line,bool xyce=false);
-    QString convert_jfet(QString line, bool xyce=false);
-    QString convert_mosfet(QString line, bool xyce=false);
-    QString convert_bjt(QString line);
-    QString convert_cccs(QString line);
-    QString convert_ccvs(QString line);
-    QString convert_ccs(QString line, bool voltage);
-    QString convert_vccs(QString line);
-    QString convert_vcvs(QString line);
-    QString convert_vcs(QString line, bool voltage);
-    QString convert_dc_src(QString line);
     QString convert_netlist(QString netlist, bool xyce=false);
 }
 
