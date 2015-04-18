14fac563f903514b77ddeccf3b82f8cb9c1d7f86
Vadim Kuznetzov <ra3xdh@gmail.com>
2015-04-18 18:00:00 +0300

    Added gyrator model

--- qucs/qucs/components/gyrator.cpp
+++ qucs/qucs/components/gyrator.cpp
@@ -16,6 +16,8 @@
  ***************************************************************************/
 
 #include "gyrator.h"
+#include "node.h"
+#include "extsimkernels/spicecompat.h"
 
 
 Gyrator::Gyrator()
@@ -73,4 +75,21 @@ Element* Gyrator::info(QString& Name, char* &BitmapFile, bool getNewOne)
 
   if(getNewOne)  return new Gyrator();
   return 0;
+}
+
+QString Gyrator::spice_netlist(bool)
+{
+    QString s;
+    QString R = spicecompat::normalize_value(Props.at(0)->Value);
+    QString n1 = Ports.at(0)->Connection->Name;
+    if (n1=="gnd") n1="0";
+    QString n2 = Ports.at(1)->Connection->Name;
+    if (n2=="gnd") n2="0";
+    QString n3 = Ports.at(2)->Connection->Name;
+    if (n3=="gnd") n3="0";
+    QString n4 = Ports.at(3)->Connection->Name;
+    if (n4=="gnd") n4="0";
+    s +=  QString("B%1_1 %2 %3 I=(1/(%4))*(V(%5)-V(%6))\n").arg(Name).arg(n1).arg(n4).arg(R).arg(n2).arg(n3);
+    s +=  QString("B%1_2 %2 %3 I=-1.0*(1/(%4))*(V(%5)-V(%6))\n").arg(Name).arg(n2).arg(n3).arg(R).arg(n1).arg(n4);
+    return s;
 }
