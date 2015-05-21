eee499e22212fe0971aca457fa980c4d0d666bae
Vadim Kuznetzov <ra3xdh@gmail.com>
2015-05-21 11:36:04 +0300

    Added AC SPICE source

--- qucs/qucs/components/volt_ac_SPICE.cpp
+++ qucs/qucs/components/volt_ac_SPICE.cpp
@@ -1,9 +1,9 @@
 /***************************************************************************
-                               volt_ac.cpp
+                             volt_ac_SPICE.cpp
                               -------------
-    begin                : Sat Aug 23 2003
-    copyright            : (C) 2003 by Michael Margraf
-    email                : michael.margraf@alumni.tu-berlin.de
+    begin                : Thu May 21 2015
+    copyright            : (C) 2015 by Vadim Kuznetsov
+    email                : ra3xdh@gmail.com
  ***************************************************************************/
 
 /***************************************************************************
@@ -15,19 +15,19 @@
  *                                                                         *
  ***************************************************************************/
 
-#include "volt_ac.h"
+#include "volt_ac_SPICE.h"
 #include "node.h"
 #include "misc.h"
 #include "extsimkernels/spicecompat.h"
 
 
-Volt_ac::Volt_ac()
+Vac_SPICE::Vac_SPICE()
 {
-  Description = QObject::tr("ideal ac voltage source");
+  Description = QObject::tr("AC voltage source (SPICE)");
 
-  Arcs.append(new Arc(-12,-12, 24, 24,     0, 16*360,QPen(Qt::darkBlue,2)));
-  Arcs.append(new Arc( -3, -7,  7,  7,16*270, 16*180,QPen(Qt::darkBlue,2)));
-  Arcs.append(new Arc( -3,  0,  7,  7, 16*90, 16*180,QPen(Qt::darkBlue,2)));
+  Arcs.append(new Arc(-12,-12, 24, 24,     0, 16*360,QPen(Qt::red,2)));
+  Arcs.append(new Arc( -3, -7,  7,  7,16*270, 16*180,QPen(Qt::red,2)));
+  Arcs.append(new Arc( -3,  0,  7,  7, 16*90, 16*180,QPen(Qt::red,2)));
   Lines.append(new Line(-30,  0,-12,  0,QPen(Qt::darkBlue,2)));
   Lines.append(new Line( 30,  0, 12,  0,QPen(Qt::darkBlue,2)));
   Lines.append(new Line( 18,  5, 18, 11,QPen(Qt::red,1)));
@@ -42,55 +42,56 @@ Volt_ac::Volt_ac()
 
   tx = x1+4;
   ty = y2+4;
-  Model = "Vac";
+  Model = "Vac_SPICE";
   SpiceModel = "V";
   Name  = "V";
 
-  Props.append(new Property("U", "1 V", true,
-		QObject::tr("peak voltage in Volts")));
-  Props.append(new Property("f", "1 GHz", false,
-		QObject::tr("frequency in Hertz")));
-  Props.append(new Property("Phase", "0", false,
-		QObject::tr("initial phase in degrees")));
-  Props.append(new Property("Theta", "0", false,
-		QObject::tr("damping factor (transient simulation only)")));
+  Props.append(new Property("Vac", "", true,"Expression"));
+  Props.append(new Property("Vac_Line 2", "", false,"Expression"));
+  Props.append(new Property("Vac_Line 3", "", false,"Expression"));
+  Props.append(new Property("Vac_Line 4", "", false,"Expression"));
+  Props.append(new Property("Vac_Line 5", "", false,"Expression"));
 
   rotate();  // fix historical flaw
 }
 
-Volt_ac::~Volt_ac()
+Vac_SPICE::~Vac_SPICE()
 {
 }
 
-Component* Volt_ac::newOne()
+Component* Vac_SPICE::newOne()
 {
-  return new Volt_ac();
+  return new Vac_SPICE();
 }
 
-Element* Volt_ac::info(QString& Name, char* &BitmapFile, bool getNewOne)
+Element* Vac_SPICE::info(QString& Name, char* &BitmapFile, bool getNewOne)
 {
   Name = QObject::tr("ac Voltage Source");
-  BitmapFile = (char *) "ac_voltage";
+  BitmapFile = (char *) "Vac_SPICE";
 
-  if(getNewOne)  return new Volt_ac();
+  if(getNewOne)  return new Vac_SPICE();
   return 0;
 }
 
-QString Volt_ac::spice_netlist(bool isXyce)
+QString Vac_SPICE::spice_netlist(bool)
 {
     QString s = spicecompat::check_refdes(Name,SpiceModel);
     foreach(Port *p1, Ports) {
         QString nam = p1->Connection->Name;
         if (nam=="gnd") nam = "0";
-        s += " "+ nam;   // node names
+        s += " "+ nam+" ";   // node names
     }
 
-    QString volts = spicecompat::normalize_value(Props.at(0)->Value);
-    QString freq = spicecompat::normalize_value(Props.at(1)->Value);
+    QString l0= Props.at(0)->Value;
+    QString l1= Props.at(1)->Value;
+    QString l2= Props.at(2)->Value;
+    QString l3= Props.at(3)->Value;
+    QString l4= Props.at(4)->Value;
 
-    QString theta = Props.at(3)->Value;
-    theta.remove(' ');
-    if (theta.isEmpty()) theta="0";
-    s += QString(" DC 0 SIN(0 %1 %2 0 %3) AC %4\n").arg(volts).arg(freq).arg(theta).arg(volts);
+    if(l0.length()> 0)   s += QString("%1\n").arg(l0);
+    if(l1.length()> 0)   s += QString("%1\n").arg(l1);
+    if(l2.length()> 0)   s += QString("%1\n").arg(l2);
+    if(l3.length()> 0)   s += QString("%1\n").arg(l3);
+    if(l4.length()> 0)   s += QString("%1\n").arg(l4);
     return s;
 }
