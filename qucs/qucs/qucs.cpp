6789ed76c2773064eefeb64587a5d71810d3c33c
Kevin Voet <kevin.voet@gmail.com>
2015-07-02 15:37:59 +0200

    Current DocumentTab is updated after simulation instead of just the schematic.

--- qucs/qucs/qucs.cpp
+++ qucs/qucs/qucs.cpp
@@ -2235,7 +2235,10 @@ void QucsApp::slotAfterSimulation(int Status, SimMessage *sim)
   }
 
   if(!isTextDocument (sim->DocWidget))
-    ((Schematic*)sim->DocWidget)->viewport()->update();
+  {
+    //((Schematic*)sim->DocWidget)->viewport()->update();
+      ((Schematic*)DocumentTab->currentPage())->viewport()->update();
+  }
 
   // Kill the simulation process, otherwise we have 200+++ sims in the background
   if(tunerDia->isVisible())
