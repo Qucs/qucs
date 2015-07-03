Qucs/qucs

commit 6a073cf486f57d7a69f5e8444057e6bcac1590f3

silence warning: assigning field to itself

qucs-core/src/equation.cpp
@@ -520,7 +520,7 @@ void assignment::mul (assignment * f)
     else if (isOne (factor))
     {
         delete factor;
-        body = body;
+        //body = body;
     }
     else
     {
@@ -548,7 +548,7 @@ void assignment::mulref (assignment * f)
     }
     else if (isOne (factor))
     {
-        body = body;
+        //body = body;
     }
     else
     {
@@ -572,12 +572,12 @@ void assignment::add (assignment * f)
     else if (isZero (body))
     {
         delete body;
-        body = factor;
+        //body = factor;
     }
     else if (isZero (factor))
     {
         delete factor;
-        body = body;
+        //body = body;
     }
     else
     {

commit 68c71f1bdb36d4831a38ba49fe3a13492167daca

Fix regression on EDD component.
Error introduced in 6a073cf.
A few lines of code were commented out to silece compiler warnings.
One line should not be commented out.
This commit reverts the situation.

Closes issue #296

qucs-core/src/equation.cpp
@@ -572,7 +572,7 @@ void assignment::add (assignment * f)
     else if (isZero (body))
     {
         delete body;
-        //body = factor;
+        body = factor;
     }
     else if (isZero (factor))
     {