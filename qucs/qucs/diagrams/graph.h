4fef7fced3e811ab0b75445a3030b151584e7833
Felix Salfelder <felix@salfelder.org>
2015-07-07 21:08:59 +0200

    compute marker position in Marker::initText
    move code from Marker::initText into GraphDeque::samplePos.

--- qucs/qucs/diagrams/graph.h
+++ qucs/qucs/diagrams/graph.h
@@ -214,6 +214,7 @@ private: // painting
 public: // marker related
   void createMarkerText() const;
   MarkerPos findSample(std::vector<double>&) const;
+  void samplePos(const_iterator, std::vector<double>& VarPos) const;
   Diagram const* parentDiagram() const{return diagram;}
   void invalidateMarkers();
 private:
