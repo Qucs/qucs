e7d8d6d3e5fee6581ce5f095643bb72840fe81e4
MikeBrinson <mbrin72043@yahoo.co.uk>
2015-09-11 12:16:51 +0100

    Improvement to B source layout

--- qucs/qucs/components/src_eqndef.cpp
+++ qucs/qucs/components/src_eqndef.cpp
@@ -6,10 +6,10 @@
 
 Src_eqndef::Src_eqndef()
 {
-  Description = QObject::tr("Equation defined (B-type) voltage or current source");
+  Description = QObject::tr("Equation defined (B-type) voltage or current source"); 
 
   Arcs.append(new Arc(-14,-14, 28, 28,     0, 16*360,QPen(Qt::red,3)));
-  Texts.append(new Text(7,-12,"Eqn",Qt::red,10.0,0.0,-1.0));
+  Texts.append(new Text(10,-12,"Eqn",Qt::red,10.0,0.0,-1.0));
   Lines.append(new Line(-30,  0,-14,  0,QPen(Qt::darkBlue,2)));
   Lines.append(new Line( 30,  0, 14,  0,QPen(Qt::darkBlue,2)));
   Lines.append(new Line( 18,  5, 18, 11,QPen(Qt::red,1)));
@@ -27,8 +27,16 @@ Src_eqndef::Src_eqndef()
   Model = "B";
   SpiceModel = "B";
   Name  = "B";
+  
+  Props.append(new Property("VI", "0", true,"Expression"));
+  Props.append(new Property("Line 2", "", false,"Expression"));
+  Props.append(new Property("Line 3", "", false,"Expression"));
+  Props.append(new Property("Line 4", "", false,"Expression"));
+  Props.append(new Property("Line 5", "", false,"Expression"));
 
-  Props.append(new Property("V", "0", true,"Expression"));
+
+
+// Props.append(new Property("V", "0", true,"Expression"));
 
   rotate();  // fix historical flaw
 }
@@ -56,6 +64,18 @@ QString Src_eqndef::netlist()
     return QString("");
 }
 
+//QString Src_eqndef::spice_netlist(bool)
+//{
+//    QString s = spicecompat::check_refdes(Name,SpiceModel);
+//    foreach(Port *p1, Ports) {
+//        QString nam = p1->Connection->Name;
+//        if (nam=="gnd") nam = "0";
+//        s += " "+ nam;   // node names
+//    }
+//    s += QString(" %1 = %2 \n").arg(Props.at(0)->Name).arg(Props.at(0)->Value);
+//    return s;
+//}
+
 QString Src_eqndef::spice_netlist(bool)
 {
     QString s = spicecompat::check_refdes(Name,SpiceModel);
@@ -64,7 +84,22 @@ QString Src_eqndef::spice_netlist(bool)
         if (nam=="gnd") nam = "0";
         s += " "+ nam;   // node names
     }
-    s += QString(" %1 = %2 \n").arg(Props.at(0)->Name).arg(Props.at(0)->Value);
+//    s += QString(" %1 = %2 \n").arg(Props.at(0)->Name).arg(Props.at(0)->Value);
+    
+    
+    QString VI  = Props.at(0)-> Name;
+    QString VI2 = Props.at(0)->Value;
+    QString Line_2 = Props.at(1)->Value;
+    QString Line_3 = Props.at(2)->Value;
+    QString Line_4 = Props.at(3)->Value;
+    QString Line_5 = Props.at(4)->Value;
+
+    s += QString(" %1 = %2 \n").arg(VI).arg(VI2);
+    if(  Line_2.length() > 0 )   s += QString("%1\n").arg(Line_2);
+    if(  Line_3.length() > 0 )   s += QString("%1\n").arg(Line_3);
+    if(  Line_4.length() > 0 )   s += QString("%1\n").arg(Line_4);
+    if(  Line_5.length() > 0 )   s += QString("%1\n").arg(Line_5);
+ 
     return s;
 }
 
