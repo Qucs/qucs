72215ed32226b75b9defa8f1665a80206e46263b
Pedro-project <macedo.pp@gmail.com>
2015-10-16 15:44:28 +0100

    Update wprobe.h

--- qucs-core/src/components/wprobe.h
+++ qucs-core/src/components/wprobe.h
@@ -1,24 +1,4 @@
-/*
- * wprobe.h - AC/DC and transient power probe class implementation
- *
- * Copyright (C) [2015], [Pedro Macedo], [macedo.pp@gmail.com]
- *
- * This file is part of Qucs
- *
- * Qucs is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Qucs.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
+//Created by Pedro Macedo
 
 #ifndef __WPROBE_H__
 #define __WPROBE_H__
