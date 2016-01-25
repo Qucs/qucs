6191adad1885042f1ea13cca551f61bfc38d8a37
andresmmera <andresmartinezmera@gmail.com>
2016-01-25 19:31:34 +0100

    Add new lumped elements models
    all added models work with the S-parameters, AC and DC
    simulations and include the noise contribution from the losses.
    A short description of the implemented models was added to the
    technical papers.
    
    1) CAPQ and INDQ models
    
    Probably, the most important figure of merit for real world lumped
    components is the quality factor. The new components INDQ and CAPQ
    implement the lossy inductance/capacitor model, respectively.
    
    The Q-factor is defined as: Q = 1/(2*pi*f*C*Rs) for capacitors and Q =
    2*pi*f*L/Rs for inductances.
    
    The value of the series resistance, Rs, is usually dependent on the
    frequency, so this model also includes three frequency weighting
    profiles.
    
    Reference:
    [1] Bahl. Lumped elements for RF and microwave circuits.
    
    2) Spiral inductor model
    
    A new component for the RF passive palette was added. It is a planar
    spiral inductor. Four types of spiral are supported: circular, square,
    hexagonal and octogonal.
    
    [1] I. Bahl, Fundamentals of RF and Microwave Transistor Amplifiers,
    John Wiley and Sons, 2009.
    [2] Simple Accurate Expressions for Planar Spiral Inductances. S. S.
    Mohan. IEEE Journal of Solid State Circuits.   Vol 34, No 10, October
    1999
    
    3) Planar circular loop inductor
    
    Added a circular loop inductor model.
    
    References:
    [1] I. Bahl, Fundamentals of RF and Microwave Transistor Amplifiers,
    John Wiley and Sons, 2009.
    
    Also, Added a new palette for the RF passive components (isolator,
    coupler, hybrid, capq, indq, circulator, attenuator, RFedd,
    RFedd2 and BiasT)

---
+++ qucs/components/spiralinductor.cpp
@@ -0,0 +1,86 @@
+/*
+* spiralinductor.cpp - Planar spiral inductor class implementation
+*
+* copyright (C) 2016 Andres Martinez-Mera <andresmartinezmera@gmail.com>
+*
+* This is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2, or (at your option)
+* any later version.
+*
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this package; see the file COPYING.  If not, write to
+* the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+* Boston, MA 02110-1301, USA.
+*
+*
+*/
+
+#include "spiralinductor.h"
+
+
+spiralinductor::spiralinductor()
+{
+  Description = QObject::tr("Planar spiral inductor");
+
+  //Spiral
+  Arcs.append(new Arc(-5, 0, 10, 10, -16*90, 16*180,QPen(Qt::darkBlue,3)));
+  Arcs.append(new Arc(-10, -10, 20, 20, 16*90, 16*180,QPen(Qt::darkBlue,3)));
+  Arcs.append(new Arc(-15, -10, 30, 30, -16*90, 16*180,QPen(Qt::darkBlue,3)));
+  Arcs.append(new Arc(-20, -20, 40, 40, 16*90, 16*180,QPen(Qt::darkBlue,3)));
+  Arcs.append(new Arc(-20, -20, 40, 40, 0, 16*90,QPen(Qt::darkBlue,3)));
+
+  Lines.append(new Line(-30,  0, 0,  0,QPen(Qt::black,4)));
+  Lines.append(new Line( 20,  0, 30,  0,QPen(Qt::darkBlue,2)));
+
+  Ports.append(new Port(-30, 0));
+  Ports.append(new Port( 30, 0));
+
+  x1 = -30; y1 =-25;
+  x2 =  30; y2 = 25;
+
+  tx = x1;
+  ty = y1+50;
+  Model = "SPIRALIND";
+  Name  = "SPIRALIND";
+
+  Props.append(new Property("Subst", "Subst1", true,
+		QObject::tr("Substrate")));
+  Props.append(new Property("Geometry", "Circular", true,
+		QObject::tr("Spiral type")+
++		"[Circular, Square, Hexagonal, Octogonal]"));
+  Props.append(new Property("W", "25 um", false,
+		QObject::tr("Width of line")));
+  Props.append(new Property("Di", "200 um", false,
+		QObject::tr("Inner diameter")));
+  Props.append(new Property("S", "25 um", false,
+		QObject::tr("Spacing between turns")));
+  Props.append(new Property("N", "3", false,
+		QObject::tr("Number of turns")));
+  Props.append(new Property("Temp", "26.85", false,
+		QObject::tr("simulation temperature in degree Celsius")));
+
+}
+
+spiralinductor::~spiralinductor()
+{
+}
+
+Component* spiralinductor::newOne()
+{
+  return new spiralinductor();
+}
+
+Element* spiralinductor::info(QString& Name, char* &BitmapFile, bool getNewOne)
+{
+  Name = QObject::tr("Spiral inductor");
+  BitmapFile = (char *) "spiralinductor";
+
+  if(getNewOne)  return new spiralinductor();
+  return 0;
+}
---
+++ qucs/components/spiralinductor.h
@@ -0,0 +1,38 @@
+/*
+* spiralinductor.h - Planar spiral inductor class definition
+*
+* copyright (C) 2016 Andres Martinez-Mera <andresmartinezmera@gmail.com>
+*
+* This is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2, or (at your option)
+* any later version.
+*
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this package; see the file COPYING.  If not, write to
+* the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+* Boston, MA 02110-1301, USA.
+*
+*
+*/
+
+#ifndef SPIRALINDUCTOR_H
+#define SPIRALINDUCTOR_H
+
+#include "component.h"
+
+
+class spiralinductor : public Component  {
+public:
+  spiralinductor();
+  ~spiralinductor();
+  Component* newOne();
+  static Element* info(QString&, char* &, bool getNewOne=false);
+};
+
+#endif
