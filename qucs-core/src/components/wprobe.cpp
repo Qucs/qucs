ee5bf65c4c0dd98da9395b685df6cb5705e37792
Pedro-project <macedo.pp@gmail.com>
2016-02-03 18:55:01 +0000

    Update wprobe.cpp

--- qucs-core/src/components/wprobe.cpp
+++ qucs-core/src/components/wprobe.cpp
@@ -1,3 +1,20 @@
+/***************************************************************************
+                        wprobe.cpp  -  description
+                             -------------------
+    begin                : Sat Oct 30 2004
+    copyright            : (C) 2015 by Pedro Macedo
+    email                : macedo.pp@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #if HAVE_CONFIG_H
 # include <config.h>
 #endif
