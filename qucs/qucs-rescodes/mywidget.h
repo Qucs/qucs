4540d86fc360ab8913d54d099728cf3da64c383d
in3otd <in3otd@qsl.net>
2016-02-06 16:23:16 +0100

    Add copyright/license header for the original author

--- qucs/qucs-rescodes/mywidget.h
+++ qucs/qucs-rescodes/mywidget.h
@@ -1,3 +1,21 @@
+/***************************************************************************
+                               mywidget.h
+                             ------------------
+    begin                : Mar 2012
+    copyright            : (C) 2012 by Sudhakar.M.K
+    email                : sudhakar.m.kumar@gmail.com
+    copyright            : (C) 2016, Qucs team (see AUTHORS file)
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #ifndef MYWIDGET_H
 #define MYWIDGET_H
