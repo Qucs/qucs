82a0459f936e087dbf6c2f211df4d12c6151fbc1
Guilherme Brondani Torri <guitorri@gmail.com>
2016-03-28 18:23:14 +0200

    Avoid using LANGUAGEDIR definition.
    To have qucs in a relocatable directory (in Windows) it cannot rely
    the prefix given at build time. This is actually a bug.
    
    This patch let each tool find the language files at runtime, relative
    to the location of the executable.

--- qucs/qucs-transcalc/main.cpp
+++ qucs/qucs-transcalc/main.cpp
@@ -105,13 +105,21 @@ int main(int argc, char *argv[])
 
   // is application relocated?
   char * var = getenv ("QUCSDIR");
+  QDir QucsDir;
   if (var != NULL) {
-    QDir QucsDir = QDir (var);
+    QucsDir = QDir (var);
     QString QucsDirStr = QucsDir.canonicalPath ();
     QucsSettings.LangDir =
       QDir::convertSeparators (QucsDirStr + "/share/qucs/lang/");
   } else {
-    QucsSettings.LangDir = LANGUAGEDIR;
+    QString QucsApplicationPath = QCoreApplication::applicationDirPath();
+#ifdef __APPLE__
+    QucsDir = QDir(QucsApplicationPath.section("/bin",0,0));
+#else
+    QucsDir = QDir(QucsApplicationPath);
+    QucsDir.cdUp();
+#endif
+    QucsSettings.LangDir = QucsDir.canonicalPath() + "/share/qucs/lang/";
   }
   QucsSettings.QucsWorkDir.setPath (QDir::homePath()+QDir::convertSeparators ("/.qucs"));
   loadSettings();
