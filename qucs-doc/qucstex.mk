1213986d3a29322583956c9ba03492abd7433e18
Felix Salfelder <felix@salfelder.org>
2016-08-17 10:37:11 +0100

    more cleanup...

--- qucs-doc/qucstex.mk
+++ qucs-doc/qucstex.mk
@@ -1,8 +1,9 @@
 ## Process this file with automake to produce Makefile.in
-# 
-# Makefile.am
 #
-# Automake input file.
+# qucstex.mk
+#
+# makefile fragments for (la)tex invocation
+# used for building qucs doc
 #
 # Copyright (C) 2016 Felix Salfelder
 #
@@ -10,12 +11,12 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
 # any later version.
-# 
+#
 # This software is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this package; see the file COPYING.  If not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
