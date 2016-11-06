5d6a2549b40043101ed01ffce1a6c049723851cc
Felix Salfelder <felix@salfelder.org>
2016-11-06 00:36:55 +0000

    import dispatcher
    just pulled it in from gnucap. and io_trace.h, for debugging, and
    io_error, so we do not need to have gnucap installed.
    
    could be taken apart, however there's nothing in it that we will not
    need anyway...

---
+++ qucs/qucs/io_error.cc
@@ -0,0 +1,19 @@
+// imported from gnucap.
+// TODO: connect to qucs error console
+//
+#include <iostream>
+
+void error(int, const char* fmt, ...)
+{
+  char buffer[1024] = "";
+  va_list arg_ptr;
+  va_start(arg_ptr,fmt);
+  vsprintf(buffer,fmt,arg_ptr);
+  va_end(arg_ptr);
+  std::cerr << buffer;
+}
+/*--------------------------------------------------------------------------*/
+void error(int, const std::string& message)
+{
+	std::cerr << message;
+}
