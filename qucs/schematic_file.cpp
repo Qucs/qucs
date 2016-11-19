3f6702a186dfbb3bd5b8304c255c7eadd8af70a8
in3otd <in3otd@qsl.net>
2016-11-19 16:01:40 +0100

    Remove usage of Qt3 compatibility functions

--- qucs/schematic_file.cpp
+++ qucs/schematic_file.cpp
@@ -187,7 +187,7 @@ bool Schematic::pasteFromClipboard(QTextStream *stream, Q3PtrList<Element> *pe)
 int Schematic::saveSymbolCpp (void)
 {
   QFileInfo info (DocName);
-  QString cppfile = info.dirPath () + QDir::separator() + DataSet;
+  QString cppfile = info.path () + QDir::separator() + DataSet;
   QFile file (cppfile);
 
   if (!file.open (QIODevice::WriteOnly)) {
@@ -255,7 +255,7 @@ int Schematic::saveSymbolCpp (void)
 int Schematic::saveSymbolJSON()
 {
   QFileInfo info (DocName);
-  QString jsonfile = info.dirPath () + QDir::separator()
+  QString jsonfile = info.path () + QDir::separator()
                    + info.completeBaseName() + "_sym.json";
 
   qDebug() << "saveSymbolJson for " << jsonfile;
@@ -450,7 +450,7 @@ int Schematic::saveDocument()
       QStringList Arguments;
       Arguments << QDir::toNativeSeparators(vaFile)
                 << "-I" << QDir::toNativeSeparators(include.absolutePath())
-                << "-e" << QDir::toNativeSeparators(include.absFilePath("qucsMODULEguiJSONsymbol.xml"))
+                << "-e" << QDir::toNativeSeparators(include.absoluteFilePath("qucsMODULEguiJSONsymbol.xml"))
                 << "-A" << "dyload";
 
 //      QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
@@ -870,9 +870,9 @@ bool Schematic::loadDocument()
     QMessageBox::StandardButton result;
     result = QMessageBox::warning(0,
                                   QObject::tr("Warning"),
-                                  QObject::tr("Wrong document version \n" +
-                                              DocName + "\n"
-                                              "Try to open it anyway?"),
+                                  QObject::tr("Wrong document version \n"
+                                              "%1\n"
+                                              "Try to open it anyway?").arg(DocName),
                                   QMessageBox::Yes|QMessageBox::No);
 
     if (result==QMessageBox::No) {
@@ -1082,7 +1082,7 @@ int Schematic::testFile(const QString& DocName)
   // To strongly speed up the file read operation the whole file is
   // read into the memory in one piece.
   QTextStream ReadWhole(&file);
-  QString FileString = ReadWhole.read();
+  QString FileString = ReadWhole.readAll();
   file.close();
   QTextStream stream(&FileString, QIODevice::ReadOnly);
 
@@ -1144,7 +1144,7 @@ void Schematic::collectDigitalSignals(void)
     if(it == Signals.end()) { // avoid redeclaration of signal
       Signals.insert(pn->Name, DigSignal(pn->Name, pn->DType));
     } else if (!pn->DType.isEmpty()) {
-      it.data().Type = pn->DType;
+      it.value().Type = pn->DType;
     }
   }
 }
@@ -1245,13 +1245,13 @@ bool Schematic::throughAllComps(QTextStream *stream, int& countInit,
       SubMap::Iterator it = FileList.find(f);
       if(it != FileList.end())
       {
-        if (!it.data().PortTypes.isEmpty())
+        if (!it.value().PortTypes.isEmpty())
         {
           i = 0;
           // apply in/out signal types of subcircuit
           foreach(Port *pp, pc->Ports)
           {
-            pp->Type = it.data().PortTypes[i];
+            pp->Type = it.value().PortTypes[i];
             pp->Connection->DType = pp->Type;
             i++;
           }
@@ -1295,7 +1295,7 @@ bool Schematic::throughAllComps(QTextStream *stream, int& countInit,
             i++;
         }
         sub.PortTypes = d->PortTypes;
-        FileList.replace(f, sub);
+        FileList.insert(f, sub);
       }
       delete d;
       if(!r)
@@ -1538,13 +1538,13 @@ int NumPorts)
       (*it_name) = pc->Ports.first()->Connection->Name;
       DigMap::Iterator it = Signals.find(*it_name);
       if(it!=Signals.end())
-        (*it_type) = it.data().Type;
+        (*it_type) = it.value().Type;
       // propagate type to port symbol
       pc->Ports.first()->Connection->DType = *it_type;
 
       if(!isAnalog) {
         if (isVerilog) {
-          Signals.erase(*it_name); // remove node name
+          Signals.remove(*it_name); // remove node name
           switch(pc->Props.at(1)->Value.at(0).toLatin1()) {
             case 'a':
               InOutPorts.append(*it_name);
@@ -1558,7 +1558,7 @@ int NumPorts)
         }
         else {
           // remove node name of output port
-          Signals.erase(*it_name);
+          Signals.remove(*it_name);
           switch(pc->Props.at(1)->Value.at(0).toLatin1()) {
             case 'a':
               (*it_name) += " : inout"; // attribute "analog" is "inout"
@@ -1582,8 +1582,8 @@ int NumPorts)
       it_type = SubcircuitPortTypes.begin();
       it_name != SubcircuitPortNames.end(); ) {
     if(*it_name == " ") {
-      it_name = SubcircuitPortNames.remove(it_name);
-      it_type = SubcircuitPortTypes.remove(it_type);
+      it_name = SubcircuitPortNames.erase(it_name);
+      it_type = SubcircuitPortTypes.erase(it_type);
     } else {
       PortTypes.append(*it_type);
       it_name++;
