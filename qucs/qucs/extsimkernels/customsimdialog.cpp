865a358240043365a249064e51cf2bb3474acd6d
Vadim Kuznetzov <ra3xdh@gmail.com>
2017-03-04 19:14:06 +0300

    Add component information in SPICE code dialog

--- qucs/qucs/extsimkernels/customsimdialog.cpp
+++ qucs/qucs/extsimkernels/customsimdialog.cpp
@@ -37,6 +37,8 @@ CustomSimDialog::CustomSimDialog(SpiceCustomSim *pc, Schematic *sch, QWidget *pa
     comp = pc;
     Sch = sch;
 
+    setWindowTitle(tr("Edit SPICE code"));
+    QLabel* lblName = new QLabel(tr("Component: ")+comp->Description);
     QLabel* lblEdt = new QLabel(tr("Spice code editor"));
     edtCode = new QTextEdit(this);
     edtCode->insertPlainText(comp->Props.at(0)->Value);
@@ -59,10 +61,14 @@ CustomSimDialog::CustomSimDialog(SpiceCustomSim *pc, Schematic *sch, QWidget *pa
     connect(btnFindOutputs,SIGNAL(clicked()),this,SLOT(slotFindOutputs()));
 
     QVBoxLayout *vl1 = new QVBoxLayout;
+    QVBoxLayout *vl2 = new QVBoxLayout;
     QHBoxLayout *hl1 = new QHBoxLayout;
 
-    vl1->addWidget(lblEdt);
-    vl1->addWidget(edtCode);
+    vl1->addWidget(lblName);
+    QGroupBox *gpb1 = new QGroupBox(tr("SPICE code editor"));
+    vl2->addWidget(edtCode);
+    gpb1->setLayout(vl2);
+    vl1->addWidget(gpb1);
     vl1->addWidget(lblVars);
     vl1->addWidget(edtVars);
     vl1->addWidget(btnPlotAll);
@@ -76,6 +82,7 @@ CustomSimDialog::CustomSimDialog(SpiceCustomSim *pc, Schematic *sch, QWidget *pa
     vl1->addLayout(hl1);
 
     this->setLayout(vl1);
+    this->setWindowTitle(tr("Edit SPICE code"));
 
     if (comp->Model == ".XYCESCR") {
         lblVars->setEnabled(false);
@@ -87,6 +94,7 @@ CustomSimDialog::CustomSimDialog(SpiceCustomSim *pc, Schematic *sch, QWidget *pa
         edtVars->setEnabled(false);
         btnPlotAll->setEnabled(false);
         btnFindOutputs->setEnabled(false);
+        lblOut->setEnabled(false);
     } else isXyceScr = false;
 }
 
