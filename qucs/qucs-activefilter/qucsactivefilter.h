a82a1586998b2eb007e3febf1a1cfb13b26740b6
gypsysattva <bodhisattvavpered@gmail.com>
2017-04-02 15:30:12 +0300

    Qucsactivefilter: Legendre filter added
    1. Added Legendre pre-calculated coefficients table legendre.h
    2. Added GUI controls to make access to Legendre filter

--- qucs/qucs-activefilter/qucsactivefilter.h
+++ qucs/qucs-activefilter/qucsactivefilter.h
@@ -41,8 +41,8 @@ class QucsActiveFilter : public QMainWindow
 private:
 
     enum lstTopology { topoMFB = 0, topoSallenKey = 1, topoCauer = 2};
-    enum lstFiltFunc { funcButterworth =0, funcChebyshev=1, funcInvChebyshev=2,
-                       funcCauer = 3, funcBessel = 4, funcUser = 5};
+    enum lstFiltFunc { funcButterworth = 0, funcChebyshev = 1, funcInvChebyshev = 2,
+                       funcCauer = 3, funcBessel = 4, funcLegendre = 5, funcUser = 6};
     enum lstFiltType { tLowPass =0, tHiPass=1, tBandPass=2, tBandStop=3};
 
     QLabel *lblInputData;
