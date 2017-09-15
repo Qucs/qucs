9d2a44a66bfa03d018bdd3b84bc8c97a2d32c70c
Guilherme Brondani Torri <guitorri@gmail.com>
2017-09-15 21:02:56 +0200

    Change Schematic, inherit from QGraphicsView
    - Remove deprecated methods:
      - MouseActions::setPainter, does PostPaintEvents cals which aren't needed.
      - Schematic::drawContents, items should draw themselves into QGraphicsView.
    
    - Mark paintAim for refactor.
    
    - Add TODO() macro to keep track of things that need work.
    
    - Comment out sections of code that need work.

--- qucs/qucs/mouseactions.h
+++ qucs/qucs/mouseactions.h
@@ -35,7 +35,6 @@ public:
   MouseActions(QucsApp*);
   virtual ~MouseActions();
 
-  void setPainter(Schematic*);
   bool pasteElements(Schematic*);
   void editElement(Schematic*, QMouseEvent*);
   void editLabel(Schematic*, WireLabel*);
