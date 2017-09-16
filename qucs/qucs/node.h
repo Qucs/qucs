93672f35be1728859f29aca3a7bfe1c836b5093c
Guilherme Brondani Torri <guitorri@gmail.com>
2017-09-17 00:30:04 +0200

    Node: port
    Set is as movable item

--- qucs/qucs/node.h
+++ qucs/qucs/node.h
@@ -1,19 +1,25 @@
-/***************************************************************************
-                                 node.h
-                                --------
-    begin                : Sat Sep 20 2003
-    copyright            : (C) 2003 by Michael Margraf
-    email                : michael.margraf@alumni.tu-berlin.de
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+/*
+ * node.h - Implementation of an electrical node
+ *
+ * Copyright (C) 2003, Michael Margraf, michael.margraf@alumni.tu-berlin.de
+ * Copyright (C) 2015, Guilherme Brondani Torri, guitorri@gmail.com
+ *
+ * This file is part of Qucs
+ *
+ * Qucs is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Qucs.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 #ifndef NODE_H
 #define NODE_H
@@ -21,9 +27,6 @@
 #include "element.h"
 
 #include <Q3PtrList>
-
-class ViewPainter;
-
 
 class Node : public Conductor {
 public:
@@ -33,7 +36,6 @@ public:
   QRectF boundingRect() const;
   void paint(QPainter* painter, const QStyleOptionGraphicsItem* item, QWidget* widget);
 
-  void  paint(ViewPainter*);
   bool  getSelected(int, int);
   void  setName(const QString&, const QString&, int x_=0, int y_=0);
 
