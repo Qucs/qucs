6e7935daa356946ffb42e6089effbe40d43252e0
Guilherme Brondani Torri <guitorri@gmail.com>
2017-10-02 21:52:07 +0200

    ellipse: fix mouse move, mouse press

--- qucs/qucs/paintings/ellipse.h
+++ qucs/qucs/paintings/ellipse.h
@@ -30,7 +30,6 @@ public:
 
   QRectF boundingRect() const;
   void paint(QPainter *painter, const QStyleOptionGraphicsItem *item, QWidget *widget);
-  void paintScheme(Schematic*);
   void getCenter(int&, int&);
   void setCenter(int, int, bool relative=false);
 
@@ -55,6 +54,8 @@ public:
   QPen  Pen;
   QBrush Brush;    // filling style/color
   bool  filled;    // filled or not (for "getSelected" etc.)
+
+  int ex, ey; // used to track position of mouse move event
 };
 
 #endif
