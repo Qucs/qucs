39f45f6e11664da041ed706358d0a942f9d4d9b8
Guilherme Brondani Torri <guitorri@gmail.com>
2017-10-10 19:25:34 +0200

    graphictextdialog: remove unused variable

--- qucs/paintings/graphictextdialog.h
+++ qucs/paintings/graphictextdialog.h
@@ -30,7 +30,7 @@ class QIntValidator;
 class GraphicTextDialog : public QDialog  {
 Q_OBJECT
 public:
-  GraphicTextDialog(QWidget *parent=0, const char *name=0);
+  GraphicTextDialog(QWidget *parent=0);
  ~GraphicTextDialog();
 
 private slots:
