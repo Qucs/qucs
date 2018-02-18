7b48b3c13951e39f4fd159a7edbc720164d3a486
Guilherme Brondani Torri <guitorri@gmail.com>
2018-02-18 22:51:52 +0100

    schematic: allow to key scroll up/down

--- qucs/qucs/schematic.cpp
+++ qucs/qucs/schematic.cpp
@@ -1825,7 +1825,7 @@ void Schematic::contentsWheelEvent(QWheelEvent *Event)
 // area accordingly.
 bool Schematic::scrollUp(int step)
 {
-  TODO("Fix scroll");
+  TODO("Fix scrollUp");
   /**
   int diff;
 
@@ -1852,7 +1852,7 @@ bool Schematic::scrollUp(int step)
 // area accordingly. ("step" must be negative!)
 bool Schematic::scrollDown(int step)
 {
-  TODO("Fix scroll");
+  TODO("Fix scrollDown");
   /**
   int diff;
 
