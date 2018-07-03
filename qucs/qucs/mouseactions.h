a7d77a075d71ce284e4fbee94558889dc5ae17b0
Guilherme Brondani Torri <guitorri@gmail.com>
2018-07-03 21:07:31 +0200

    Simplify rightPressMenu
    Let the method use the MouseEvent alone to figure out x y position.
    
    Note, MAx1 and MAy1 is used in nonterminal actions to store the location
    of the first mouse press or event.

--- qucs/qucs/mouseactions.h
+++ qucs/qucs/mouseactions.h
@@ -113,7 +113,7 @@ public:
   void moveElements(Schematic*, int&, int&);
   void moveElements(Q3PtrList<Element>*, int, int);
   void endElementMoving(Schematic*, Q3PtrList<Element>*);
-  void rightPressMenu(Schematic*, QMouseEvent*, float, float);
+  void rightPressMenu(Schematic*, QMouseEvent*);
 };
 
 #endif
