eb8882f94a4e76421b36ba4c1bd80641f26f3bfc
Guilherme Brondani Torri <guitorri@gmail.com>
2018-07-11 22:13:33 +0200

    Draw grid with SchematicScene::drawBackground

--- qucs/qucs/schematicscene.cpp
+++ qucs/qucs/schematicscene.cpp
@@ -2,6 +2,7 @@
  * schematicscene.cpp - implement schematic scene
  *
  * Copyright (C) 2014, Yodalee, lc85301@gmail.com
+ * Copyright (C) 2017, Guilherme Brondani Torri, guitorri@gmail.com
  *
  * This file is part of Qucs
  *
@@ -23,6 +24,8 @@
 
 #include "schematicscene.h"
 
+#include <QPainter>
+
 SchematicScene::SchematicScene(QObject *parent) :
   QGraphicsScene(parent)
 {
@@ -30,5 +33,35 @@ SchematicScene::SchematicScene(QObject *parent) :
 
 SchematicScene::~SchematicScene()
 {
+}
+
+/*!
+ * \brief SchematicScene::drawBackground of scene
+ * \param painter
+ * \param rect
+ */
+void SchematicScene::drawBackground(QPainter *painter, const QRectF &rect)
+{
+
+  // Draw origin when visible
+  if(rect.contains(QPointF(0, 0))) {
+    painter->drawLine(QLine(-3.0, 0.0, 3.0, 0.0));
+    painter->drawLine(QLine(0.0, -3.0, 0.0, 3.0));
+  }
+  /// \todo getter and setter
+  int GridX = 10;
+  int GridY = 10;
+
+  // Extrema grid points
+  qreal left = int(rect.left()) - (int(rect.left()) % GridX);
+  qreal top = int(rect.top()) - (int(rect.top()) % GridY);
+  qreal right = int(rect.right()) - (int(rect.right()) % GridX);
+  qreal bottom = int(rect.bottom()) - (int(rect.bottom()) % GridY);
+
+  // Draw grid
+  painter->setBrush(Qt::NoBrush);
+  for (int x=left; x<=right; x+=GridX)
+      for (int y=top; y<=bottom; y+=GridY)
+          painter->drawPoint(x, y);
 }
 
