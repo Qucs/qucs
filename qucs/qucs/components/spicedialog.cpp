3d2a893a5f28c0712f47a66c17150d570368ceac
Felix Salfelder <felix@salfelder.org>
2018-07-17 13:53:20 +0100

    compiler warning x7

--- qucs/qucs/components/spicedialog.cpp
+++ qucs/qucs/components/spicedialog.cpp
@@ -448,8 +448,7 @@ bool SpiceDialog::loadSpiceNetList(const QString& s)
 
   QucsConv->start(Program, Arguments);
 
-  if(!QucsConv->Running)
-  {
+  if(QucsConv->Running!=0) {
     QMessageBox::critical(this, tr("Error"),
                           tr("Cannot execute \"%1\".").arg(QucsSettings.Qucsconv));
     return false;
