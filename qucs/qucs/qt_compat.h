8aecd52d3e90a40ca35616ce10328d6332c7e538
Felix Salfelder <felix@salfelder.org>
2018-07-20 19:16:58 +0100

    revisit Q3Ptrlist
    - switch to sensible backend.
    - fix a couple of members. particularly, fix the "cur" logic...
    - moving nets is working again

--- qucs/qucs/qt_compat.h
+++ qucs/qucs/qt_compat.h
@@ -20,7 +20,7 @@
 #ifndef QUCS_CONTAINER_H
 #define QUCS_CONTAINER_H
 
-#include <QList>
+#include <list>
 #include <QDebug>
 
 #include "trace.h"
@@ -30,7 +30,7 @@
 template <class T>
 class Q3PtrList {
 public:
-	typedef QList<T*> container_type;
+	typedef std::list<T*> container_type;
 	typedef typename container_type::const_iterator const_iterator;
 	typedef typename container_type::iterator iterator;
 public:
@@ -48,8 +48,9 @@ public:
 		}
 	};
 	T* at(unsigned int i) {
-		if (i < (unsigned int)localList.count()) {
-			cur = localList.begin()+i;
+		if (i < (unsigned int)localList.size()) {
+			cur = localList.begin();
+			std::advance(cur, i);
 			return *cur;
 		} else {
 			cur = localList.end();
@@ -65,21 +66,21 @@ public:
 	};
 	T* first() {
 		cur = localList.begin();
-		if (localList.count() == 0)
+		if (localList.size() == 0)
 			return nullptr;
 		return *cur;
 	};
 	T* last() {
-		if (localList.count()) { untested();
-			cur = localList.end()-1; // yikes. no rbegin. use std::list?!
+		if (localList.size()) { untested();
+			cur = localList.end();
+			--cur;
 			return *cur;
 		} else { untested();
 			return nullptr;
 		}
 	};
 	void append(T *t) {
-		localList.append(t);
-		cur = localList.end()-1; // yikes. no rbegin. use std::list?!
+		cur = localList.insert(localList.end(), t);
 	};
 	int contains(T *) { untested();
 		incomplete();
@@ -105,18 +106,16 @@ public:
 		incomplete();
 		return true;
 	};
-	int containsRef(T *t) { untested();
-		int n = 0;
-		for (int i = 0; i < localList.count(); i++){ untested();
-			if (t == localList[i]) { untested();
-				n++;
-			}
+	unsigned containsRef(T *t) { untested();
+		unsigned i=0;
+		for (const_iterator c=begin(); c!=end(); ++c){
+			i += (t == *c);
 		}
-		return n;
+		return i;
 	};
 	int findRef(T *t) {
 		auto i=0;
-		for (cur=localList.begin(); cur!=localList.end(); ++cur){
+		for (cur=begin(); cur!=end(); ++cur){
 			if (t == *cur){
 				return i;
 			}
@@ -133,10 +132,19 @@ public:
 		return false;
 	};
 	void prepend(T *t) { untested();
-		localList.insert(0, t);
+		localList.push_front(t);
+		cur=begin();
 	};
-	T* take(uint index) { untested();
-		return localList.takeAt(index);
+	T* take(uint i) { untested();
+		if(within_range(i)){
+			cur = begin();
+			std::advance(cur, i);
+			T* ret=*cur;
+			cur = localList.erase(cur);
+			return ret;
+		}else{ untested();
+			return nullptr;
+		}
 	};
 	T* take() { untested();
 		if (cur != localList.end()){ untested();
@@ -150,11 +158,11 @@ public:
 			return nullptr;
 		}
 	};
-	T* getFirst() { itested();
-		return localList.first();
+	T* getFirst() { untested();
+		return localList.front();
 	};
-	T* getLast() { itested();
-		return localList.last();
+	T* getLast() { untested();
+		return localList.back();
 	};
 	T* next() {
 		if (cur == localList.end()){ untested();
@@ -187,48 +195,69 @@ public:
 		}
 	};
 	unsigned int count() {
-		return (unsigned int)localList.count();
+		return (unsigned int)localList.size();
 	};
-	void remove(int i) { //
-		if (i >= 0 && i < localList.count()) { untested();
-			localList.removeAt(i);
+	void remove(int i) { untested();
+		if (i < 0){ untested();
+		}else if(i<int(localList.size())) { untested();
+			cur=begin();
+			std::advance(cur, i);
+			remove();
 		}
 	};
-	void remove() { // remove the current element
+	void remove() {
 		if (cur!=localList.end()){ untested();
 			auto next=cur;
 			++next;
-			if(_autodelete){ untested();
-				delete *cur;
-			}else{
-			}
+			maybe_delete(*cur);
 			localList.erase(cur);
+			cur = next;
 		}else{ untested();
 		}
 	};
-	void removeLast() { untested();
-		int c = localList.count() -1;
-		if (c >= 0) { untested();
-			localList.removeAt(c);
+	bool removeLast() { untested();
+		auto c = localList.size();
+		if(c) { untested();
+			cur=end();
+			--cur;
+			maybe_delete(*cur);
+			cur = localList.erase(cur);
+			--cur;
+			return true;
+		}else{ untested();
+			return false;
 		}
 	};
-	void removeFirst() { untested();
-		int c = localList.count() -1;
-		if (c >= 0) { untested();
-			localList.removeAt(0);
+	bool removeFirst() { untested();
+		auto c = localList.size();
+		if(c) { untested();
+			cur=begin();
+			cur=localList.erase(cur);
+			return true;
+		}else{ untested();
+			return false;
 		}
 	};
 	void insert(unsigned int i, T *t) { untested();
-		localList.insert(i, t);
-		cur=localList.begin()+i;
+		cur=localList.begin();
+		std::advance(cur, i);
+		cur=localList.insert(cur, t);
 	};
+   // check: this doesn't look right
 	int find(T *t) { untested();
-		for (int i = 0; i < localList.count(); i++){ untested();
-			if (t == localList[i]) { untested();
-				return i;
-			}
-		}
-		return -1;
+		incomplete();
+		return 0;
+
+		// int i=0;
+		// for (auto x : localList){ untested();
+		// 	// NB: this is what compareitem would do... it does not work
+		// 	if (*t == *x) { untested();
+		// 		return i;
+		// 	}else{
+		// 		++i;
+		// 	}
+		// }
+		// return -1;
 	};
 	const_iterator begin() const{ untested();
 		return localList.begin();
@@ -245,11 +274,26 @@ public:
 	void clear(){
 		return localList.clear();
 	}
-
 private:
-	QList<T*> localList; // why not std::list!?
+	void maybe_delete(T* x){
+		if(_autodelete){ untested();
+			delete x;
+		}else{
+		}
+	}
+	bool within_range(int i){
+		if (i < 0){ untested();
+			return false;
+		}else if(i<int(localList.size())) { untested();
+			return true;
+		}else{ untested();
+			return false;
+		}
+	}
+private:
+	std::list<T*> localList;
 	bool _autodelete;
-	typename QList<T*>::iterator cur;
+	iterator cur;
 };
 
 #endif
