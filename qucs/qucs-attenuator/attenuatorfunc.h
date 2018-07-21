984f20df30582327b33217d7eef2ccc042099b08
andresmmera <andresmartinezmera@gmail.com>
2018-07-21 17:31:29 +0200

    Update copyright and headers

--- qucs/qucs-attenuator/attenuatorfunc.h
+++ qucs/qucs-attenuator/attenuatorfunc.h
@@ -1,15 +1,25 @@
-//-*- C++ -*-
-/****************************************************************************
-**     Qucs Attenuator Synthesis
-**     attenuatorfunc.h
-**
-**     since 2006/6/14
-**
-**
-**
-**
-**
-*****************************************************************************/
+/*
+ * attenuatorfunc.h - Attenuator synthesis and schematic generation header
+ *
+ * Copyright (C) 2018 Qucs Team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this package; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ */
+
 #ifndef QUCS_ATT_H
 #define QUCS_ATT_H
 
--- qucs/qucs-attenuator/qucsattenuator.cpp
+++ qucs/qucs-attenuator/qucsattenuator.cpp
@@ -1,14 +1,25 @@
-/****************************************************************************
-**     Qucs Attenuator Synthesis
-**     qucsattenuator.cpp
-**
-**
-**
-**
-**
-**
-**
-*****************************************************************************/
+/*
+ * qucsattenuator.cpp - Attenuator synthesis tool GUI
+ *
+ * Copyright (C) 2018 Qucs Team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this package; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ */
+
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -388,6 +399,7 @@ void QucsAttenuator::slotHelpAbout()
     QMessageBox::about(this, tr("About..."),
     "QucsAttenuator Version " PACKAGE_VERSION+
     tr("\nAttenuator synthesis program\n")+
+    tr("Copyright (C) 2018 by")+" Qucs Team\n" +
     tr("Copyright (C) 2006 by")+" Toyoyuki Ishikawa"
     "\n"+
     tr("Copyright (C) 2006 by")+" Stefan Jahn"
