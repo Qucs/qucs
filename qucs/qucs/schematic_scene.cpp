7c1feaa0a968ed6f97b51f572d7d8411eb2d74e5
Felix Salfelder <felix@salfelder.org>
2018-07-31 23:02:25 +0100

    schematic_scene

--- qucs/qucs/schematic_scene.cpp
+++ qucs/qucs/schematic_scene.cpp
@@ -5,17 +5,17 @@
 // ---------------------------------------------------
 // forward to graphicscene, once it is there.
 // scene->itemAt(Doc->mapToScene(Event->pos()), QTransform()
-ElementGraphics Schematic::itemAt(float x, float y)
+ElementGraphics* Schematic::itemAt(float x, float y)
 {
     for(Component *pc = Components->first(); pc != 0; pc = Components->next())
         if(pc->getSelected(x, y))
-            return ElementGraphics(pc);
+            return (ElementGraphics*)(pc);
 
     float Corr = 5.0 / Scale; // size of line select
 
     for(Painting *pp = Paintings->first(); pp != 0; pp = Paintings->next())
         if(pp->getSelected(x, y, Corr))
-            return ElementGraphics(pp);
+            return (ElementGraphics*)(pp);
 
     incomplete(); // also select the other stuff.
     return nullptr;
