0e5d9adefad5dd6c613ef14307cbb2fa9d237d0f
Felix Salfelder <felix@salfelder.org>
2018-07-31 11:27:54 +0100

    use QT_MAJOR_VERSION in schematic.h
    moc ignores the other (?!)

--- qucs/qucs/schematic.h
+++ qucs/qucs/schematic.h
@@ -101,7 +101,7 @@ public:
 	void sizeOfAll(int& xmin, int& ymin, int& xmax, int& ymax) const;
 };
 
-#if QT_VERSION < 0x050000
+#if QT_MAJOR_VERSION < 5
 typedef Element ElementGraphics;
 #define SchematicBase Q3ScrollView
 #else
