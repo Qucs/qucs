41e61c5bb87c1466f97aee10c37cd69b70b01a05
Felix Salfelder <felix@salfelder.org>
2018-08-02 07:33:47 +0100

    debugging

--- qucs/qucs/mouseactions.cpp
+++ qucs/qucs/mouseactions.cpp
@@ -1011,7 +1011,7 @@ void MouseActions::MPressSelect(Schematic *Doc, QMouseEvent *Event)
     // print define value in hex, see element.h
     //qDebug() << "MPressSelect: focusElement->Type" <<  QString("0x%1").arg(focusElement->Type, 0, 16);
   }else{
-    qDebug() << "MPressSelect miss";
+    qDebug() << "MPressSelect miss" << Event->pos() << pos;
   }
 
   incomplete(); //this does not add up.
@@ -2023,7 +2023,10 @@ void MouseActions::editElement(Schematic *Doc, QMouseEvent *Event)
 { untested();
 //    qDebug() << "+double click, editElement";
 
-  if(!focusElement) return;
+  if(!focusElement){untested();
+    return;
+  }else{ untested();
+  }
 
 //  qDebug() << "+focusElement->Type" << focusElement->Type;
 
@@ -2040,7 +2043,7 @@ void MouseActions::editElement(Schematic *Doc, QMouseEvent *Event)
   float fY=pos.y();
 
   // BUG. move to respective classes. 1 at a time...
-  if(auto c=component(focusElement)){
+  if(auto c=component(focusElement)){ untested();
 //         qDebug() << "cast focusElement into" << c->Name;
          bool done=false;
          if(c->obsolete_model_hack() == "GND") { // BUG
