222e86e2cbb0997198c16eaeb50a864eb607820a
Felix Salfelder <felix@salfelder.org>
2018-08-03 16:31:12 +0100

    debug editDelete

--- qucs/qucs/schematic_element.cpp
+++ qucs/qucs/schematic_element.cpp
@@ -974,7 +974,7 @@ void Schematic::markerUpDown(bool up, Q3PtrList<Element> *Elements)
    menu.
 */
 ElementMouseAction MouseActions::selectElement(Schematic* Doc,
-	QPoint const& xy, bool flag, int *index)
+	QPoint const& xy, bool flag, int *ndex)
 { untested();
    // THIS IS MISLEADING. it is also used to generate mouse actions.
    // we need something that produces actions, not Elements
@@ -1432,6 +1432,9 @@ void Schematic::highlightWireLabels ()
 // bug: why do we not have a list of selected elements?!
 void MouseActions::deselectElements(Schematic* Doc, ElementMouseAction e)
 {
+#ifndef USE_SCROLLVIEW
+incomplete();
+#else
     // test all components
     for(auto* pc : Doc->components()){
         if(e != pc)  pc->setSelected(false);
@@ -1469,6 +1472,7 @@ void MouseActions::deselectElements(Schematic* Doc, ElementMouseAction e)
     for(auto *pp : Doc->paintings()){
         if(e != pp)  pp->setSelected(false);
     }
+#endif
 }
 
 // ---------------------------------------------------
@@ -2003,8 +2007,32 @@ int Schematic::copyElements(int& x1, int& y1, int& x2, int& y2,
 
 // ---------------------------------------------------
 // Deletes all selected elements.
+// return what?
 bool Schematic::deleteElements()
 {
+#ifndef USE_SCROLLVIEW
+    // todo: elements seem to be entangled, see below old code.
+//    scene()->selectionChanged();
+    for(auto ge : scene()->selectedItems()){ untested();
+	qDebug() << "select" << ge << ge->isSelected();
+	if(auto e=dynamic_cast<ElementGraphics*>(ge)){
+	    scene()->removeItem(ge);
+
+	    // todo: forward delete to schematicModel (or so) container
+	    if(auto c=component(e)){ untested();
+		deleteComp(c);
+	    }else if(auto w=wire(e)){ untested();
+		deleteWire(w);
+	    }else{
+		incomplete();
+	    }
+	}else{
+	    unreachable();
+	    incomplete();
+	}
+    }
+    return true; // ?
+#else
     bool sel = false;
 
     Component *pc = Components->first();
@@ -2112,6 +2140,7 @@ bool Schematic::deleteElements()
         setChanged(sel, true);
     }
     return sel;
+#endif
 }
 
 // ---------------------------------------------------
