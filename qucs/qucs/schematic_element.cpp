032638bbc854199dade8ce88686d571fe4db80e9
Felix Salfelder <felix@salfelder.org>
2018-08-03 09:37:26 +0100

    fix elementSelect

--- qucs/qucs/schematic_element.cpp
+++ qucs/qucs/schematic_element.cpp
@@ -1015,6 +1015,7 @@ ElementMouseAction MouseActions::selectElement(Schematic* Doc,
 	   qDebug() << "nothing at" << xy << scenepos;
        return ElementMouseAction(nullptr);
    }
+    return ElementMouseAction(pe_1st);
 #else
 
     int n;
