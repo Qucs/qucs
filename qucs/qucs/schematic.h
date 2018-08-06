32fd20811de63caab784fcc1e640e398541c0759
Felix Salfelder <felix@salfelder.org>
2018-08-06 13:52:13 +0100

    SchMod::simpleinserWire and LoadWires

--- qucs/qucs/schematic.h
+++ qucs/qucs/schematic.h
@@ -127,10 +127,12 @@ public: // stuff saved from Schematic
   QString createClipboardFile();
   void sizeOfAll(int&, int&, int&, int&, float) const;
   void simpleInsertComponent(Component* c);
+  void simpleInsertWire(Wire*);
   bool loadPaintings(QTextStream*, PaintingList*);
   bool loadProperties(QTextStream*);
   bool loadComponents(QTextStream*);
   bool loadDiagrams(QTextStream*);
+  bool loadWires(QTextStream*);
 public: // obsolete.
   static void saveComponent(QTextStream& s, Component /* FIXME const */* c);
 private: // TODO: actually store here.
@@ -464,8 +466,7 @@ private:
   int  saveDocument();
 
   void simpleInsertComponent(Component* c) { return DocModel.simpleInsertComponent(c); }
-  void simpleInsertWire(Wire*);
-  bool loadWires(QTextStream*, EGPList *List=0);
+  void simpleInsertWire(Wire* w) { return DocModel.simpleInsertWire(w); }
   bool loadIntoNothing(QTextStream*);
 
   bool    pasteFromClipboard(QTextStream *, EGPList*);
