68e417973a3d90474c11c6ea85c6b3a117da7f56
Felix Salfelder <felix@salfelder.org>
2018-12-08 20:00:58 +0000

    missing files

--- qucs/qucs/globals.cpp
+++ qucs/qucs/globals.cpp
@@ -1,3 +1,18 @@
+/***************************************************************************
+                              globals.h
+                             --------------------
+    author               : 2018 Felix Salfelder
+    license              : GPLv3+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #include "globals.h"
 
 Dispatcher<Symbol> symbol_dispatcher;
--- qucs/qucs/globals.h
+++ qucs/qucs/globals.h
@@ -1,3 +1,18 @@
+/***************************************************************************
+                              globals.h
+                             --------------------
+    author               : 2018 Felix Salfelder
+    license              : GPLv3+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #include "platform.h"
 #include "l_dispatcher.h"
 
