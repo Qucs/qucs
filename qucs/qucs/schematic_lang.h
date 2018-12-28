065eac994d234a5a0861f42a42685fa1ccb1f93a
Felix Salfelder <felix@salfelder.org>
2018-12-28 13:26:38 +0000

    fuzz?

---
+++ qucs/qucs/schematic_lang.h
@@ -0,0 +1,23 @@
+// 2018 Felix
+// QUCS Project
+// GPLv3+
+//
+
+#ifndef QUCS_SCHEMATIC_LANG_H
+#define QUCS_SCHEMATIC_LANG_H
+
+#include "object.h"
+#include "qucsdoc.h"
+
+#define SchematicModel Schematic // fix later.
+class SchematicModel;
+
+class SchematicLanguage : public DocumentLanguage{
+protected:
+	SchematicLanguage() : DocumentLanguage() {}
+public:
+	virtual ~SchematicLanguage() {}
+	virtual void parse(DocumentStream& stream, SchematicModel*) const=0;
+};
+
+#endif
