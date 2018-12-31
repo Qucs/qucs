83155afef4d4bc4e4d9312d96f2e92728493087a
Felix Salfelder <felix@salfelder.org>
2018-12-31 09:43:05 +0000

    move stuff to schematic_lang

--- qucs/qucs/schematic.cpp
+++ qucs/qucs/schematic.cpp
@@ -2218,11 +2218,12 @@ void Schematic::contentsDragEnterEvent(QDragEnterEvent *Event)
     QString s = Event->mimeData()->text();
     if(s.left(15) == "QucsComponent:<") {
       s = s.mid(14);
-		incomplete();
-      App->view->selElem = getComponentFromName(s);
+      incomplete();
+      //App->view->selElem = legacySchematicLanguage::getComponentFromName(s);
       if(App->view->selElem) {
         Event->accept();
         return;
+      }else{
       }
     }
     Event->ignore();
