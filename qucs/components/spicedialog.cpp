6e441f9b96119a54ad615e7a7d82cd7bb663accd
Guilherme Brondani Torri <guitorri@gmail.com>
2020-02-15 21:11:27 +0100

    warning: enum constant in boolean context

--- qucs/components/spicedialog.cpp
+++ qucs/components/spicedialog.cpp
@@ -450,7 +450,7 @@ bool SpiceDialog::loadSpiceNetList(const QString& s)
 
   QucsConv->start(Program, Arguments);
 
-  if(!QucsConv->Running)
+  if(QucsConv->state() != QProcess::Running)
   {
     QMessageBox::critical(this, tr("Error"),
                           tr("Cannot execute \"%1\".").arg(QucsSettings.Qucsconv));
