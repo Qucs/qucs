50c77405694ef308f3977c50237963e9768c99f3
oxmon-2500 <blachuta@gmx.net>
2020-03-04 21:39:38 +0100

    qucs: command line enhancement for openning a project and schematics
    qucs issue #956
    Additionaly to existing options the following arguments are allowed:
    qucs [ projectPath] [ filePath1  [ filePath2 ... ] ] ]

--- qucs/qucs/qucs.cpp
+++ qucs/qucs/qucs.cpp
@@ -141,19 +141,6 @@ QucsApp::QucsApp()
   switchSchematicDoc(true);  // "untitled" document is schematic
 
   lastExportFilename = QDir::homePath() + QDir::separator() + "export.png";
-
-  // load documents given as command line arguments
-  for(int z=1; z<qApp->arguments().size(); z++) {
-    QString arg = qApp->arguments()[z];
-    QByteArray ba = arg.toLatin1();
-    const char *c_arg= ba.data();
-    if(*(c_arg) != '-') {
-      QFileInfo Info(arg);
-      QucsSettings.QucsWorkDir.setPath(Info.absoluteDir().absolutePath());
-      arg = QucsSettings.QucsWorkDir.filePath(Info.fileName());
-      gotoPage(arg);
-    }
-  }
 }
 
 QucsApp::~QucsApp()
@@ -161,6 +148,15 @@ QucsApp::~QucsApp()
   Module::unregisterModules ();
 }
 
+void QucsApp::initProjectSchematics(const QString argvProjectPath, const QStringList argvFileList){
+  initProjects(argvProjectPath);
+  // load documents given as command line arguments
+  foreach(QString f, argvFileList) {
+    QFileInfo info(f);
+    QucsSettings.QucsWorkDir.setPath(info.absoluteDir().absolutePath());
+    gotoPage(QucsSettings.QucsWorkDir.filePath(info.fileName()));
+  }
+}
 
 // #######################################################################
 // ##########                                                   ##########
@@ -353,22 +349,41 @@ void QucsApp::initView()
   //m_proxyModel->setDynamicSortFilter(true);
   // show all directories (project and non-project)
   m_homeDirModel->setFilter(QDir::NoDot | QDir::AllDirs);
-
-  // ............................................
-  QString path = QucsSettings.QucsHomeDir.absolutePath();
-  QDir ProjDir(path);
+}
+void QucsApp::initProjects(const QString argvProjsDir)
+{
+  QString path = argvProjsDir.isNull()? QucsSettings.QucsHomeDir.absolutePath() : argvProjsDir;
   // initial projects directory is the Qucs home directory
   QucsSettings.projsDir = path;
 
+  QDir projDir(path);
   // create home dir if not exist
-  if(!ProjDir.exists()) {
-    if(!ProjDir.mkdir(path)) {
+  if(!projDir.exists()) {
+    if(!projDir.mkdir(path)) {
       QMessageBox::warning(this, tr("Warning"),
-          tr("Cannot create work directory !"));
+          tr("Cannot create work directory !")+" "+path);
       return;
     }
   }
   readProjects(); // reads all projects and inserts them into the ListBox
+  
+  if (!argvProjsDir.isNull()){
+    if (argvProjsDir.endsWith("_prj/")){
+      int lx0=0;
+      for (int lx=0; lx<argvProjsDir.length()-1; ++lx){
+        if(argvProjsDir.at(lx)=='/'){
+          lx0 = lx;
+        }
+      }
+      path = argvProjsDir.mid(0, lx0); //cat "/name_prj
+    }
+    QucsSettings.projsDir = path;
+    readProjects();
+    if (argvProjsDir.endsWith("_prj/")){
+      //QucsSettings.projsDir = argvProjsDir;
+      openProject(argvProjsDir);
+    }
+  }
 }
 
 // Put all available libraries into ComboBox.
@@ -1177,6 +1192,11 @@ void QucsApp::openProject(const QString& Path)
   octave->adjustDirectory();
 
   Content->setProjPath(QucsSettings.QucsWorkDir.absolutePath());
+  //-------------------------------------------------------------------------------------------------------
+  // select (highlight) project in QListView (projects)
+  QModelIndex idx = m_homeDirModel->index(openProjName, 0); //FIXME
+  Projects->selectionModel()->select(idx, QItemSelectionModel::Select); //FIXME
+  //-------------------------------------------------------------------------------------------------------
 
   TabView->setCurrentIndex(1);   // switch to "Content"-Tab
 
