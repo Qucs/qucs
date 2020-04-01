265a8b411ed65c86028529a6d30c43ffc7f72f7e
oxmon-2500 <blachuta@gmx.net>
2020-04-01 12:05:54 +0200

    travis check 04

--- .travis_check.sh
+++ .travis_check.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 OS=$1 ;
 echo "----------------------------------------$2---------------------------" ;
-find . -name "checklibraries.*" :
+find . -name "checklibraries.*" ;
+find . -name "displaydialog.*" ;
+find . -type f -exec grep -H "displaydialog" {} \; ;
 echo "####################" ;
 ls -la qucs/qucs-lib ;
+ls -la _build/qucs/qucs-lib ;
 echo "----------------------------------------$2---------------------------" ;
