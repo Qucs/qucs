cbe1d89124ae6feb5d835eb43d67cef14434b060
oxmon-2500 <blachuta@gmx.net>
2020-04-01 14:49:19 +0200

    travis test 07

--- .travis_check.sh
+++ .travis_check.sh
@@ -4,7 +4,9 @@ echo "----------------------------------------$2---------------------------" ;
 find . -name "checklibraries.*" ;
 find . -name "displaydialog.*" ;
 find . -type f -exec grep -H "displaydialog" {} \; ;
-echo "####################" ;
+find . -type f -exec grep -H "checklibraries" {} \; ;
+echo "####################ls -la qucs/qucs-lib ;##################################################" ;
 ls -la qucs/qucs-lib ;
-ls -la _build/qucs/qucs-lib ;
+echo "########################ls -la qucs-0.0.20/_build/qucs/qucs-lib ;###########################"
+ls -la qucs-0.0.20/_build/qucs/qucs-lib ;
 echo "----------------------------------------$2---------------------------" ;
