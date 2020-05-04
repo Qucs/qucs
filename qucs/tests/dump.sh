3f3c11f345b72573f0baf694eadd5240576959df
Felix Salfelder <felix@salfelder.org>
2020-05-04 15:31:03 +0100

    debugging
    something with strange node pointers

--- qucs/tests/dump.sh
+++ qucs/tests/dump.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # testing netlisting
 # there are more tests in the test package. but not reachable from here.
-# TODO: migrate all tests
+
+# now this one seems obsolete.
 
 do_test(){
 echo "../main/qucs -l leg_sch -n -i ${examplesdir}/$1 -o $1.out" >&9
 ../main/qucs -l leg_sch -n -i ${examplesdir}/$1 -o $1.out 2>&9
 
-diff ${srcdir}/$1.ref $1.out >&9
+diff -w ${srcdir}/$1.ref $1.out >&9
 
 if [ $? -ne 0 ]; then
 	echo $1 failed
