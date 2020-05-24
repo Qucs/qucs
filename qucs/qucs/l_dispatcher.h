468b79db3efda2843ba527354493859cf94d16e0
Felix Salfelder <felix@salfelder.org>
2020-05-24 08:44:02 +0100

    dispatcher: fix static link order issue
    now, dispatchers must be static, but construct on first use.
    (they have always been static).

--- qucs/qucs/l_dispatcher.h
+++ qucs/qucs/l_dispatcher.h
@@ -1,6 +1,6 @@
-/*$Id: l_dispatcher.h 2016/03/29 al -*- C++ -*-
+/*$Id: l_dispatcher.h  al -*- C++ -*-
  * Copyright (C) 2006 Albert Davis
- * Author: Albert Davis <aldavis@gnu.org>
+ *               2020 Felix Salfelder
  *
  * This file is part of "Gnucap", the Gnu Circuit Analysis Package
  *
@@ -20,199 +20,48 @@
  * 02110-1301, USA.
  *------------------------------------------------------------------
  * dispatcher -- for dynamically loaded modules
+ *
+ * To address the issue of unknown static construction order:
+ * Must not have a constructor or destructor, or any parts that do.
+ * Also must not have any virtual functions, because the v-table has a constructor.
+ * "construction" will be done on demand by check_init.
+ *
+ * DISPATCHER is suitable for static objects only!
  */
-//testing=script 2015.01.21
+//testing=manual 2020.04.29
 #ifndef L_DISPATCHER_H
 #define L_DISPATCHER_H
-//#include "e_base.h"
-//#include "l_stlextra.h"
-//#include "u_opt.h"
-//#include "ap.h"
+// #include "e_base.h"
+// #include "l_stlextra.h"
+// #include "u_opt.h"
+// #include "ap.h"
 /*--------------------------------------------------------------------------*/
 class DISPATCHER_BASE {
-protected:
-  std::map<std::string, CKT_BASE*> * _map;
 private:
-  explicit DISPATCHER_BASE(DISPATCHER_BASE*) {unreachable();incomplete();}
+  std::map<std::string, CKT_BASE*> _map;
 public:
-  DISPATCHER_BASE() /*: _map(new std::map<std::string, CKT_BASE*>)*/ {
-    if (!_map) {
-      _map = new std::map<std::string, CKT_BASE*>;
-    }else{unreachable();
-      puts("build error: link order: constructing dispatcher that already has contents\n");
-    }
-  }
-  ~DISPATCHER_BASE() {
-#if !defined(NDEBUG)
-    for (typename std::map<std::string, CKT_BASE*>::iterator
-	 ii = _map->begin();
-	 ii != _map->end();
-	 ++ii) {
-      assert(!(ii->second));
-    }
-#endif
-    delete _map;
-    _map = NULL;
-  }
-
   typedef std::map<std::string, CKT_BASE*>::const_iterator const_iterator;
-  //class const_iterator : public std::map<std::string, CKT_BASE*>::const_iterator {};
-
-  const_iterator begin()const		{assert(_map); return _map->begin();}
-  const_iterator end()const		{assert(_map); return _map->end();}
-
-  CKT_BASE* operator[](std::string s) {
-    assert(_map);
-    CKT_BASE* rv = (*_map)[s];
-    if (!rv && OPT::case_insensitive) {
-      assert(false);
-      notstd::to_lower(&s);
-      rv = (*_map)[s];
-    }else{
-    }
-    return rv;
-  }
-
-  void uninstall(CKT_BASE* p) {
-    assert(_map);
-    for (typename std::map<std::string, CKT_BASE*>::iterator
-	 ii = _map->begin();
-	 ii != _map->end();
-	 ++ii) {
-      if (ii->second == p) {
-	ii->second = NULL;
-      }else{
-      }
-    }
-#if !defined(NDEBUG)
-    for (typename std::map<std::string, CKT_BASE*>::iterator
-	 ii = _map->begin();
-	 ii != _map->end();
-	 ++ii) {
-      assert(ii->second != p);
-    }
-#endif
-  }
-
-  void uninstall(const std::string& s) {untested();
-    assert(_map);
-    // loop over all keys, separated by '|'
-    for (std::string::size_type			// bss: begin sub-string
-	 bss = 0, ess = s.find('|', bss);	// ess: end sub-string
-	 bss != std::string::npos;
-	 bss = (ess != std::string::npos) ? ess+1 : std::string::npos,
-	   ess = s.find('|', bss)) {untested();
-      std::string name = s.substr(bss,
-		(ess != std::string::npos) ? ess-bss : std::string::npos);
-      if (name == "") {untested();
-	// quietly ignore empty string
-      }else if ((*_map)[name]) {untested();
-	// delete, try to get back the old one
-	int ii = 0;
-	std::string save_name = name + ":0";
-	for (ii = 0; (*_map)[save_name]; ++ii) {untested();
-	  save_name = name + ":" + to_string(ii);
-	}
-	if (ii > 1) {untested();
-	  save_name = name + ":" + to_string(ii-2);
-	  (*_map)[name] = (*_map)[save_name];
-	  (*_map)[save_name] = NULL;
-	  error(bWARNING, "restoring " + save_name + " as " + name + "\n");
-	}else{untested();
-	  (*_map)[name] = NULL;
-	}
-      }else{untested();
-	error(bWARNING, name + ": not installed, doing nothing\n");
-      }
-    }
-  }
-};
-/*--------------------------------------------------------------------------*/
-template <class TT>
-class INTERFACE DISPATCHER : public DISPATCHER_BASE {
+private:
+  DISPATCHER_BASE(DISPATCHER_BASE const&) {unreachable();}
 public:
-  void install(const std::string& s, TT* p) {
-    assert(p);
-    assert(s.find(',', 0) == std::string::npos);
-    if (!_map) {unreachable();
-      puts("build error: link order: dispatcher not yet constructed\n");
-      _map = new std::map<std::string, CKT_BASE*>;
-    }else{
-    }
-    trace0(s.c_str());
-    // loop over all keys, separated by '|'
-    for (std::string::size_type			// bss: begin sub-string
-	 bss = 0, ess = s.find('|', bss);	// ess: end sub-string
-	 bss != std::string::npos;
-	 bss = (ess != std::string::npos) ? ess+1 : std::string::npos,
-	   ess = s.find('|', bss)) {
-      std::string name = s.substr(bss,
-		(ess != std::string::npos) ? ess-bss : std::string::npos);
-      trace2(name.c_str(), bss, ess);
-      if (name == "") {untested();
-	// quietly ignore empty string
-      }else if ((*_map)[name]) {
-	// duplicate .. stash the old one so we can get it back
-	error(bWARNING, name + ": already installed, replacing\n");
-	std::string save_name = name + ":0";
-	for (int ii = 0; (*_map)[save_name]; ++ii) {untested();
-	  save_name = name + ":" + to_string(ii);
-	}
-	(*_map)[save_name] = (*_map)[name];
-	error(bWARNING, "stashing as " + save_name + "\n");
-      }else{
-	// it's new, just put it in
-      }
-      (*_map)[name] = p;
-    }
-  }
-
-  TT* operator[](std::string s) {
-    assert(_map);
-    CKT_BASE* rv = (*_map)[s];
-    if (!rv && OPT::case_insensitive) {
-      assert(false);
-      notstd::to_lower(&s);
-      rv = (*_map)[s];
-    }else{
-    }
-    return prechecked_cast<TT*>(rv);
-  }
-
-#if 0
-  TT* operator[](CS& cmd) {
-    unsigned here = cmd.cursor();
-    std::string s;
-    cmd >> s;
-    //------------------------
-    TT* p = (*this)[s];
-    //------------------------
-    if (!p) {
-      cmd.reset(here);
-    }else{
-    }
-    return p;
-  }
-#endif
-
-  TT* clone(std::string s) {
-    TT* proto = (*this)[s];
-    if (proto) {
-      TT* c=prechecked_cast<TT*>(proto->clone());
-      assert(c);
-      return c;
-    }else{
-      return NULL;
-    }
-  }
-
+  explicit DISPATCHER_BASE() {}
+  ~DISPATCHER_BASE();
+public:
+  const_iterator begin()const		{return _map.begin();}
+  const_iterator end()const		{return _map.end();}
+public:
+  CKT_BASE* operator[](std::string s);
+private:
+  void      uninstall(CKT_BASE* p);
+  void	    install(const std::string& s, CKT_BASE* p);
+public:
   class INSTALL {
   private:
     const std::string _name;
-    DISPATCHER<TT>* _d;
-    TT* _p;
+    DISPATCHER_BASE* _d;
+    CKT_BASE* _p;
   public:
-    INSTALL(DISPATCHER<TT>* d, const std::string& name, TT* p) :
+    INSTALL(DISPATCHER_BASE* d, const std::string& name, CKT_BASE* p) :
       _name(name),
       _d(d),
       _p(p)
@@ -223,11 +72,170 @@ public:
     }
 
     ~INSTALL() {
-      //_d->uninstall(_name);
       _d->uninstall(_p);
     }
   };
 };
+/*--------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------*/
+// thin wrapper controlling initialisation.
+// only static instances allowed.
+template <class TT>
+class INTERFACE DISPATCHER {
+public:
+  typedef typename DISPATCHER_BASE::const_iterator const_iterator;
+  typedef typename DISPATCHER_BASE::INSTALL INSTALL;
+public:
+  TT* operator[](std::string s);
+/* QUCS  TT* operator[](CS& cmd); */
+  TT* clone(std::string s);
+public: // forward to BASE
+  DISPATCHER_BASE* operator&() {
+    check_init();
+    return _base;
+  }
+  const_iterator begin()const {
+    assert(_base);
+    return _base->begin();
+  }
+  const_iterator end()const {
+    assert(_base);
+    return _base->end();
+  }
+private:
+  void check_init();
+  DISPATCHER_BASE* _base;
+};
+/*--------------------------------------------------------------------------*/
+// not sure if this is needed, could be put wherever used.
+// just drop it?
+template <class TT>
+class INTERFACE D_DISPATCHER : public DISPATCHER_BASE {
+};
+/*--------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------*/
+inline DISPATCHER_BASE::~DISPATCHER_BASE()
+{ untested();
+#if !defined(NDEBUG)
+  for (typename std::map<std::string, CKT_BASE*>::iterator
+      ii = _map.begin();  ii != _map.end();  ++ii) { untested();
+    assert(!(ii->second));
+  }
+#endif
+}
+/*--------------------------------------------------------------------------*/
+inline CKT_BASE* DISPATCHER_BASE::operator[](std::string s)
+{
+  CKT_BASE* rv = _map[s];
+  if (!rv && OPT::case_insensitive) {
+    notstd::to_lower(&s);
+    rv = _map[s];
+  }else{
+  }
+  return rv;
+}
+/*--------------------------------------------------------------------------*/
+inline void DISPATCHER_BASE::uninstall(CKT_BASE* p)
+{
+  for (typename std::map<std::string, CKT_BASE*>::iterator
+	 ii = _map.begin();  ii != _map.end();  ++ii) {
+    if (ii->second == p) {
+      ii->second = NULL;
+    }else{
+    }
+  }
+#if !defined(NDEBUG)
+  for (typename std::map<std::string, CKT_BASE*>::iterator
+	 ii = _map.begin();  ii != _map.end();  ++ii) {
+    assert(ii->second != p);
+  }
+#endif
+}
+/*--------------------------------------------------------------------------*/
+template<class TT>
+void DISPATCHER<TT>::check_init()
+{
+  if (!_base) {
+    _base = new DISPATCHER_BASE;
+  }else{
+  }
+}
+/*--------------------------------------------------------------------------*/
+inline void DISPATCHER_BASE::install(const std::string& s, CKT_BASE* p)
+{
+  assert(s.find(',', 0) == std::string::npos);
+  trace0(s.c_str());
+  // loop over all keys, separated by '|'
+  for (std::string::size_type			// bss: begin sub-string
+	 bss = 0, ess = s.find('|', bss);	// ess: end sub-string
+       bss != std::string::npos;
+       bss = (ess != std::string::npos) ? ess+1 : std::string::npos,
+	 ess = s.find('|', bss)) {
+    std::string name = s.substr(bss, (ess != std::string::npos) ? ess-bss : std::string::npos);
+    trace2(name.c_str(), bss, ess);
+    if (name == "") {untested();
+      // quietly ignore empty string
+    }else if (_map[name]) {untested();
+      // duplicate .. stash the old one so we can get it back
+      error(bWARNING, name + ": already installed, replacing\n");
+      std::string save_name = name + ":0";
+      for (int ii = 0; _map[save_name]; ++ii) {untested();
+	save_name = name + ":" + to_string(ii);
+      }
+      _map[save_name] = _map[name];
+      error(bWARNING, "stashing as " + save_name + "\n");
+    }else{
+      // it's new, just put it in
+    }
+    _map[name] = p;
+  }
+}
+/*--------------------------------------------------------------------------*/
+template <class TT>
+TT* DISPATCHER<TT>::operator[](std::string s)
+{
+  check_init(); // main.cc gets here first (??)
+  CKT_BASE* rv = (*_base)[s];
+  if (!rv && OPT::case_insensitive) {
+    notstd::to_lower(&s);
+    rv = (*_base)[s];
+  }else{
+  }
+  TT* t=prechecked_cast<TT*>(rv);
+  assert(t || !rv);
+  return(t);
+}
+/*--------------------------------------------------------------------------*/
+#if 0 // qucs
+template <class TT>
+TT* DISPATCHER<TT>::operator[](CS& cmd)
+{
+  unsigned here = cmd.cursor();
+  std::string s;
+  cmd >> s;
+  //------------------------
+  TT* p = (*this)[s];
+  //------------------------
+  if (!p) {
+    cmd.reset(here);
+  }else{
+  }
+  return p;
+}
+#endif
+/*--------------------------------------------------------------------------*/
+template <class TT>
+TT* DISPATCHER<TT>::clone(std::string s)
+{
+  TT* proto = (*this)[s];
+  if (proto) {
+    TT* c=prechecked_cast<TT*>(proto->clone());
+    assert(c);
+    return c;
+  }else{untested();
+    return NULL;
+  }
+}
 /*--------------------------------------------------------------------------*/
 /*--------------------------------------------------------------------------*/
 #endif
