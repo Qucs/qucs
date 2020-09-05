a5ddaf56d65ee3883a06658b050b1c8f0217e4f6
Felix Salfelder <felix@salfelder.org>
2020-09-05 20:07:08 +0100

    fix dots and catch up with test output
    not sure about "prep" state in sckt symbols...

--- qucs/tests/ref/AC_SW_resonance.prj_resonance.sch.v
+++ qucs/tests/ref/AC_SW_resonance.prj_resonance.sch.v
@@ -1,8 +1,8 @@
 # Qucs 0.1.0  ../../tests/AC_SW_resonance.prj/resonance.sch
 
-Vac #(.U(1 V), .f(1 GHz), .Phase(0), .Theta(0)) V1(_net7, _net1);
+Vac #(.U(1 V), .f(1 GHz), .Phase(0), .Theta(0)) V1(_net7, gnd);
 GND #() *(gnd);
-GND #() *(_net1);
+GND #() *(gnd);
 R #(.R(R_par), .Temp(26.85), .Tc1(0.0), .Tc2(0.0), .Tnom(26.85), .Symbol(european)) R1(_net4, _net7);
 C #(.C(2 pF), .V(), .Symbol(neutral)) C1(gnd, Voltage);
 L #(.L(13 nH), .I()) L1(Voltage, _net4);
