5ea07bc7fb8c3eec2310451b443cd0cecdefe51b
Felix Salfelder <felix@salfelder.org>
2020-10-07 00:00:00 +0100

    deduplicate swap

--- qucs/qucs/wiredialog.cpp
+++ qucs/qucs/wiredialog.cpp
@@ -20,6 +20,7 @@
 #include <QLabel>
 #include <QRegExpValidator>
 #include <QVBoxLayout>
+#include "swap.h"
 
 WireDialog::WireDialog(QucsDoc* d) : SchematicDialog(d)
 {
@@ -158,39 +159,6 @@ void WireDialog::reject()
   slotButtCancel();
 }
 
-#include <QUndoCommand>
-
-class SwapSymbolCommand : public QUndoCommand {
-  SwapSymbolCommand() = delete;
-  SwapSymbolCommand(SwapSymbolCommand const&) = delete;
-public:
-  ~SwapSymbolCommand(){
-    delete _elt;
-  }
-  SwapSymbolCommand(ElementGraphics* g, Element* e)
-    : _gfx(g), _elt(e)
-  {
-    assert(e);
-    setText("Swap Element " + e->label());
-  }
-private:
-  void undo(){
-    redo();
-  }
-  void redo(){
-    assert(_gfx);
-    _gfx->hide();
-    if(1){
-      Element* tmp = _gfx->detachElement();
-      _gfx->attachElement(_elt);
-      _elt = tmp;
-    }
-    _gfx->show();
-  }
-private:
-  ElementGraphics* _gfx;
-  Element* _elt;
-};
 // -------------------------------------------------------------------------
 // Is called, if the "Apply"-button is pressed.
 void WireDialog::slotApplyInput()
