0d6588e33457066226ab4f81866597d0dc9fe7d3
Felix Salfelder <felix@salfelder.org>
2020-10-21 00:00:00 +0100

    Iac

--- qucs/qucs/components/ampere_ac.h
+++ qucs/qucs/components/ampere_ac.h
@@ -21,12 +21,5 @@
 #include "component.h"
 
 
-class Ampere_ac : public Component  {
-public:
-  Ampere_ac();
-  ~Ampere_ac();
-  Component* newOne();
-  static Element* info(QString&, char* &, bool getNewOne=false);
-};
 
 #endif
