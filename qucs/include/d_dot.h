e19999bf61919c950ea4d9cc92c6349d41a45173
Felix Salfelder <felix@salfelder.org>
2020-10-27 00:00:00 +0000

    missing files

---
+++ qucs/include/d_dot.h
@@ -0,0 +1,47 @@
+/*$Id: d_dot.h 2014/11/23$ -*- C++ -*-
+ * Copyright (C) 2007 Albert Davis
+ * Author: Albert Davis <aldavis@gnu.org>
+ *
+ * This file is part of "Gnucap", the Gnu Circuit Analysis Package
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *------------------------------------------------------------------
+ */
+#ifndef D_DOT_H
+#define D_DOT_H
+//#include "e_card.h"
+#include "element.h"
+/*--------------------------------------------------------------------------*/
+class DEV_DOT : public Element {
+private:
+  std::string	_s;
+  explicit DEV_DOT(const DEV_DOT& p) :Element(p), _s(p._s) {}//{set_constant(true);}
+public:
+  explicit	DEV_DOT()		:Element() {}//{set_constant(true);}
+private: // override virtual
+  std::string   value_name()const	{untested();return "";}
+  char		id_letter()const	{untested();return '\0';}
+  std::string	dev_type()const		{untested();return "dotcard";}
+  Element*		clone()const		{return new DEV_DOT(*this);}
+  void paint(ViewPainter*) const override{}
+public:
+  void set(const std::string& S) {_s = S;}
+  const std::string& s()const {untested();return _s;}
+};
+/*--------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------*/
+#endif
+// vim:ts=8:sw=2:noet:
---
+++ qucs/qucs/legacy/paramset.cpp
@@ -0,0 +1,96 @@
+/***************************************************************************
+    copyright            : 2020 Felix Salfelder
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+// paramset style models from .lib files.
+
+#include "qucs.h"
+#include "schematic_doc.h"
+#include "misc.h"
+#include "globals.h"
+
+#include <limits.h>
+
+#include <QTextStream>
+#include <QDir>
+#include <QRegExp>
+#include <QDebug>
+
+namespace{
+
+//paramset hack; takes a copy of a Symbol...
+class LibParamset : public Symbol{
+public:
+	explicit LibParamset():Symbol(){
+		setTypeName("LibParamset"); // really?
+	}
+	LibParamset( LibParamset const& l) : Symbol(l), _proto(nullptr){}
+	~LibParamset() { delete _proto; }
+
+private: // Element
+	Symbol* clone()const override{
+		return new LibParamset(*this);
+	}
+
+private: // Symbol
+	Symbol* clone_instance()const override{
+		untested();
+		assert(_proto);
+		return prechecked_cast<Symbol*>(_proto->clone());
+	}
+	void setTypeName(std::string const& n) override{
+		trace1("paramset type", n);
+		_proto = symbol_dispatcher.clone(n);
+		if(_proto){
+		}else{
+			// list-entries
+		}
+	}
+	unsigned numPorts() const override{ incomplete(); return 0; }
+	Port& port(unsigned i) override {unreachable(); return *new Port();}
+	pos_t portPosition(unsigned) const override{
+		incomplete(); return pos_t(0,0);
+	}
+	void setParameter(std::string const& n, std::string const& v) override{
+		if(_proto){
+			_proto->setParameter(n, v);
+		}else{
+			unreachable();
+		}
+		// Symbol::setParameter(n, v);
+	}
+	void setParameter(unsigned n, std::string const& v) override{
+		// BUG: proto is not shared.
+		if(_proto){
+			trace3("paramset fwd", label(), n, v);
+
+			_proto->setParameter(n, v);
+		}else{
+		}
+	}
+	// used in list-entries
+	std::string paramValue(std::string const& n) const override{
+		if (n=="$tx"){
+			return "0";
+		}else if (n=="$ty"){
+			return "0";
+		}else{
+			return Symbol::paramValue(n);
+		}
+	}
+
+private:
+	Symbol* _proto; // BUG. common.
+}D;
+static Dispatcher<Symbol>::INSTALL p2(&symbol_dispatcher, "LegacyParamset", &D);
+
+} //namespace
