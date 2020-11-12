21fb8f71c3d02baf2067c2fb5963c4cc04d17c79
Felix Salfelder <felix@salfelder.org>
2020-11-12 00:00:00 +0000

    DC_AC_*prj

---
+++ qucs/tests/ref/DC_AC_active_bp.prj_active_bp.sch.v
@@ -0,0 +1,12 @@
+// Qucs
+
+OpAmp #(.G(1e6), .Umax(15 V)) OP1(gnd, _net11, Output);
+GND #() *(gnd);
+GND #() *(gnd);
+GND #() *(gnd);
+Vac #(.U(1 V), .f(1 GHz), .Phase(0), .Theta(0)) V1(_net14, gnd);
+R #(.R(390), .Temp(26.85), .Tc1(0.0), .Tc2(0.0), .Tnom(26.85), .Symbol(european)) R3(_net14, _net8);
+R #(.R(5.6k), .Temp(26.85), .Tc1(0.0), .Tc2(0.0), .Tnom(26.85), .Symbol(european)) R2(gnd, _net8);
+C #(.C(47n), .V(), .Symbol(neutral)) C1(_net8, _net11);
+C #(.C(47n), .V(), .Symbol(neutral)) C2(_net8, Output);
+R #(.R(3.9k), .Temp(26.85), .Tc1(0.0), .Tc2(0.0), .Tnom(26.85), .Symbol(european)) R1(_net11, Output);
---
+++ qucs/tests/ref/DC_AC_notch.prj_notch.sch.v
@@ -0,0 +1,15 @@
+// Qucs
+
+GND #() *(gnd);
+C #(.C(1.01u), .V(), .Symbol(neutral)) C1(_net9, _net15);
+R #(.R(10k), .Temp(26.85), .Tc1(0.0), .Tc2(0.0), .Tnom(26.85), .Symbol(european)) R5(In, Out);
+R #(.R(1k), .Temp(26.85), .Tc1(0.0), .Tc2(0.0), .Tnom(26.85), .Symbol(european)) R3(_net10, _net18);
+R #(.R(1k), .Temp(26.85), .Tc1(0.0), .Tc2(0.0), .Tnom(26.85), .Symbol(european)) R4(_net5, _net18);
+R #(.R(1k), .Temp(26.85), .Tc1(0.0), .Tc2(0.0), .Tnom(26.85), .Symbol(european)) R2(_net15, _net10);
+C #(.C(1u), .V(), .Symbol(neutral)) C2(_net5, Out);
+OpAmp #(.G(1e6), .Umax(15 V)) OP1(_net5, _net9, _net15);
+OpAmp #(.G(1e6), .Umax(15 V)) OP2(_net5, _net10, _net18);
+Eqn #(.Gain(dB(Out.v/In.v)), .Export(yes)) Eqn1();
+R #(.R(10k), .Temp(26.85), .Tc1(0.0), .Tc2(0.0), .Tnom(26.85), .Symbol(european)) R1(gnd, _net9);
+GND #() *(gnd);
+Vac #(.U(1uV), .f(1 GHz), .Phase(0), .Theta(0)) V1(In, gnd);
