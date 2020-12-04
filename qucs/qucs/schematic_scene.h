ed2b914bb5f6e7b3ac725fa31d5f4a5e8d02fffe
Felix Salfelder <felix@salfelder.org>
2020-12-04 00:00:00 +0000

    add ordered parameters to lib instances.
    somehow need to move this to "paramset" style instances...
    also "fix" Rectangle

--- qucs/qucs/schematic_scene.h
+++ qucs/qucs/schematic_scene.h
@@ -73,8 +73,10 @@ public:
 
 public: // wrap items
   QList<ElementGraphics*> selectedItems() const;
-  QList<ElementGraphics*> items() const;
-  QList<ElementGraphics*> items(QRectF const&) const;
+  QList<ElementGraphics*> items();
+  QList<ElementGraphics const*> items() const;
+  QList<ElementGraphics*> items(QRectF const&);
+  QList<ElementGraphics const*> items(QRectF const&) const;
   QList<ElementGraphics*> items(QPointF const& pos,
                                 Qt::ItemSelectionMode mode=Qt::IntersectsItemShape,
                                 Qt::SortOrder order = Qt::DescendingOrder) const;
