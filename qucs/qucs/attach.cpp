c189519ace3d81c0663cfd2ae97b380449227ba2
Felix Salfelder <felix@salfelder.org>
2020-12-06 00:00:00 +0000

    revisit attach.cpp
    needs to be a command plugin
    
    TODO:
    - install
    - use from main.cc

--- qucs/qucs/attach.cpp
+++ qucs/qucs/attach.cpp
@@ -1,16 +1,12 @@
 /***************************************************************************
-                                attach.cpp
-                                ----------
-    begin                : yes
-    copyright            : 2016 by Felix Salfelder
-    email                : felix@salfelder.org
+copyright            : 2016, 2020 Felix Salfelder
  ***************************************************************************/
 
 /***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
+ *   the Free Software Foundation; either version 3 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
@@ -23,40 +19,44 @@
 #include <ctype.h>
 
 #include "platform.h"
+#include "command.h"
+#include "ap.h"
 #include "io_trace.h"
 
-class plugins{
-  public:
-    ~plugins(){
-      for (std::map<std::string, void*>::iterator ii = attach_list.begin();
-	  ii != attach_list.end(); ++ii) {
-	void* m=ii->second;
-	if(m){
-	  dlclose(m);
-	}
+class plugins : public Command{
+public:
+  ~plugins(){
+    for (std::map<std::string, void*>::iterator ii = attach_list.begin();
+	ii != attach_list.end(); ++ii) {
+      void* m=ii->second;
+      if(m){
+	dlclose(m);
       }
     }
+  }
 
-    void attach(std::string what){
-      // RTLD_NOW means to resolve symbols on loading
-      // RTLD_LOCAL means symbols defined in a plugin are local
-      int dl_scope = RTLD_LOCAL;
-      int check = RTLD_NOW;
-      void* handle;
+  void attach(std::string what){
+    // RTLD_NOW means to resolve symbols on loading
+    // RTLD_LOCAL means symbols defined in a plugin are local
+    int dl_scope = RTLD_LOCAL;
+    int check = RTLD_NOW;
+    void* handle;
 
-      handle = dlopen((what).c_str(), check | dl_scope);
-      if (handle) {
-	attach_list[what] = handle;
-      }else{
-	std::cerr << "failed to attach " << what << " (" << errno << ")\n";
-	std::cerr << dlerror() << "\n";
-	exit(1); // temporary, should actually throw.
-//	throw Exception_something(dlerror());
-      }
+    handle = dlopen((what).c_str(), check | dl_scope);
+    if (handle) {
+      attach_list[what] = handle;
+    }else{
+      std::cerr << "failed to attach " << what << " (" << errno << ")\n";
+      std::cerr << dlerror() << "\n";
+      exit(1); // temporary, should actually throw.
+      //	throw Exception_something(dlerror());
     }
+  }
 
-  private:
-    std::map<std::string, void*> attach_list;
+private:
+  virtual void do_it(istream_t&, SchematicModel*) {incomplete();}
+private:
+  std::map<std::string, void*> attach_list;
 } my_plugins;
 
 void attach(const char* what)
@@ -64,5 +64,3 @@ void attach(const char* what)
   my_plugins.attach(std::string(what));
 }
 
-
-// vim:ts=8:sw=2:noet
