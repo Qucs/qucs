f9dbfa5c6d68c44eb0cb26560b9df04547f7d736
Martin <martin.marmsoler@gmail.com>
2020-12-22 18:07:20 +0100

    move install dependencies into its own file to get a cleaner main build file

---
+++ qucs/cmake/QucsApplication/compileAndInstallDependencies.cmake
@@ -0,0 +1,40 @@
+# install dependencies of the qucs application if desired.
+# Used for getting a debug environment which is ready to use
+# The QUCS_PLUGPATH will be changed and all dependency libraries
+# get copied into this path
+
+if (${CompileDependencies})
+	set(LIB_PATH_TEMP ${CMAKE_BINARY_DIR}/libraries)
+	file(MAKE_DIRECTORY ${LIB_PATH_TEMP})
+
+	add_compile_definitions(QUCS_PLUGPATH="${CMAKE_BINARY_DIR}/libraries")
+	#message("QUCS_PLUGPATH: ${CMAKE_BINARY_DIR}/libraries")
+
+	# copy the library into a common folder for all dependencies of qucs
+	# so they can be used independently to the installed ones
+	add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/../../legacy legacy)
+	#set_target_properties(legacy PROPERTIES RUNTIME_OUTPUT_DIRECTORY_DEBUG ${LIB_PATH_TEMP}) # this does not work
+	if(EXISTS ${CMAKE_BINARY_DIR}/legacy/liblegacy.so)
+		file(COPY ${CMAKE_BINARY_DIR}/legacy/liblegacy.so DESTINATION ${LIB_PATH_TEMP})
+	endif()
+	if (NOT TARGET qucsator)
+		   add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/../../../qucsator qucsator)
+	endif()
+	#set_target_properties(libqucsator PROPERTIES RUNTIME_OUTPUT_DIRECTORY_DEBUG ${LIB_PATH_TEMP})
+	if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/qucsator/src/libqucsator.so)
+		file(COPY ${CMAKE_CURRENT_BINARY_DIR}/qucsator/src/libqucsator.so DESTINATION ${LIB_PATH_TEMP})
+	endif()
+
+	add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/../../legacy/components legacy/components)
+	#set_target_properties(components PROPERTIES RUNTIME_OUTPUT_DIRECTORY_DEBUG ${LIB_PATH_TEMP})
+	set_target_properties(components PROPERTIES OUTPUT_NAME legacy-components)
+	if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/legacy/components/liblegacy-components.so)
+		file(COPY ${CMAKE_CURRENT_BINARY_DIR}/legacy/components/liblegacy-components.so DESTINATION ${LIB_PATH_TEMP})
+	endif()
+
+	add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/../../legacy/paintings legacy/paintings)
+	#set_target_properties(paintings PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${LIB_PATH_TEMP})
+	if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/legacy/paintings/libpaintings.so)
+		file(COPY ${CMAKE_CURRENT_BINARY_DIR}/legacy/paintings/libpaintings.so DESTINATION ${LIB_PATH_TEMP})
+	endif()
+endif()
