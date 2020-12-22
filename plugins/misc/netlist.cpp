282eb7343ee964d411c5aa16877dc1ef37230ce7
Felix Salfelder <felix@salfelder.org>
2020-12-22 00:00:00 +0000

    generic netlister & test

--- plugins/misc/netlist.cpp
+++ plugins/misc/netlist.cpp
@@ -13,6 +13,11 @@
 
 // print flat netlists
 /* -------------------------------------------------------------------------------- */
+#include "command.h"
+#include "globals.h"
+#include "settings.h"
+#include "language.h"
+/* -------------------------------------------------------------------------------- */
 namespace {
 /* -------------------------------------------------------------------------------- */
 class Netlister : public Command{
@@ -20,40 +25,58 @@ public:
 	typedef std::map<std::string, Element const*> declmap;
 	Netlister(Netlister const&) = delete;
 public:
-	explicit Netlister() : _lang(nullptr) {}
+	explicit Netlister() {}
 private: // Command
 	void do_it(istream_t&, SchematicModel*) override;
 
-private: // legacy implementation
+private: // implementation
+	void resolve(ostream_t&, SchematicModel*, declmap&) const{incomplete();}
 	void printDeclarations(ostream_t& d,
-			declmap& declarations) const;
+			declmap& declarations) const{incomplete();}
+	void printMain(ostream_t& stream, SchematicModel const*,
+			DocumentLanguage const*) const;
 }c0;
 static Dispatcher<Command>::INSTALL p1(&command_dispatcher, "netlist", &c0);
 /* -------------------------------------------------------------------------------- */
-void Netlist::do_it(istream_t& cs, SchematicModel* m)
+void Netlister::do_it(istream_t& cs, SchematicModel* m)
 {
 	declmap declarations;
-	std::string fn;
+	std::string fn, language;
+	bool emit_decl = false;
+
+	while(
+		Get(cs, "{l}ang", &language)
+		);
+
 	cs >> fn;
 
-	Get(cs, "{l}ang", &language);
+	if(fn==""){
+		fn="/dev/stdout";
+	}else{
+	}
 
 	QFile NetlistFile(QString::fromStdString(fn));
 	if(!NetlistFile.open(QIODevice::WriteOnly | QFile::Truncate)) { untested();
 		fprintf(stderr, "Failed to open %s\n", fn.c_str());
 		incomplete();
 		throw Exception("...");
-	}else{
+	}else{ untested();
 	}
 	ostream_t Stream(&NetlistFile);
 
-	Language const* lang = nullptr;
-	if(language==""){
-		Language = tQucsSettings::Language;
-	}else{
+	DocumentLanguage const* lang = nullptr;
+	if(language==""){ untested();
+		lang = tQucsSettings::language;
+	}else{ untested();
 		lang = language_dispatcher[language];
+		trace2("netlist", lang, language);
+		assert(language_dispatcher[language]);
 	}
-	assert(lang);
+
+	if(lang){
+	}else{
+		throw Exception("no lang: " + language);
+	}
 
 	// global symbols are not in this model, need to find them and
 	// emit separately.
@@ -64,7 +87,42 @@ void Netlist::do_it(istream_t& cs, SchematicModel* m)
 	}
 
 	Stream << '\n';
-	createNetlist(Stream, m);
+	printMain(Stream, m, lang);
+}
+/* -------------------------------------------------------------------------------- */
+void Netlister::printMain(ostream_t& stream,
+		SchematicModel const* scope_, DocumentLanguage const* lang) const
+{
+	assert(scope_);
+	auto s = scope_->find_("main");
+	assert(s!=scope_->end());
+	assert(*s);
+	auto scope = (*s)->scope();
+
+	stream << "netlisting...\n";
+
+	QString Time;
+	assert(scope);
+	for(auto it_ : *scope){
+		auto pc = dynamic_cast<Symbol const*>(it_);
+		if(pc){
+		}else{
+			incomplete();
+			continue;
+		}
+
+		// if dynamic_cast<Label*>
+		//   ignore
+		if(dynamic_cast<Conductor const*>(pc)){
+			// possibly a wire.
+		}else if(pc->typeName()=="GND"){
+			// qucsator hack, just ignore.
+		}else if(pc->typeName()=="NodeLabel"){ untested();
+			// qucsator hack, just ignore.
+		}else{
+			lang->printItem(pc, stream);
+		}
+	} // components
 }
 /* -------------------------------------------------------------------------------- */
 } // namespace
