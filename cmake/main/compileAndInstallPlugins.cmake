2b5188afed61c0c49212044718342e5c41532e5a
Martin Marmsoler <martin.marmsoler@gmail.com>
2021-01-07 16:59:36 +0100

    use a custom target to install the libraries. So after building the files get copied and no more cmake must be run

--- cmake/main/compileAndInstallPlugins.cmake
+++ cmake/main/compileAndInstallPlugins.cmake
@@ -11,33 +11,34 @@ if (${CompileDependencies})
 
     # copy the library into a common folder for all dependencies of qucs
     # so they can be used independently to the installed ones
-    #set_target_properties(legacy PROPERTIES RUNTIME_OUTPUT_DIRECTORY_DEBUG ${LIB_PATH_TEMP}) # this does not work
-    if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/legacy/liblegacy.so)
-        file(COPY ${CMAKE_CURRENT_BINARY_DIR}/legacy/liblegacy.so DESTINATION ${LIB_PATH_TEMP})
-    endif()
+	add_custom_target(liblegacyInstall ALL
+		DEPENDS legacy
+		COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_BINARY_DIR}/legacy/liblegacy.so ${LIB_PATH_TEMP}
+		)
 
-	if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/legacy/qucsator/libqucsator.so)
-		file(COPY ${CMAKE_CURRENT_BINARY_DIR}/legacy/qucsator/libqucsator.so DESTINATION ${LIB_PATH_TEMP}/legacy)
-    endif()
+	add_custom_target(libqucsatorInstall ALL
+		DEPENDS qucsator
+		COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_BINARY_DIR}/legacy/qucsator/libqucsator.so ${LIB_PATH_TEMP}/legacy/libqucsator.so
+		)
 
-    #set_target_properties(components PROPERTIES RUNTIME_OUTPUT_DIRECTORY_DEBUG ${LIB_PATH_TEMP})
-    if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/legacy/components/libcomponents.so)
-        file(COPY ${CMAKE_CURRENT_BINARY_DIR}/legacy/components/libcomponents.so DESTINATION ${LIB_PATH_TEMP}/legacy)
-    endif()
+	add_custom_target(libcomponentsInstall ALL
+		DEPENDS components
+		COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_BINARY_DIR}/legacy/components/libcomponents.so ${LIB_PATH_TEMP}/legacy/libcomponents.so
+		)
 
-    #set_target_properties(paintings PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${LIB_PATH_TEMP})
-    if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/legacy/paintings/libpaintings.so)
-        file(COPY ${CMAKE_CURRENT_BINARY_DIR}/legacy/paintings/libpaintings.so DESTINATION ${LIB_PATH_TEMP}/legacy)
-    endif()
+	add_custom_target(libpaintingsInstall ALL
+		DEPENDS paintings
+		COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_BINARY_DIR}/legacy/paintings/libpaintings.so ${LIB_PATH_TEMP}/legacy/libpaintings.so
+		)
 
-    #set_target_properties(paintings PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${LIB_PATH_TEMP})
-    if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/legacy/libfiles/liblibfiles.so)
-        file(COPY ${CMAKE_CURRENT_BINARY_DIR}/legacy/libfiles/liblibfiles.so DESTINATION ${LIB_PATH_TEMP}/legacy)
-    endif()
+	add_custom_target(liblibfilesInstall ALL
+		DEPENDS libfiles
+		COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_BINARY_DIR}/legacy/libfiles/liblibfiles.so ${LIB_PATH_TEMP}/legacy/liblibfiles.so
+		)
 
-     #set_target_properties(misc PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${LIB_PATH_TEMP})
-     if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/plugins/misc/libmisc.so)
-        file(COPY ${CMAKE_CURRENT_BINARY_DIR}/plugins/misc/libmisc.so DESTINATION ${LIB_PATH_TEMP}/plugins)
-     endif()
+	add_custom_target(libmiscInstall ALL
+		DEPENDS misc
+		COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_BINARY_DIR}/plugins/misc/libmisc.so ${LIB_PATH_TEMP}/plugins/libmisc.so
+		)
 endif()
 
