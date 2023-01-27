2925a4ed4eababa9d6ad2c1b4f0185d35cead6db
Yaakov Selkowitz <yselkowi@redhat.com>
2023-01-27 00:02:20 -0500

    Fix build with GCC 13
    Signed-off-by: Yaakov Selkowitz <yselkowi@redhat.com>

--- qucs/qucs/schematic_element.cpp
+++ qucs/qucs/schematic_element.cpp
@@ -14,6 +14,7 @@
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
+#include <stdint.h>
 #include <stdlib.h>
 #include <limits.h>
 
