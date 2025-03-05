2bad7911d46a39dbd3dfa8aed9ff12ced1a1a055
David Lanzendörfer <leviathan@libresilicon.com>
2025-03-05 16:11:50 +0000

    Reading Verilog: File type switch
    Introducing a switch which checks whether the file
    is contains the legacy format or the new Verilog
    schematic format and then either executes the legacy
    reader or the yet to be implemented Verilog reader.
    
    If the file format is unknown, and error will be thrown.

--- qucs/schematic_file_read.cpp
+++ qucs/schematic_file_read.cpp
@@ -1,11 +1,10 @@
-#define TRACE_FUNCTION_CALLS 0
+#define TRACE_FUNCTION_CALLS 1
 
 #ifdef HAVE_CONFIG_H
 # include <config.h>
 #endif
 #include <iostream>
 
-#include <QtCore>
 #include <QMessageBox>
 #include <QDir>
 #include <QStringList>
@@ -33,11 +32,52 @@
 #define trace_method_calls() {}
 #endif
 
-/*!
- * \brief Schematic::loadDocument tries to load a schematic document.
- * \return true/false in case of success/failure
- */
-bool Schematic::loadVerilogDocument()
+bool Schematic::readLegacy(QFile &file) {
+  trace_method_calls();
+  file.reset();
+  QString Line;
+  QTextStream stream(&file);
+  Line = stream.readLine();
+  while(!stream.atEnd()) {
+    Line = stream.readLine();
+    Line = Line.trimmed();
+    if(Line.isEmpty()) continue;
+    if(Line == "<Symbol>") {
+      if(!loadPaintings(&stream, SymbolPaints)) {
+        file.close();
+        return false;
+      }
+    }
+    else
+    if(Line == "<Properties>") {
+      if(!loadProperties(&stream)) { file.close(); return false; } }
+    else
+    if(Line == "<Components>") {
+      if(!loadComponents(&stream)) { file.close(); return false; } }
+    else
+    if(Line == "<Wires>") {
+      if(!loadWires(&stream)) { file.close(); return false; } }
+    else
+    if(Line == "<Diagrams>") {
+      if(!loadDiagrams(&stream, DocDiags)) { file.close(); return false; } }
+    else
+    if(Line == "<Paintings>") {
+      if(!loadPaintings(&stream, DocPaints)) { file.close(); return false; } }
+    else {
+      qDebug() << Line;
+      QMessageBox::critical(0, QObject::tr("Error"),
+      QObject::tr("File Format Error:\nUnknown field!"));
+      file.close();
+      return false;
+    }
+  }
+  file.close();
+  return true;
+}
+
+bool Schematic::readVerilog(QFile &file)
 {
+  trace_method_calls();
+  file.close();
   return true;
 }
