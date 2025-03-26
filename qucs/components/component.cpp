Qucs/qucs

commit d59e07c16f749ca85121cb3bbbcd1e39d4aeacbd

review Component::attr_get & bump ref
- move _attr back
- fix separator
- bump ref
- TODO? orientation attributes in Command::attr_get (override?)

qucs/components/component.cpp
@@ -759,11 +759,15 @@ void Component::set_port_by_index(int num, std::string const& ext_name)
 // Attributes
 std::string Component::attr_get() const
 {
-  std::string ret = _attr;
-  ret += "qucs_mirroredX=";
+  std::string ret;
+  ret += "qucs_mirrored=";
   ret += std::to_string(mirroredX);
   ret += ", qucs_rotated=";
   ret += std::to_string(rotated);
+  if(_attr.size()){
+    ret += ", " + _attr;
+  }else{
+  }
   return ret;
 }
 

tests/ref/DC_SW_curtice_1_tb1.prj_curtice_1_tb1.sch.vs
@@ -20,17 +20,17 @@
     wire n_380_160;
     wire n_380_220;
     wire n_540_220;
-    (* S0_x1=80, S0_y1=130, S0_x2=80, S0_y2=190, qucs_mirroredX=0, qucs_rotated=1 *) Vdc #(.U(Vgs)) V2 ( n_80_130, n_80_190 );
-    (* S0_x1=80, S0_y1=190, qucs_mirroredX=0, qucs_rotated=0 *) GND #() \*  ( n_80_190 );
-    (* , qucs_mirroredX=0, qucs_rotated=0 *) \.DC  #(.Temp(26.85),.reltol(0.001),.abstol(1 pA),.vntol(1 uV),.saveOPs(no),.MaxIter(150),.saveAll(no),.convHelper(none),.Solver(CroutLU)) DC1 (  );
-    (* , qucs_mirroredX=0, qucs_rotated=0 *) Eqn #(.Id(-V1.I),.Export(yes)) Eqn1 (  );
-    (* , qucs_mirroredX=0, qucs_rotated=0 *) \.SW  #(.Sim(DC1),.Type(lin),.Start(Vds),.Stop(-10),.Points(10),.Points(41)) SW1 (  );
-    (* , qucs_mirroredX=0, qucs_rotated=0 *) \.SW  #(.Sim(SW1),.Type(lin),.Start(Vgs),.Stop(-5),.Points(0),.Points(6)) SW2 (  );
-    (* S0_x1=380, S0_y1=160, qucs_mirroredX=0, qucs_rotated=0 *) GND #() \*  ( n_380_160 );
-    (* S0_x1=380, S0_y1=90, S0_x2=380, S0_y2=150, qucs_mirroredX=0, qucs_rotated=1 *) Vdc #(.U(Vds)) V1 ( n_380_90, n_380_150 );
-    (* S0_x1=210, S0_y1=160, qucs_mirroredX=0, qucs_rotated=0 *) GND #() \*  ( n_210_160 );
-    (* S0_x1=350, S0_y1=60, S0_x2=290, S0_y2=60, qucs_mirroredX=1, qucs_rotated=2 *) IProbe #() Pr1 ( n_350_60, n_290_60 );
-    (* S0_x1=210, S0_y1=160, S0_x2=180, S0_y2=120, S0_x3=210, S0_y3=80, qucs_mirroredX=0, qucs_rotated=0 *) Sub #(.File(curtice_1.sch),.p(1m),.RG(1m),.RD(1m),.RS(10e10),.VBR(1e-15),.LG(1e-15),.LD(1e-15),.LS(10f),.IS(1),.N(0),.XTI(1.11),.EG(3m),.Beta(40m),.Lambda(-6),.VT0(27),.Temp(0.8),.Alpha(300f),.CDS(10p),.TAU(1m),.RIN(300f),.CGS(300f),.CGD(27)) Curtice1 ( n_210_160, n_180_120, n_210_80 );
+    (* S0_x1=80, S0_y1=130, S0_x2=80, S0_y2=190, qucs_mirrored=0, qucs_rotated=1 *) Vdc #(.U(Vgs)) V2 ( n_80_130, n_80_190 );
+    (* S0_x1=80, S0_y1=190, qucs_mirrored=0, qucs_rotated=0 *) GND #() \*  ( n_80_190 );
+    (* qucs_mirrored=0, qucs_rotated=0 *) \.DC  #(.Temp(26.85),.reltol(0.001),.abstol(1 pA),.vntol(1 uV),.saveOPs(no),.MaxIter(150),.saveAll(no),.convHelper(none),.Solver(CroutLU)) DC1 (  );
+    (* qucs_mirrored=0, qucs_rotated=0 *) Eqn #(.Id(-V1.I),.Export(yes)) Eqn1 (  );
+    (* qucs_mirrored=0, qucs_rotated=0 *) \.SW  #(.Sim(DC1),.Type(lin),.Start(Vds),.Stop(-10),.Points(10),.Points(41)) SW1 (  );
+    (* qucs_mirrored=0, qucs_rotated=0 *) \.SW  #(.Sim(SW1),.Type(lin),.Start(Vgs),.Stop(-5),.Points(0),.Points(6)) SW2 (  );
+    (* S0_x1=380, S0_y1=160, qucs_mirrored=0, qucs_rotated=0 *) GND #() \*  ( n_380_160 );
+    (* S0_x1=380, S0_y1=90, S0_x2=380, S0_y2=150, qucs_mirrored=0, qucs_rotated=1 *) Vdc #(.U(Vds)) V1 ( n_380_90, n_380_150 );
+    (* S0_x1=210, S0_y1=160, qucs_mirrored=0, qucs_rotated=0 *) GND #() \*  ( n_210_160 );
+    (* S0_x1=350, S0_y1=60, S0_x2=290, S0_y2=60, qucs_mirrored=1, qucs_rotated=2 *) IProbe #() Pr1 ( n_350_60, n_290_60 );
+    (* S0_x1=210, S0_y1=160, S0_x2=180, S0_y2=120, S0_x3=210, S0_y3=80, qucs_mirrored=0, qucs_rotated=0 *) Sub #(.File(curtice_1.sch),.p(1m),.RG(1m),.RD(1m),.RS(10e10),.VBR(1e-15),.LG(1e-15),.LD(1e-15),.LS(10f),.IS(1),.N(0),.XTI(1.11),.EG(3m),.Beta(40m),.Lambda(-6),.VT0(27),.Temp(0.8),.Alpha(300f),.CDS(10p),.TAU(1m),.RIN(300f),.CGS(300f),.CGD(27)) Curtice1 ( n_210_160, n_180_120, n_210_80 );
     (* S0_x1=380, S0_y1=150, S0_x2=380, S0_y2=160 *) net #() net1 ( n_380_150, n_380_160 );
     (* S0_x1=380, S0_y1=60, S0_x2=380, S0_y2=90 *) net #() net2 ( n_380_60, n_380_90 );
     (* S0_x1=210, S0_y1=60, S0_x2=290, S0_y2=60 *) net #() net3 ( n_210_60, n_290_60 );

tests/ref/misc.prj_libparam.sch.vs
@@ -14,11 +14,11 @@
     wire n_340_250;
     wire n_340_280;
     wire n_340_300;
-    (* S0_x1=140, S0_y1=220, S0_x2=140, S0_y2=280, qucs_mirroredX=0, qucs_rotated=1 *) Vdc #(.U(1 V)) V1 ( n_140_220, n_140_280 );
-    (* S0_x1=140, S0_y1=320, qucs_mirroredX=0, qucs_rotated=0 *) GND #() \*  ( n_140_320 );
-    (* S0_x1=340, S0_y1=220, S0_x2=340, S0_y2=280, qucs_mirroredX=1, qucs_rotated=3 *) R #(.R(50 Ohm),.Temp(26.85),.Tc1(0.0),.Tc2(0.0),.Tnom(26.85)) R1 ( n_340_220, n_340_280 );
-    (* , qucs_mirroredX=0, qucs_rotated=0 *) Lib #(.Lib(Ideal),.Comp(Notch2)) IB1 (  );
-    (* , qucs_mirroredX=0, qucs_rotated=0 *) \.DC  #(.Temp(26.85),.reltol(0.001),.abstol(1 pA),.vntol(1 uV),.saveOPs(no),.MaxIter(150),.saveAll(no),.convHelper(none),.Solver(CroutLU)) DC1 (  );
+    (* S0_x1=140, S0_y1=220, S0_x2=140, S0_y2=280, qucs_mirrored=0, qucs_rotated=1 *) Vdc #(.U(1 V)) V1 ( n_140_220, n_140_280 );
+    (* S0_x1=140, S0_y1=320, qucs_mirrored=0, qucs_rotated=0 *) GND #() \*  ( n_140_320 );
+    (* S0_x1=340, S0_y1=220, S0_x2=340, S0_y2=280, qucs_mirrored=1, qucs_rotated=3 *) R #(.R(50 Ohm),.Temp(26.85),.Tc1(0.0),.Tc2(0.0),.Tnom(26.85)) R1 ( n_340_220, n_340_280 );
+    (* qucs_mirrored=0, qucs_rotated=0 *) Lib #(.Lib(Ideal),.Comp(Notch2)) IB1 (  );
+    (* qucs_mirrored=0, qucs_rotated=0 *) \.DC  #(.Temp(26.85),.reltol(0.001),.abstol(1 pA),.vntol(1 uV),.saveOPs(no),.MaxIter(150),.saveAll(no),.convHelper(none),.Solver(CroutLU)) DC1 (  );
     (* S0_x1=140, S0_y1=280, S0_x2=140, S0_y2=300 *) net #() net1 ( n_140_280, n_140_300 );
     (* S0_x1=140, S0_y1=300, S0_x2=140, S0_y2=320 *) net #() net2 ( n_140_300, n_140_320 );
     (* S0_x1=140, S0_y1=300, S0_x2=340, S0_y2=300 *) net #() net3 ( n_140_300, n_340_300 );

tests/ref/misc.prj_nest1.sch.vs
@@ -12,12 +12,12 @@
     wire n_380_280;
     wire n_380_310;
     wire n_380_360;
-    (* S0_x1=400, S0_y1=180, qucs_mirroredX=0, qucs_rotated=2 *) Port #(.Num(2),.Type(analog)) P2 ( n_400_180 );
-    (* S0_x1=180, S0_y1=180, qucs_mirroredX=1, qucs_rotated=0 *) Port #(.Num(1),.Type(analog)) P1 ( n_180_180 );
-    (* S0_x1=380, S0_y1=360, qucs_mirroredX=0, qucs_rotated=0 *) GND #() \*  ( n_380_360 );
-    (* S0_x1=380, S0_y1=310, S0_x2=380, S0_y2=250, qucs_mirroredX=0, qucs_rotated=1 *) C #(.C(1 pF),.V(),.Symbol(neutral)) C1 ( n_380_310, n_380_250 );
-    (* S0_x1=340, S0_y1=180, qucs_mirroredX=0, qucs_rotated=0 *) Sub #(.File(nest2.sch)) SC2 ( n_340_180 );
-    (* S0_x1=200, S0_y1=180, S0_x2=260, S0_y2=180, qucs_mirroredX=0, qucs_rotated=0 *) R #(.R(0 Ohm),.Temp(26.85),.Tc1(0.0),.Tc2(0.0),.Tnom(26.85)) R1 ( n_200_180, n_260_180 );
+    (* S0_x1=400, S0_y1=180, qucs_mirrored=0, qucs_rotated=2 *) Port #(.Num(2),.Type(analog)) P2 ( n_400_180 );
+    (* S0_x1=180, S0_y1=180, qucs_mirrored=1, qucs_rotated=0 *) Port #(.Num(1),.Type(analog)) P1 ( n_180_180 );
+    (* S0_x1=380, S0_y1=360, qucs_mirrored=0, qucs_rotated=0 *) GND #() \*  ( n_380_360 );
+    (* S0_x1=380, S0_y1=310, S0_x2=380, S0_y2=250, qucs_mirrored=0, qucs_rotated=1 *) C #(.C(1 pF),.V(),.Symbol(neutral)) C1 ( n_380_310, n_380_250 );
+    (* S0_x1=340, S0_y1=180, qucs_mirrored=0, qucs_rotated=0 *) Sub #(.File(nest2.sch)) SC2 ( n_340_180 );
+    (* S0_x1=200, S0_y1=180, S0_x2=260, S0_y2=180, qucs_mirrored=0, qucs_rotated=0 *) R #(.R(0 Ohm),.Temp(26.85),.Tc1(0.0),.Tc2(0.0),.Tnom(26.85)) R1 ( n_200_180, n_260_180 );
     (* S0_x1=380, S0_y1=180, S0_x2=400, S0_y2=180 *) net #() net1 ( n_380_180, n_400_180 );
     (* S0_x1=380, S0_y1=180, S0_x2=380, S0_y2=250 *) net #() net2 ( n_380_180, n_380_250 );
     (* S0_x1=380, S0_y1=310, S0_x2=380, S0_y2=360 *) net #() net3 ( n_380_310, n_380_360 );

commit d7333ca893646829d39c6d77bba608aed6531477

fix parameter parsing
parameter parsing was already broken in d59e07c16f749ca8512
ref/DC_SW_curtice_1_tb1.prj_curtice_1_tb1.sch.vs

possibly introced by db1601d905203863, now reverted:
-    for(int i = 0; i < (numProps-1) && pp != Props.end(); ++i)
+    for(int i = 0; i < (numProps) && pp != Props.end(); ++i)

qucs/components/component.cpp
@@ -1221,19 +1221,26 @@ int Component::analyseLine(const QString& Row, int numProps)
 
     i1 = 1;
     auto pp = Props.begin();
-    for(int i = 0; i < (numProps) && pp != Props.end(); ++i)
+    for(int i = 0; i < (numProps-1) && pp != Props.end(); ++i)
       ++pp;
     for(;;) {
       s = Row.section('"', i1,i1);
-      if(s.isEmpty())  break;
+      if(s.isEmpty()){
+	break;
+      }else{
+      }
 
+      assert(pp!=Props.end());
       ++pp;
       if(pp == Props.end()) {
         Props.push_back(qucs::Property());
         pp = --Props.end();
+	assert(pp!=Props.end());
         pp->display = (s.at(0) == '1');
         pp->Value = s.section('=', 2,2);
+      }else{
       }
+      assert(pp!=Props.end());
 
       pp->Name  = s.section('=', 1,1);
       pp->Description = s.section('=', 3,3);
@@ -1243,10 +1250,11 @@ int Component::analyseLine(const QString& Row, int numProps)
       i1 += 2;
     }
 
-    /// BUG: Lib stores type and filename in Props.
-    /// must keep it, either way
-    /// (there are more bugs related to this).
-    // Props.erase(pp, Props.end());
+    if (pp!=Props.end()){
+      ++pp;
+    }else{
+    }
+    Props.erase(pp, Props.end());
     return 0;   // do not count IDs
   }
   else if(s == "Arrow") {

qucs/components/libcomp.cpp
@@ -285,6 +285,12 @@ int LibComp::loadSymbol()
   x2 = y2 = INT_MIN;
 
   assert(Props.size()>1);
+  if(Props.size()==2){
+	  // BUG: analyseLine erases the trailing parameter?!
+	  // (wtf)
+	 // Props.push_back(qucs::Property("dummy", "", false));
+  }else{
+  }
   QTextStream stream(&FileString, QIODevice::ReadOnly);
   while(!stream.atEnd()) {
     Line = stream.readLine();

qucs/components/subcircuit.cpp
@@ -47,6 +47,7 @@ Subcircuit::Subcircuit()
 Component* Subcircuit::newOne()
 {
   Subcircuit *p = new Subcircuit();
+  assert(Props.size());
   p->Props.front().Value = Props.front().Value;
   p->recreate(0);
   return p;

tests/ref/DC_SW_curtice_1_tb1.prj_curtice_1_tb1.sch.net
@@ -31,4 +31,4 @@ Eqn:Eqn1 Id="-V1.I" Export="yes"
 .SW:SW2 Sim="SW1" Type="lin" Start="Vgs" Stop="-5" Points="0" Points="6"
 Vdc:V1 _net1 gnd U="Vds"
 IProbe:Pr1 _net1 _net2
-Sub:Curtice1 gnd _net0 _net2 Type="curtice_1" p="1m" RG="1m" RD="1m" RS="10e10" VBR="1e-15" LG="1e-15" LD="1e-15" LS="10f" IS="1" N="0" XTI="1.11" EG="3m" Beta="40m" Lambda="-6" VT0="27" Temp="0.8" Alpha="300f" CDS="10p" TAU="1m" RIN="300f" CGS="300f" CGD="27" Tnom="27"
+Sub:Curtice1 gnd _net0 _net2 Type="curtice_1" RG="1m" RD="1m" RS="1m" VBR="10e10" LG="1e-15" LD="1e-15" LS="1e-15" IS="10f" N="1" XTI="0" EG="1.11" Beta="3m" Lambda="40m" VT0="-6" Temp="27" Alpha="0.8" CDS="300f" TAU="10p" RIN="1m" CGS="300f" CGD="300f" Tnom="27"

tests/ref/DC_SW_curtice_1_tb1.prj_curtice_1_tb1.sch.sch
@@ -31,7 +31,7 @@
   <Vdc V1 1 380 120 18 -26 0 1 "Vds" 1>
   <GND * 5 210 160 0 0 0 0>
   <IProbe Pr1 1 320 60 -26 16 1 2>
-  <Sub Curtice1 1 210 120 20 -46 0 0 "curtice_1.sch" 0 "1m" 1 "1m" 1 "1m" 1 "10e10" 1 "1e-15" 1 "1e-15" 1 "1e-15" 1 "10f" 1 "1" 1 "0" 1 "1.11" 1 "3m" 1 "40m" 1 "-6" 1 "27" 1 "0.8" 1 "300f" 1 "10p" 1 "1m" 1 "300f" 1 "300f" 1 "27" 1 "27" 1>
+  <Sub Curtice1 1 210 120 20 -46 0 0 "curtice_1.sch" 0 "1m" 1 "1m" 1 "1m" 1 "10e10" 1 "1e-15" 1 "1e-15" 1 "1e-15" 1 "10f" 1 "1" 1 "0" 1 "1.11" 1 "3m" 1 "40m" 1 "-6" 1 "27" 1 "0.8" 1 "300f" 1 "10p" 1 "1m" 1 "300f" 1 "300f" 1 "27" 1>
 </Components>
 <Wires>
   <380 150 380 160 "" 0 0 0 "">

tests/ref/DC_SW_curtice_1_tb1.prj_curtice_1_tb1.sch.vs
@@ -30,7 +30,7 @@
     (* qucs_mirrored=0, qucs_rotated=1, S0_x1=380, S0_y1=90, S0_x2=380, S0_y2=150 *) Vdc #(.U(Vds)) V1 ( n_380_90, n_380_150 );
     (* qucs_mirrored=0, qucs_rotated=0, S0_x1=210, S0_y1=160 *) GND #() \*  ( n_210_160 );
     (* qucs_mirrored=1, qucs_rotated=2, S0_x1=350, S0_y1=60, S0_x2=290, S0_y2=60 *) IProbe #() Pr1 ( n_350_60, n_290_60 );
-    (* qucs_mirrored=0, qucs_rotated=0, S0_x1=210, S0_y1=160, S0_x2=180, S0_y2=120, S0_x3=210, S0_y3=80 *) Sub #(.File(curtice_1.sch),.p(1m),.RG(1m),.RD(1m),.RS(10e10),.VBR(1e-15),.LG(1e-15),.LD(1e-15),.LS(10f),.IS(1),.N(0),.XTI(1.11),.EG(3m),.Beta(40m),.Lambda(-6),.VT0(27),.Temp(0.8),.Alpha(300f),.CDS(10p),.TAU(1m),.RIN(300f),.CGS(300f),.CGD(27),.Tnom(27)) Curtice1 ( n_210_160, n_180_120, n_210_80 );
+    (* qucs_mirrored=0, qucs_rotated=0, S0_x1=210, S0_y1=160, S0_x2=180, S0_y2=120, S0_x3=210, S0_y3=80 *) Sub #(.File(curtice_1.sch),.RG(1m),.RD(1m),.RS(1m),.VBR(10e10),.LG(1e-15),.LD(1e-15),.LS(1e-15),.IS(10f),.N(1),.XTI(0),.EG(1.11),.Beta(3m),.Lambda(40m),.VT0(-6),.Temp(27),.Alpha(0.8),.CDS(300f),.TAU(10p),.RIN(1m),.CGS(300f),.CGD(300f),.Tnom(27)) Curtice1 ( n_210_160, n_180_120, n_210_80 );
     (* S0_x1=380, S0_y1=150, S0_x2=380, S0_y2=160 *) net #() net1 ( n_380_150, n_380_160 );
     (* S0_x1=380, S0_y1=60, S0_x2=380, S0_y2=90 *) net #() net2 ( n_380_60, n_380_90 );
     (* S0_x1=210, S0_y1=60, S0_x2=290, S0_y2=60 *) net #() net3 ( n_210_60, n_290_60 );

tests/ref/misc.prj_libparam.sch.net
@@ -12,5 +12,5 @@ C:C1 _net4 _net2 C="C" V="0"
 
 Vdc:V1 _net0 gnd U="1 V"
 R:R1 _net1 gnd R="50 Ohm" Temp="26.85" Tc1="0.0" Tc2="0.0" Tnom="26.85"
-Sub:IB1 _net0 _net1 Type="Ideal_Notch2"
+Sub:IB1 _net0 _net1 Type="Ideal_Notch2" fc="1E3" Q="1"
 .DC:DC1 Temp="26.85" reltol="0.001" abstol="1 pA" vntol="1 uV" saveOPs="no" MaxIter="150" saveAll="no" convHelper="none" Solver="CroutLU"

tests/ref/misc.prj_libparam.sch.sch
@@ -19,7 +19,7 @@
   <Vdc V1 1 140 250 18 -26 0 1 "1 V" 1>
   <GND * 5 140 320 0 0 0 0>
   <R R1 1 340 250 -22 15 1 3 "50 Ohm" 1 "26.85" 0 "0.0" 0 "0.0" 0 "26.85" 0 "US" 0>
-  <Lib IB1 1 250 200 0 0 0 0 "Ideal" 0 "Notch2" 0>
+  <Lib IB1 1 250 200 0 0 0 0 "Ideal" 0 "Notch2" 0 "1E3" 1 "1" 1>
   <.DC DC1 1 140 380 0 43 0 0 "26.85" 0 "0.001" 0 "1 pA" 0 "1 uV" 0 "no" 0 "150" 0 "no" 0 "none" 0 "CroutLU" 0>
 </Components>
 <Wires>

tests/ref/misc.prj_libparam.sch.vs
@@ -17,7 +17,7 @@
     (* qucs_mirrored=0, qucs_rotated=1, S0_x1=140, S0_y1=220, S0_x2=140, S0_y2=280 *) Vdc #(.U(1 V)) V1 ( n_140_220, n_140_280 );
     (* qucs_mirrored=0, qucs_rotated=0, S0_x1=140, S0_y1=320 *) GND #() \*  ( n_140_320 );
     (* qucs_mirrored=1, qucs_rotated=3, S0_x1=340, S0_y1=220, S0_x2=340, S0_y2=280 *) R #(.R(50 Ohm),.Temp(26.85),.Tc1(0.0),.Tc2(0.0),.Tnom(26.85)) R1 ( n_340_220, n_340_280 );
-    (* qucs_mirrored=0, qucs_rotated=0, S0_x1=220, S0_y1=200, S0_x2=280, S0_y2=200 *) Lib #(.Lib(Ideal),.Comp(Notch2)) IB1 ( n_220_200, n_280_200 );
+    (* qucs_mirrored=0, qucs_rotated=0, S0_x1=220, S0_y1=200, S0_x2=280, S0_y2=200 *) Lib #(.Lib(Ideal),.Comp(Notch2),.fc(1E3),.Q(1)) IB1 ( n_220_200, n_280_200 );
     (* qucs_mirrored=0, qucs_rotated=0 *) \.DC  #(.Temp(26.85),.reltol(0.001),.abstol(1 pA),.vntol(1 uV),.saveOPs(no),.MaxIter(150),.saveAll(no),.convHelper(none),.Solver(CroutLU)) DC1 (  );
     (* S0_x1=140, S0_y1=280, S0_x2=140, S0_y2=300 *) net #() net1 ( n_140_280, n_140_300 );
     (* S0_x1=140, S0_y1=300, S0_x2=140, S0_y2=320 *) net #() net2 ( n_140_300, n_140_320 );

tests/ref/misc.prj_nest0.sch.net
@@ -7,7 +7,7 @@ C:C1 gnd _net0 C="1 pF" V=""
 
 .Def:nest1 _net1 _net0
 C:C1 gnd _net0 C="1 pF" V=""
-Sub:SC2 _net0 Type="nest2" p=""
+Sub:SC2 _net0 Type="nest2"
 R:R1 _net1 _net2 R="0 Ohm" Temp="26.85" Tc1="0.0" Tc2="0.0" Tnom="26.85"
 .Def:End
 

tests/ref/misc.prj_nest0.vs.net
@@ -7,7 +7,7 @@ C:C1 gnd _net0 C="1 pF" V=""
 
 .Def:nest1 _net1 _net0
 C:C1 gnd _net0 C="1 pF" V=""
-Sub:SC2 _net0 Type="nest2" p=""
+Sub:SC2 _net0 Type="nest2"
 R:R1 _net1 _net2 R="0 Ohm" Temp="26.85" Tc1="0.0" Tc2="0.0" Tnom="26.85"
 .Def:End
 

tests/ref/misc.prj_nest1.sch.net
@@ -6,5 +6,5 @@ C:C1 gnd _net0 C="1 pF" V=""
 .Def:End
 
 C:C1 gnd _net0 C="1 pF" V=""
-Sub:SC2 _net0 Type="nest2" p=""
+Sub:SC2 _net0 Type="nest2"
 R:R1 _net1 _net2 R="0 Ohm" Temp="26.85" Tc1="0.0" Tc2="0.0" Tnom="26.85"

tests/ref/misc.prj_nest1.sch.sch
@@ -23,7 +23,7 @@
   <Port P1 1 180 180 -23 -52 1 0 "1" 1 "analog" 0>
   <GND * 5 380 360 0 0 0 0>
   <C C1 1 380 280 17 -26 0 1 "1 pF" 0 "" 0 "neutral" 0>
-  <Sub SC2 1 310 180 -20 14 0 0 "nest2.sch" 0 "" 0>
+  <Sub SC2 1 310 180 -20 14 0 0 "nest2.sch" 0>
   <R R1 1 230 180 -26 15 0 0 "0 Ohm" 0 "26.85" 0 "0.0" 0 "0.0" 0 "26.85" 0 "US" 0>
 </Components>
 <Wires>

tests/ref/misc.prj_nest1.sch.vs
@@ -16,7 +16,7 @@
     (* qucs_mirrored=1, qucs_rotated=0, S0_x1=180, S0_y1=180 *) Port #(.Num(1),.Type(analog)) P1 ( n_180_180 );
     (* qucs_mirrored=0, qucs_rotated=0, S0_x1=380, S0_y1=360 *) GND #() \*  ( n_380_360 );
     (* qucs_mirrored=0, qucs_rotated=1, S0_x1=380, S0_y1=310, S0_x2=380, S0_y2=250 *) C #(.C(1 pF),.V(),.Symbol(neutral)) C1 ( n_380_310, n_380_250 );
-    (* qucs_mirrored=0, qucs_rotated=0, S0_x1=340, S0_y1=180 *) Sub #(.File(nest2.sch),.p()) SC2 ( n_340_180 );
+    (* qucs_mirrored=0, qucs_rotated=0, S0_x1=340, S0_y1=180 *) Sub #(.File(nest2.sch)) SC2 ( n_340_180 );
     (* qucs_mirrored=0, qucs_rotated=0, S0_x1=200, S0_y1=180, S0_x2=260, S0_y2=180 *) R #(.R(0 Ohm),.Temp(26.85),.Tc1(0.0),.Tc2(0.0),.Tnom(26.85)) R1 ( n_200_180, n_260_180 );
     (* S0_x1=380, S0_y1=180, S0_x2=400, S0_y2=180 *) net #() net1 ( n_380_180, n_400_180 );
     (* S0_x1=380, S0_y1=180, S0_x2=380, S0_y2=250 *) net #() net2 ( n_380_180, n_380_250 );