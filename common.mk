a716ea9315f21bc5859284966610d9c305b84ab0
Felix Salfelder <felix@salfelder.org>
2025-03-28 00:00:00 +0000

    Makefile fixes
    - disentangle & deduplicate qt stuff
    - move -std=c++-14 to AM_CXXFLAGS

--- common.mk
+++ common.mk
@@ -22,13 +22,6 @@
 # the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
 # Boston, MA 02110-1301, USA.
 #
+CXX_STD = -std=c++14
+AM_CXXFLAGS = ${CXX_STD}
 
-SUFFIXES = .qrc
-
-.qrc.cpp:
-	$(RCC) -o $@ $<
-
-SUFFIXES += .moc.cpp
-
-.h.moc.cpp:
-	$(MOC) -o $@ $<
